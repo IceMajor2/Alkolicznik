b369f034f895c67b94c949376b75e7afefbe81b5
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-18 17:03:51 +0200

    Write test + create test data

--- src/test/java/com/demo/alkolicznik/AlkolicznikApplicationTests.java
+++ src/test/java/com/demo/alkolicznik/AlkolicznikApplicationTests.java
@@ -1,11 +1,21 @@
 package com.demo.alkolicznik;
 
+import com.demo.alkolicznik.models.Beer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+
+import javax.sql.DataSource;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,9 +25,11 @@ class AlkolicznikApplicationTests {
 	@Autowired
 	private TestRestTemplate restTemplate;
 
+	private JdbcTemplate jdbcTemplate;
 
-	@Test
-	void contextLoads() {
+	@BeforeEach
+	public void setUp() throws Exception {
+		this.jdbcTemplate = new JdbcTemplate(createTestDataSource());
 	}
 
 	@Test
@@ -25,5 +37,37 @@ class AlkolicznikApplicationTests {
 		ResponseEntity<String> response = restTemplate.getForEntity("/", String.class);
 
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+
+	@Test
+	public void getBeerFromApi() {
+		ResponseEntity<Beer> response = restTemplate.getForEntity("/api/beers/1", Beer.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		Beer beer = response.getBody();
+		Beer expected = jdbcTemplate.queryForObject("SELECT * FROM beers WHERE beers.id = 1", mapToBeer());
+
+		assertThat(beer).isEqualTo(expected);
+	}
+
+	private RowMapper<Beer> mapToBeer() {
+		return new RowMapper<Beer>() {
+			@Override
+			public Beer mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Beer beer = new Beer();
+				beer.setId(rs.getLong("id"));
+				beer.setName(rs.getString("name"));
+				return beer;
+			}
+		};
+	}
+
+	private DataSource createTestDataSource() {
+		return new EmbeddedDatabaseBuilder()
+				.setName("beers")
+				.addScript("/dbs/beer-schema.sql")
+				.addScript("/dbs/beer-data.sql")
+				.build();
 	}
 }
