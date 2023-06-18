28941369d75a74be98728249be40bb61666bfacc
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-18 17:04:15 +0200

    Create beer model

---
+++ src/main/java/com/demo/alkolicznik/models/Beer.java
@@ -0,0 +1,33 @@
+package com.demo.alkolicznik.models;
+
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "beers")
+public class Beer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Nonnull
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Nonnull
+    public String getName() {
+        return name;
+    }
+
+    public void setName(@Nonnull String name) {
+        this.name = name;
+    }
+}
