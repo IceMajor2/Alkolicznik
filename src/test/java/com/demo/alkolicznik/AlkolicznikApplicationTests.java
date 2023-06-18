9ee18c8e8f1c808f061e72ae759eb0e2dce6b624
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-18 16:08:35 +0200

    Write test

--- src/test/java/com/demo/alkolicznik/AlkolicznikApplicationTests.java
+++ src/test/java/com/demo/alkolicznik/AlkolicznikApplicationTests.java
@@ -1,13 +1,29 @@
 package com.demo.alkolicznik;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AlkolicznikApplicationTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void anyoneCanAccessIndexPage() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/", String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
 }
