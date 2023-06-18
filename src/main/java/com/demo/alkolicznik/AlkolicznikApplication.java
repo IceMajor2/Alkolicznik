0d119f9ef6e4fccad9dc847ed0eeb527dbe6239d
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-18 15:15:24 +0200

    Create Spring application

---
+++ src/main/java/com/demo/alkolicznik/AlkolicznikApplication.java
@@ -0,0 +1,13 @@
+package com.demo.alkolicznik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkolicznikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkolicznikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/alkolicznik/AlkolicznikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.alkolicznik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkolicznikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
