cc694dbf5c2cceafec1c96c547d7e074d55c54ce
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-19 17:29:26 +0200

    Find BeerPrice entities of a given store

--- src/main/java/com/demo/alkolicznik/repositories/StoreRepository.java
+++ src/main/java/com/demo/alkolicznik/repositories/StoreRepository.java
@@ -1,7 +1,17 @@
 package com.demo.alkolicznik.repositories;
 
+import com.demo.alkolicznik.models.BeerPrice;
 import com.demo.alkolicznik.models.Store;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 public interface StoreRepository extends CrudRepository<Store, Long> {
+
+    /**
+     * Find {@code BeerPrice} objects of a given store.
+     * @param id store id
+     * @return store's beers with their prices
+     */
+    Set<BeerPrice> findPricesById(Long id);
 }
