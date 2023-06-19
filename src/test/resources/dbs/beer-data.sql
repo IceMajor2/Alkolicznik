a27471ac843ddefa7ca2d656be9dbe52bdd41311
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-19 17:53:25 +0200

    Include beer_price table

--- src/test/resources/dbs/beer-data.sql
+++ src/test/resources/dbs/beer-data.sql
@@ -1,6 +1,6 @@
-INSERT INTO beers (ID, NAME) VALUES (1, 'Perla');
-INSERT INTO beers (ID, NAME) VALUES (2, 'Ksiazece');
-INSERT INTO beers (ID, NAME) VALUES (3, 'Tyskie');
-INSERT INTO beers (ID, NAME) VALUES (4, 'Zubr');
-INSERT INTO beers (ID, NAME) VALUES (5, 'Komes');
-INSERT INTO beers (ID, NAME) VALUES (6, 'Miloslaw');
+INSERT INTO beer (ID, NAME) VALUES (1, 'Perla');
+INSERT INTO beer (ID, NAME) VALUES (2, 'Ksiazece');
+INSERT INTO beer (ID, NAME) VALUES (3, 'Tyskie');
+INSERT INTO beer (ID, NAME) VALUES (4, 'Zubr');
+INSERT INTO beer (ID, NAME) VALUES (5, 'Komes');
+INSERT INTO beer (ID, NAME) VALUES (6, 'Miloslaw');
--- src/test/resources/dbs/beer-schema.sql
+++ src/test/resources/dbs/beer-schema.sql
@@ -1,11 +1,20 @@
-DROP TABLE IF EXISTS stores;
-DROP TABLE IF EXISTS beers;
+DROP TABLE IF EXISTS beer_price;
+DROP TABLE IF EXISTS store;
+DROP TABLE IF EXISTS beer;
 
-CREATE TABLE beers(
-    id INTEGER IDENTITY PRIMARY KEY,
+CREATE TABLE beer (
+    id BIGINT IDENTITY PRIMARY KEY,
     name VARCHAR(30) NOT NULL);
 
-CREATE TABLE stores(
-    id INTEGER IDENTITY PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-    beer_id INTEGER FOREIGN KEY REFERENCES beers(id));
+CREATE TABLE store (
+    id BIGINT IDENTITY PRIMARY KEY,
+    name VARCHAR(30) NOT NULL);
+
+CREATE TABLE beer_price (
+    beer_id BIGINT NOT NULL,
+    store_id BIGINT NOT NULL,
+    price FLOAT(53) NOT NULL,
+    PRIMARY KEY (beer_id, store_id),
+    FOREIGN KEY (beer_id) REFERENCES beer(id),
+    FOREIGN KEY (store_id) REFERENCES store(id)
+    );
