3954a6128463e42f562f27cddda2c51079a1e95d
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-19 13:07:26 +0200

    Update Security

--- src/main/java/com/demo/alkolicznik/security/SecurityConfig.java
+++ src/main/java/com/demo/alkolicznik/security/SecurityConfig.java
@@ -4,17 +4,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private RestAuthenticationEntryPoint authenticationEntryPoint;
+
+    public SecurityConfig(RestAuthenticationEntryPoint authenticationEntryPoint) {
+        this.authenticationEntryPoint = authenticationEntryPoint;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable())
+                .httpBasic((httpBasic) -> httpBasic
+                        .authenticationEntryPoint(authenticationEntryPoint))
                 .authorizeHttpRequests((auth) -> auth
-                        .anyRequest().permitAll());
-               // .httpBasic(Customizer.withDefaults());
+                        .anyRequest().permitAll())
+        ;
         return http.build();
     }
 }
