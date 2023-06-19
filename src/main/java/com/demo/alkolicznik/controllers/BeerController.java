91b7b5c028d795dd3a98cd77c7ecd278a05a8608
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-19 13:09:26 +0200

    Create endpoints with basic functionality (no service yet)

--- src/main/java/com/demo/alkolicznik/controllers/BeerController.java
+++ src/main/java/com/demo/alkolicznik/controllers/BeerController.java
@@ -1,19 +1,36 @@
 package com.demo.alkolicznik.controllers;
 
 import com.demo.alkolicznik.models.Beer;
+import com.demo.alkolicznik.repositories.BeerRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 public class BeerController {
 
+    private BeerRepository beerRepository;
+
+    public BeerController(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
     @GetMapping("/api/beer/{id}")
     public ResponseEntity<Beer> getBeer(@PathVariable Long id) {
-        Beer beer = new Beer();
-        beer.setId(1L);
-        beer.setName("Perla");
+        Beer beer = beerRepository.findById(id).get();
         return ResponseEntity.ok(beer);
+    }
+
+    @PostMapping("/api/beer")
+    public ResponseEntity<Beer> addBear(@RequestBody Beer beer) {
+        Beer saved = beerRepository.save(beer);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(beer.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(saved);
     }
 }
