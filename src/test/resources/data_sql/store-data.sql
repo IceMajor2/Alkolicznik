970472720dbdde559078139087c2103f06faae40
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-20 11:19:32 +0200

    Refactor + store test data

---
+++ src/test/resources/data_sql/store-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO store (ID, NAME) VALUES (1, 'Carrefour');
+INSERT INTO store (ID, NAME) VALUES (2, 'Biedronka');
+INSERT INTO store (ID, NAME) VALUES (3, 'Lidl');
+INSERT INTO store (ID, NAME) VALUES (4, 'ABC');
+INSERT INTO store (ID, NAME) VALUES (5, 'Lubi');
+INSERT INTO store (ID, NAME) VALUES (6, 'Grosik');
