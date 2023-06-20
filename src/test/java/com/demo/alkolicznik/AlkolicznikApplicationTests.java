04b880db48b7f40fc098dab9fbf31aa6298a41ec
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-20 12:15:20 +0200

    Write test for getting a list of stores

--- src/test/java/com/demo/alkolicznik/AlkolicznikApplicationTests.java
+++ src/test/java/com/demo/alkolicznik/AlkolicznikApplicationTests.java
@@ -1,24 +1,34 @@
 package com.demo.alkolicznik;
 
 import com.demo.alkolicznik.models.Beer;
+import com.demo.alkolicznik.models.Store;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.net.URI;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Sql({"/dbs/beer-schema.sql", "/dbs/beer-data.sql"})
+@Sql({"/data_sql/schema.sql", "/data_sql/beer-data.sql", "/data_sql/store-data.sql"})
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AlkolicznikApplicationTests {
 
     @Autowired
@@ -27,6 +37,36 @@ class AlkolicznikApplicationTests {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+
+    private List<Store> stores;
+
+    private List<Beer> beers;
+
+    private static boolean initialized = false;
+    // @BeforeAll does not work because it is executed
+    // even before @Sql annotation
+    @BeforeEach
+    public void setUp() {
+        if(initialized) {
+            return;
+        }
+        this.stores = this.getStores();
+        this.beers = this.getBeers();
+        initialized = true;
+    }
+
+    /**
+     * Launch this test to see whether the
+     * ApplicationContext loads correctly.
+     */
+    @Test
+    public void contextLoads() {
+    }
+
+    /**
+     * Index.html is expected to
+     * be accessible by anyone.
+     */
     @Test
     public void anyoneCanAccessIndexPageTest() {
         ResponseEntity<String> response = restTemplate.getForEntity("/", String.class);
@@ -34,20 +74,30 @@ class AlkolicznikApplicationTests {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
+    /**
+     * Get loaded at launch (through .sql script) beer.
+     */
     @Test
-    public void getBeerFromApiTest() {
+    public void getBeerTest() {
         ResponseEntity<Beer> response = restTemplate.getForEntity("/api/beer/1", Beer.class);
-
+        this.getBeers();
+        this.getStores();
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
         Beer beer = response.getBody();
-        Beer expected = jdbcTemplate.queryForObject("SELECT * FROM beers WHERE beers.id = 1", mapToBeer());
+        Beer expected = jdbcTemplate.queryForObject("SELECT * FROM beer WHERE beer.id = 1", mapToBeer());
 
         assertThat(beer).isEqualTo(expected);
     }
 
+    /**
+     * Creating new beer should return location of the newly
+     * created object within response entity. GET request to
+     * this URI location should return the just-posted beer.
+     */
     @Test
-    public void getCreatedBeerFromApiTest() {
+    @DirtiesContext
+    public void getCreatedBeerTest() {
         // Create new Beer and post it to database.
         Beer beer = new Beer("Lech");
         ResponseEntity<Beer> postResponse = restTemplate.postForEntity("/api/beer", beer, Beer.class);
@@ -62,16 +112,61 @@ class AlkolicznikApplicationTests {
         assertThat(actual).isEqualTo(savedBear);
 
         // Additionally: fetch the beer directly from database.
-		String sql = "SELECT * FROM beers WHERE beers.id = ?";
+        String sql = "SELECT * FROM beer WHERE beer.id = ?";
         Beer dbBeer = jdbcTemplate.queryForObject(sql, mapToBeer(), savedBear.getId());
 
         assertThat(dbBeer).isEqualTo(savedBear);
     }
 
+    /**
+     * Should return HTTP 404 (NOT_FOUND) when fetching
+     * non-existent beer object from database.
+     */
     @Test
     public void getNonExistingBeerShouldReturn404Test() {
         ResponseEntity<Beer> getResponse = restTemplate.getForEntity("/api/beer/9999", Beer.class);
         assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * GET '/api/store' should return a list of stores.
+     */
+    @Test
+    public void getStoresTest() {
+        ResponseEntity<List<Store>> getResponse = restTemplate
+                .exchange("/api/store",
+                        HttpMethod.GET,
+                        HttpEntity.EMPTY,
+                        new ParameterizedTypeReference<List<Store>>() {
+                        });
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        List<Store> actualStores = getResponse.getBody();
+        assertThat(actualStores).isEqualTo(this.getStores());
+    }
+
+    private List<Store> getStores() {
+        String sql = "SELECT * FROM store";
+        List<Store> initializedStores = jdbcTemplate.query(sql, mapToStore());
+        return initializedStores;
+    }
+
+    private List<Beer> getBeers() {
+        String sql = "SELECT * FROM beer";
+        List<Beer> initializedBeers = jdbcTemplate.query(sql, mapToBeer());
+        return initializedBeers;
+    }
+
+    private RowMapper<Store> mapToStore() {
+        return new RowMapper<Store>() {
+            @Override
+            public Store mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Store store = new Store();
+                store.setId(rs.getLong("id"));
+                store.setName(rs.getString("name"));
+                return store;
+            }
+        };
     }
 
     private RowMapper<Beer> mapToBeer() {
