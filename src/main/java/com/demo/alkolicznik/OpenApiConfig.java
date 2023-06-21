a1a5bf41f996ded633413f2019fdcc7b1833d11b
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-21 18:08:48 +0200

    Add Swagger UI

---
+++ src/main/java/com/demo/alkolicznik/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.demo.alkolicznik;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Marcin Startek",
+                        email = "icemajorr2@gmail.com"
+                ),
+                description = "API documentation for Alkolicznik",
+                title = "Alkolicznik Web API",
+                version = "1.0 alpha"
+        )
+)
+public class OpenApiConfig {
+}
