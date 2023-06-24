9f66df79aae8eaf2837334f9b0dc3c1f90b0cc2a
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-24 16:10:17 +0200

    Move RowMapper methods

--- src/test/java/com/demo/alkolicznik/TestConfig.java
+++ src/test/java/com/demo/alkolicznik/TestConfig.java
@@ -5,9 +5,12 @@ import com.demo.alkolicznik.models.Store;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Configuration
@@ -23,14 +26,14 @@ public class TestConfig {
     @Bean
     public List<Store> stores() {
         String sql = "SELECT * FROM store";
-        List<Store> initializedStores = jdbcTemplate.query(sql, TestUtils.mapToStore());
+        List<Store> initializedStores = jdbcTemplate.query(sql, this.mapToStore());
         return initializedStores;
     }
 
     @Bean
     public List<Beer> beers() {
         String sql = "SELECT * FROM beer";
-        List<Beer> initializedBeers = jdbcTemplate.query(sql, TestUtils.mapToBeer());
+        List<Beer> initializedBeers = jdbcTemplate.query(sql, this.mapToBeer());
         return initializedBeers;
     }
 
@@ -43,5 +46,31 @@ public class TestConfig {
                         .addScript("classpath:data_sql/store-data.sql")
                         .addScript("classpath:data_sql/store_equipment-data.sql")
                         .build();
+    }
+
+    private RowMapper<Store> mapToStore() {
+        return new RowMapper<Store>() {
+            @Override
+            public Store mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Store store = new Store();
+                store.setId(rs.getLong("id"));
+                store.setName(rs.getString("name"));
+                return store;
+            }
+        };
+    }
+
+    private RowMapper<Beer> mapToBeer() {
+        return new RowMapper<Beer>() {
+            @Override
+            public Beer mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Beer beer = new Beer();
+                beer.setId(rs.getLong("id"));
+                beer.setBrand(rs.getString("brand"));
+                beer.setType(rs.getString("type"));
+                beer.setVolume(rs.getDouble("volume"));
+                return beer;
+            }
+        };
     }
 }
