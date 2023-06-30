424cd285449de01b68a4acba27291e26c36bd815
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-30 15:00:33 +0200

    Fix SQL syntax

--- src/test/resources/data_sql/store-data.sql
+++ src/test/resources/data_sql/store-data.sql
@@ -1,6 +1,6 @@
-INSERT INTO store (ID, NAME) VALUES (1, 'Carrefour', 'Olsztyn', 'ul. Barcza 4');
-INSERT INTO store (ID, NAME) VALUES (2, 'Biedronka', 'Olsztyn', 'ul. Sikorskiego-Wilczynskiego 12');
-INSERT INTO store (ID, NAME) VALUES (3, 'Lidl', 'Olsztyn', 'Iwaszkiewicza 1');
-INSERT INTO store (ID, NAME) VALUES (4, 'ABC', 'Warszawa', 'ul. Zeromskiego 3');
-INSERT INTO store (ID, NAME) VALUES (5, 'Lubi', 'Warszawa', 'ul. Nowaka 5');
-INSERT INTO store (ID, NAME) VALUES (6, 'Grosik', 'Olsztyn', 'ul. Dworcowa 3');
+INSERT INTO store (ID, NAME, CITY, STREET) VALUES (1, 'Carrefour', 'Olsztyn', 'ul. Barcza 4');
+INSERT INTO store (ID, NAME, CITY, STREET) VALUES (2, 'Biedronka', 'Olsztyn', 'ul. Sikorskiego-Wilczynskiego 12');
+INSERT INTO store (ID, NAME, CITY, STREET) VALUES (3, 'Lidl', 'Olsztyn', 'ul. Iwaszkiewicza 1');
+INSERT INTO store (ID, NAME, CITY, STREET) VALUES (4, 'ABC', 'Warszawa', 'ul. Zeromskiego 3');
+INSERT INTO store (ID, NAME, CITY, STREET) VALUES (5, 'Lubi', 'Warszawa', 'ul. Nowaka 5');
+INSERT INTO store (ID, NAME, CITY, STREET) VALUES (6, 'Grosik', 'Olsztyn', 'ul. Dworcowa 3');
