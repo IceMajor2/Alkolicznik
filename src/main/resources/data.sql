869ce87c925a0c9789f831b074ec09e89570ea09
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-01 12:39:38 +0200

    Update start data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
 -- stores
-INSERT INTO store (ID, NAME) VALUES (1, 'Carrefour')
+INSERT INTO store (ID, NAME, CITY, STREET) VALUES (1, 'Carrefour', 'Olsztyn', 'ul. Borkowskiego 13')
 ON CONFLICT DO NOTHING;
-INSERT INTO store (ID, NAME) VALUES (2, 'Biedronka')
+INSERT INTO store (ID, NAME, CITY, STREET) VALUES (2, 'Biedronka', 'Olsztyn', 'ul. Krasickiego 8')
 ON CONFLICT DO NOTHING;
-INSERT INTO store (ID, NAME) VALUES (3, 'Grosik')
+INSERT INTO store (ID, NAME, CITY, STREET) VALUES (3, 'Grosik', 'Gdansk', 'ul. Marianskiego 9')
 ON CONFLICT DO NOTHING;
 
 -- beers
@@ -18,4 +18,14 @@ ON CONFLICT DO NOTHING;
 INSERT INTO beer (ID, BRAND, TYPE, VOLUME) VALUES (5, 'Komes', 'Malinowe', 0.33)
 ON CONFLICT DO NOTHING;
 INSERT INTO beer (ID, BRAND, TYPE, VOLUME) VALUES (6, 'Miloslaw', 'Biale', 0.5)
+ON CONFLICT DO NOTHING;
+
+-- store equipment
+INSERT INTO beer_price (store_id, beer_id, price) VALUES (1, 1, 3.39)
+ON CONFLICT DO NOTHING;
+INSERT INTO beer_price (store_id, beer_id, price) VALUES (1, 3, 3.29)
+ON CONFLICT DO NOTHING;
+INSERT INTO beer_price (store_id, beer_id, price) VALUES (2, 4, 2.99)
+ON CONFLICT DO NOTHING;
+INSERT INTO beer_price (store_id, beer_id, price) VALUES (3, 6, 6.99)
 ON CONFLICT DO NOTHING;
