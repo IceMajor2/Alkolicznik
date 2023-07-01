1b4c557274443964677b1bb60da7d2c1cc34a4b3
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-01 14:16:25 +0200

    Change response of unauthenticated 401

--- src/main/java/com/demo/alkolicznik/security/RestAuthenticationEntryPoint.java
+++ src/main/java/com/demo/alkolicznik/security/RestAuthenticationEntryPoint.java
@@ -14,6 +14,10 @@ public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+        if (request.getRequestURI().contains("/api/admin")) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, "Resource not found");
+        } else {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+        }
     }
 }
