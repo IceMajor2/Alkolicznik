95bf165612674b92fc890af38c724f35bc11de9b
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-02 18:14:28 +0200

    Update constraints

--- src/main/java/com/demo/alkolicznik/dto/BeerPriceRequestDTO.java
+++ src/main/java/com/demo/alkolicznik/dto/BeerPriceRequestDTO.java
@@ -1,6 +1,9 @@
 package com.demo.alkolicznik.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +14,15 @@ import lombok.Setter;
 public class BeerPriceRequestDTO {
 
     @JsonProperty("beer_name")
+    @NotBlank(message = "Beer was not specified")
     private String beerName;
+
+    @JsonProperty("beer_volume")
+    @Positive(message = "Volume must be a positive number")
+    private Double beerVolume = 0.5;
+
+    @JsonProperty("price")
+    @NotNull(message = "Price was not specified")
+    @Positive(message = "Price must be a positive number")
     private Double price;
 }
