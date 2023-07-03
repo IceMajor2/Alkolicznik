359c0a12c1bbe1da758377148dae6af42383cbd6
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-03 13:45:09 +0200

    Create new BeerPriceRequest DTO

---
+++ src/main/java/com/demo/alkolicznik/dto/BeerPriceRequestDTO_Object.java
@@ -0,0 +1,28 @@
+package com.demo.alkolicznik.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class BeerPriceRequestDTO_Object {
+
+    @JsonProperty("beer_name")
+    @NotBlank(message = "Beer was not specified")
+    private String beerName;
+
+    @JsonProperty("beer_volume")
+    @Positive(message = "Volume must be a positive number")
+    private Double beerVolume = 0.5;
+
+    @JsonProperty("beer_price")
+    @NotNull(message = "Price was not specified")
+    @Positive(message = "Price must be a positive number")
+    private Double price;
+}
