25246c7c776d61bd2252de84ca40ca89d93ce916
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-03 11:58:51 +0200

    Write new tests

--- src/test/java/com/demo/alkolicznik/api/BeerPriceTests.java
+++ src/test/java/com/demo/alkolicznik/api/BeerPriceTests.java
@@ -156,6 +156,28 @@ public class BeerPriceTests {
         }
 
         @Test
+        @DisplayName("Add valid beer price to store: ALL (1-piece name)")
+        @DirtiesContext
+        public void addBeerPriceToStoreTest2() {
+            var postResponse = postRequest("/api/store/{id}/beer-price",
+                    createBeerPriceRequest("Zubr", 0.5, 2.79),
+                    1L);
+            assertThat(postResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+            String actualJson = postResponse.getBody();
+            BeerPriceResponseDTO actual = toModel(actualJson, BeerPriceResponseDTO.class);
+
+            BeerPriceResponseDTO expected = createBeerPriceResponse(
+                    createBeerResponse(4L, "Zubr", 0.5),
+                    createStoreResponse(1L, "Carrefour", "Olsztyn", "ul. Barcza 4"),
+                    2.79
+            );
+            String expectedJson = toJsonString(expected);
+            assertThat(actual).isEqualTo(expected);
+            assertThat(actualJson).isEqualTo(expectedJson);
+        }
+
+        @Test
         @DisplayName("Add valid beer price to store: VOLUME not specified")
         @DirtiesContext
         public void addBeerPriceToStoreDefaultVolumeTest() {
@@ -282,6 +304,36 @@ public class BeerPriceTests {
                     HttpStatus.BAD_REQUEST,
                     "Beer was not specified; Price must be a positive number; Volume must be a positive number",
                     "/api/store/2/beer-price");
+        }
+
+        @Test
+        @DisplayName("Add invalid beer price to store: BEER_NOT_EXISTS")
+        public void createBeerPriceBeerNotExistsTest() {
+            var postResponse = postRequest("/api/store/{id}/beer-price",
+                    createBeerPriceRequest("Kormoran Miodne", 0.5, 7.99),
+                    4L);
+
+            String jsonResponse = postResponse.getBody();
+
+            assertIsError(jsonResponse,
+                    HttpStatus.NOT_FOUND,
+                    "Unable to find beer of 'Kormoran Miodne' name",
+                    "/api/store/4/beer-price");
+        }
+
+        @Test
+        @DisplayName("Add valid beer price to invalid store: STORE_NOT_EXISTS")
+        public void createBeerPriceStoresNotExistsTest() {
+            var postResponse = postRequest("/api/store/{id}/beer-price",
+                    createBeerPriceRequest("Ksiazece Zlote pszeniczne", 0.5, 3.79),
+                    9999L);
+
+            String jsonResponse = postResponse.getBody();
+
+            assertIsError(jsonResponse,
+                    HttpStatus.NOT_FOUND,
+                    "Unable to find store of 9999 id",
+                    "/api/store/9999/beer-price");
         }
     }
 }
