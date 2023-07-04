76509cfa581bbb7c5a60aa41bd296648d96f0665
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-04 16:37:52 +0200

    Change HTTP code

--- src/main/java/com/demo/alkolicznik/exceptions/classes/PropertiesMissingException.java
+++ src/main/java/com/demo/alkolicznik/exceptions/classes/PropertiesMissingException.java
@@ -3,6 +3,6 @@ package com.demo.alkolicznik.exceptions.classes;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "No property to update was specified")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "No property to update was specified")
 public class PropertiesMissingException extends RuntimeException {
 }
