38d3b711fc465941a7660244ba6c8bf2a57e833b
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-06 15:38:36 +0200

    Create new page route class + provide welcome page with components

---
+++ src/main/java/com/demo/alkolicznik/gui/BeerPage.java
@@ -0,0 +1,4 @@
+package com.demo.alkolicznik.gui;
+
+public class BeerPage {
+}
--- src/main/java/com/demo/alkolicznik/gui/WelcomePage.java
+++ src/main/java/com/demo/alkolicznik/gui/WelcomePage.java
@@ -1,13 +1,39 @@
 package com.demo.alkolicznik.gui;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Route("")
+@PageTitle("Alkolicznik")
 public class WelcomePage extends VerticalLayout {
 
     public WelcomePage() {
-        add(new H1("Hello world!"));
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        List<Component> components = getMainPageComponents();
+        add(components);
+    }
+
+    public List<Component> getMainPageComponents() {
+        List<Component> components = new ArrayList<>();
+
+        H1 header = new H1("Alkolicznik");
+        Button login = new Button("Zaloguj się");
+        Button beers = new Button("Piwa");
+        Button stores = new Button("Sklepy");
+        Button button = new Button("Ceny");
+
+        components.add(header);
+        components.add(login);
+        components.add(beers);
+        components.add(stores);
+        components.add(button);
+        return components;
     }
 }
