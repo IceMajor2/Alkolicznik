d4150d595e1a9cc6e5dbd647aa6bb6ad9f008b06
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-08 00:47:07 +0200

    Remove commented line

--- src/main/java/com/demo/alkolicznik/security/SecurityConfig.java
+++ src/main/java/com/demo/alkolicznik/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.demo.alkolicznik.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -13,7 +12,6 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-// @EnableMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 public class SecurityConfig {
 
