d494880721815177d24c647ef9982166f1268661
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-07 23:41:33 +0200

    Set theme config + PWA

--- src/main/java/com/demo/alkolicznik/AlkolicznikApplication.java
+++ src/main/java/com/demo/alkolicznik/AlkolicznikApplication.java
@@ -1,14 +1,22 @@
 package com.demo.alkolicznik;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-public class AlkolicznikApplication {
+@Theme(value = "style")
+@PWA(
+        name = "Alkolicznik",
+        shortName = "Alkolicznik"
+)
+public class AlkolicznikApplication implements AppShellConfigurator {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AlkolicznikApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AlkolicznikApplication.class, args);
+    }
 
 }
