a6ad8b56f99ebc2d58f05e9b8aa27bae87656201
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-08 16:47:30 +0200

    Assign automatically a role on user create

--- src/main/java/com/demo/alkolicznik/api/services/AuthService.java
+++ src/main/java/com/demo/alkolicznik/api/services/AuthService.java
@@ -2,6 +2,7 @@ package com.demo.alkolicznik.api.services;
 
 import com.demo.alkolicznik.dto.requests.UserRequestDTO;
 import com.demo.alkolicznik.exceptions.classes.UserAlreadyExistsException;
+import com.demo.alkolicznik.models.Roles;
 import com.demo.alkolicznik.models.User;
 import com.demo.alkolicznik.repositories.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,13 +20,23 @@ public class AuthService {
     }
 
     public User registerUser(UserRequestDTO userDTO) {
-        if(userRepository.existsByUsername(userDTO.getUsername())) {
+        if (userRepository.existsByUsername(userDTO.getUsername())) {
             throw new UserAlreadyExistsException();
         }
         userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
 
         User user = new User(userDTO);
+        assignRole(user);
+
         User saved = userRepository.save(user);
         return saved;
+    }
+
+    private void assignRole(User user) {
+        if (userRepository.count() == 0) {
+            user.getRoles().add(Roles.ADMIN);
+            return;
+        }
+        user.getRoles().add(Roles.USER);
     }
 }
