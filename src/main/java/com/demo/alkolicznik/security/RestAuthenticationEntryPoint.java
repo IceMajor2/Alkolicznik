8fb3207f1b871d9740b569d390920d276eec757e
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-08 16:56:40 +0200

    Handle forbidden and unauthorized requests for '/api/admin' endpoints

--- src/main/java/com/demo/alkolicznik/security/RestAuthenticationEntryPoint.java
+++ src/main/java/com/demo/alkolicznik/security/RestAuthenticationEntryPoint.java
@@ -3,14 +3,16 @@ package com.demo.alkolicznik.security;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
-public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint, AccessDeniedHandler {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
@@ -18,6 +20,15 @@ public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
             response.sendError(HttpServletResponse.SC_NOT_FOUND, "Resource not found");
         } else {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+        }
+    }
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        if (request.getRequestURI().contains("/api/admin")) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, "Resource not found");
+        } else {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, accessDeniedException.getMessage());
         }
     }
 }
--- src/main/java/com/demo/alkolicznik/security/SecurityConfig.java
+++ src/main/java/com/demo/alkolicznik/security/SecurityConfig.java
@@ -1,15 +1,10 @@
 package com.demo.alkolicznik.security;
 
-import com.demo.alkolicznik.models.Roles;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -28,25 +23,13 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .httpBasic((httpBasic) -> httpBasic
                         .authenticationEntryPoint(authenticationEntryPoint))
+                .exceptionHandling(excHandler -> excHandler.accessDeniedHandler(authenticationEntryPoint))
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/api/admin/**").hasAuthority("ADMIN")
                         .anyRequest().permitAll())
         ;
         return http.build();
     }
-
-//    @Bean
-//    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
-//        UserDetails admin = User.withUsername("admin")
-//                .password(passwordEncoder.encode("admin"))
-//                .authorities("ADMIN")
-//                .build();
-//        UserDetails admin2 = User.withUsername("admin2")
-//                .password(passwordEncoder.encode("admin"))
-//                .authorities("ADMIN")
-//                .build();
-//        return new InMemoryUserDetailsManager(admin, admin2);
-//    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
