0be474e2ac66fed28d13b02afe5fd9c071201c91
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-08 16:44:15 +0200

    Implement manual database design with DDL update

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS beer_price;
+DROP TABLE IF EXISTS store;
+DROP TABLE IF EXISTS beer;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE beer (
+    id BIGINT PRIMARY KEY,
+    brand VARCHAR(30) NOT NULL,
+    type VARCHAR(30),
+    volume FLOAT(53) DEFAULT 0.5);
+
+CREATE TABLE store (
+    id BIGINT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    city VARCHAR(30) NOT NULL,
+    street VARCHAR(60) NOT NULL);
+
+CREATE TABLE beer_price (
+    beer_id BIGINT NOT NULL,
+    store_id BIGINT NOT NULL,
+    price_amount NUMERIC(38, 2) NOT NULL,
+    price_currency VARCHAR(255) DEFAULT 'PLN',
+    PRIMARY KEY (beer_id, store_id),
+    FOREIGN KEY (beer_id) REFERENCES beer(id),
+    FOREIGN KEY (store_id) REFERENCES store(id)
+    );
+
+CREATE TABLE users (
+    id BIGINT PRIMARY KEY,
+    username VARCHAR(32) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    account_non_locked BOOLEAN,
+    roles VARCHAR(255) ARRAY
+);
