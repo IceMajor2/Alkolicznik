46c2609ec55c731bd1e65e4565becc08ee86bffb
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-08 15:08:09 +0200

    Change exception reason message

--- src/main/java/com/demo/alkolicznik/exceptions/classes/UserAlreadyExistsException.java
+++ src/main/java/com/demo/alkolicznik/exceptions/classes/UserAlreadyExistsException.java
@@ -3,6 +3,6 @@ package com.demo.alkolicznik.exceptions.classes;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.CONFLICT, reason = "User already exists")
+@ResponseStatus(code = HttpStatus.CONFLICT, reason = "Username is already taken")
 public class UserAlreadyExistsException extends RuntimeException {
 }
