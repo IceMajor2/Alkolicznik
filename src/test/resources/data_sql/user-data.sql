0cd4ec13aa9b31593e32cedf90af9828867168d1
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-08 15:03:40 +0200

    Add user test data

---
+++ src/test/resources/data_sql/user-data.sql
@@ -0,0 +1,10 @@
+INSERT INTO users (ID, USERNAME, PASSWORD, ACCOUNT_NON_LOCKED, ROLES) -- "admin", "Nacovuvaxa77"
+    VALUES (1, 'admin', '$2y$13$kSVDc2VayVtA4Zv6eGcLn.rlC3DJWSOoXJL2mlp77Pf9q7ksze5pO', true, ARRAY['ADMIN']);
+INSERT INTO users (ID, USERNAME, PASSWORD, ACCOUNT_NON_LOCKED, ROLES) -- "jacek", "Lasuviyojo90"
+    VALUES (2, 'jacek', '$2y$13$gs0zdMMq/Ed1PFEiOWi7ZeJaFlXryv6bJ6ywgkmf1yQMh3DFeVfpm', true, ARRAY['USER']);
+INSERT INTO users (ID, USERNAME, PASSWORD, ACCOUNT_NON_LOCKED, ROLES) -- "user", "Wijotatewo42"
+    VALUES (3, 'user', '$2y$13$kOPlxvOwGQavy7zniiBUG.RxRy9ZjcvF906pN8JY0oL9jOKIUhqq6', true, ARRAY['USER']);
+INSERT INTO users (ID, USERNAME, PASSWORD, ACCOUNT_NON_LOCKED, ROLES) -- "kacprox07", "Kodobarazi20"
+    VALUES (4, 'kacprox07', '$2y$13$8gz4b5kwgWQ2t6a.SY3lk.Uct6zySI1blvdh6MF.8EmEMDd1ufulu', true, ARRAY['ACCOUNTANT']);
+INSERT INTO users (ID, USERNAME, PASSWORD, ACCOUNT_NON_LOCKED, ROLES) -- "heckler", "Gaziwubalu66"
+    VALUES (5, 'heckler', '$2y$13$z951BviP1jqU7jfpJ7j8g.jISs7hOcZxJH/dTqzy/NSfaGcT0/vNu', false, ARRAY['USER']);
