156672599af8f1a62bd246fb6d60dbcd32a53ff2
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-09 17:26:15 +0200

    Remove unused imports

--- src/main/java/com/demo/alkolicznik/security/SecurityVaadinConfig.java
+++ src/main/java/com/demo/alkolicznik/security/SecurityVaadinConfig.java
@@ -2,15 +2,10 @@ package com.demo.alkolicznik.security;
 
 import com.demo.alkolicznik.gui.LoginView;
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
