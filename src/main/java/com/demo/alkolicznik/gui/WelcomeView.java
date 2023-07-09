97619ea4bf64de9162ed18f74ff325a74b1e9d3d
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-09 23:22:11 +0200

    Update

--- src/main/java/com/demo/alkolicznik/gui/WelcomeView.java
+++ src/main/java/com/demo/alkolicznik/gui/WelcomeView.java
@@ -5,6 +5,7 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -24,30 +25,55 @@ public class WelcomeView extends VerticalLayout {
     public WelcomeView(@Autowired AuthService authService) {
         this.authService = authService;
 
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        List<Component> components = getMainPageComponents();
-        add(components);
+        HorizontalLayout hl = getHorizontalLayout();
+        VerticalLayout vl = getMainLayout();
+
+        add(hl, vl);
     }
 
-    public List<Component> getMainPageComponents() {
+    private HorizontalLayout getHorizontalLayout() {
+        HorizontalLayout hl = new HorizontalLayout();
+        hl.setWidthFull();
+        hl.setJustifyContentMode(JustifyContentMode.END);
+
+        Button button = getAuthButton();
+        hl.add(button);
+        return hl;
+    }
+
+    private VerticalLayout getMainLayout() {
+        VerticalLayout vl = new VerticalLayout();
+        vl.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+
+        var components = getMainComponents();
+        vl.add(components);
+
+        return vl;
+    }
+
+    private Button getAuthButton() {
+        Button loginout;
+        if (authService.getAuthenticatedUser() != null) {
+            loginout = new Button("Wyloguj się", click -> authService.logout());
+        } else {
+            loginout = new Button("Zaloguj się", click -> UI.getCurrent().navigate("login"));
+        }
+        return loginout;
+    }
+
+
+    private List<Component> getMainComponents() {
         List<Component> components = new ArrayList<>();
 
         H1 header = new H1("Alkolicznik");
-        Button login = new Button("Zaloguj się", click -> UI.getCurrent().navigate("login"));
         Button beers = new Button("Piwa", click -> UI.getCurrent().navigate("beer"));
         Button stores = new Button("Sklepy", click -> UI.getCurrent().navigate("store"));
         Button button = new Button("Ceny", click -> UI.getCurrent().navigate("beer-price"));
 
         components.add(header);
-        components.add(login);
         components.add(beers);
         components.add(stores);
         components.add(button);
-
-        if (authService.getAuthenticatedUser() != null) {
-            Button logout = new Button("Wyloguj się", click -> authService.logout());
-            components.add(logout);
-        }
         return components;
     }
 }
