990ae0a46e0098dff8fc823a2e4bbc58324ba9f1
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-13 13:35:48 +0200

    Make findAll query return ordered list of beers by id

--- src/main/java/com/demo/alkolicznik/repositories/BeerRepository.java
+++ src/main/java/com/demo/alkolicznik/repositories/BeerRepository.java
@@ -14,10 +14,13 @@ public interface BeerRepository extends CrudRepository<Beer, Long> {
     @Query(value = "SELECT * FROM beer b WHERE concat_ws(' ', brand, type) = ?1 AND volume = ?2", nativeQuery = true)
     Optional<Beer> findByFullnameAndVolume(String fullname, Double volume);
 
+    @Query(value = "SELECT b.id FROM beer b WHERE concat_ws(' ', brand, type) = ?1 AND volume = ?2", nativeQuery = true)
+    Long findIdByFullnameAndVolume(String fullname, Double volume);
+
     @Query(value = "SELECT count(*) > 0 FROM beer b WHERE concat_ws(' ', brand, type) = ?1", nativeQuery = true)
     boolean existsByFullname(String fullname);
 
-    List<Beer> findAll();
+    List<Beer> findAllByOrderByIdAsc();
 
     default boolean exists(Beer beer) {
         String brand = beer.getBrand();
