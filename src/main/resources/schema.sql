c8d479bc34ad64e952202b3925a14a60cb0f96b8
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-13 13:30:18 +0200

    Fix primary key creation

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,21 +1,21 @@
-DROP TABLE IF EXISTS beer_price;
-DROP TABLE IF EXISTS store;
-DROP TABLE IF EXISTS beer;
-DROP TABLE IF EXISTS users;
+-- DROP TABLE IF EXISTS beer_price;
+-- DROP TABLE IF EXISTS store;
+-- DROP TABLE IF EXISTS beer;
+-- DROP TABLE IF EXISTS users;
 
-CREATE TABLE beer (
-    id BIGINT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS beer (
+    id SERIAL PRIMARY KEY,
     brand VARCHAR(30) NOT NULL,
     type VARCHAR(30),
     volume FLOAT(53) DEFAULT 0.5);
 
-CREATE TABLE store (
-    id BIGINT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS store (
+    id SERIAL PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
     city VARCHAR(30) NOT NULL,
     street VARCHAR(60) NOT NULL);
 
-CREATE TABLE beer_price (
+CREATE TABLE IF NOT EXISTS beer_price (
     beer_id BIGINT NOT NULL,
     store_id BIGINT NOT NULL,
     price_amount NUMERIC(38, 2) NOT NULL,
@@ -25,8 +25,8 @@ CREATE TABLE beer_price (
     FOREIGN KEY (store_id) REFERENCES store(id)
     );
 
-CREATE TABLE users (
-    id BIGINT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
     username VARCHAR(32) NOT NULL,
     password VARCHAR(255) NOT NULL,
     account_non_locked BOOLEAN,
