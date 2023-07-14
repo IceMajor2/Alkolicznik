92c90b5d61e3c67f140d85786ca50845e2b2f998
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-14 14:03:32 +0200

    Get just a numeric price value out of price string

--- src/main/java/com/demo/alkolicznik/dto/responses/BeerPriceResponseDTO.java
+++ src/main/java/com/demo/alkolicznik/dto/responses/BeerPriceResponseDTO.java
@@ -21,4 +21,42 @@ public class BeerPriceResponseDTO {
         this.beer = new BeerResponseDTO(beerPrice.getBeer());
         this.price = beerPrice.getPrice().toString();
     }
+
+    public Double getAmountOnly() {
+        int firstDigitIndex = findFirstDigitIndex(this.price);
+        int lastDigitIndex = findLastDigitIndex(this.price, firstDigitIndex);
+
+        return Double.valueOf(this.price.substring(firstDigitIndex, lastDigitIndex + 1));
+    }
+
+    private boolean isDigit(char character) {
+        return character >= 48 && character <= 57;
+    }
+
+    private int findFirstDigitIndex(String string) {
+        int currIndex = 0;
+        for (char ch : string.toCharArray()) {
+            if (isDigit(ch)) {
+                return currIndex;
+            }
+            currIndex++;
+        }
+        return -1;
+    }
+
+    private int findLastDigitIndex(String string, int firstDigitIndex) {
+        int currIndex = firstDigitIndex + 1;
+        for (int i = currIndex; i < string.length(); i++) {
+            if (string.charAt(i) == '.') {
+                continue;
+            }
+            if (!isDigit(string.charAt(i))) {
+                return i - 1;
+            }
+        }
+        if (isDigit(string.charAt(string.length() - 1))) {
+            return string.length() - 1;
+        }
+        return -1;
+    }
 }
