7e6b8836fe4bb074074f77d1ca7ac458b9cacc4a
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-14 15:36:14 +0200

    Add LEFT JOIN FETCH to queries

--- src/main/java/com/demo/alkolicznik/repositories/BeerRepository.java
+++ src/main/java/com/demo/alkolicznik/repositories/BeerRepository.java
@@ -3,19 +3,21 @@ package com.demo.alkolicznik.repositories;
 import com.demo.alkolicznik.models.Beer;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface BeerRepository extends CrudRepository<Beer, Long> {
 
-    Optional<Beer> findByBrand(String brand);
+    @Query("SELECT b FROM Beer b LEFT JOIN FETCH b.prices WHERE b.id = :id")
+    Optional<Beer> findById(@Param("id") Long id);
 
     @Query(value = "SELECT * FROM beer b WHERE concat_ws(' ', brand, type) = ?1 AND volume = ?2", nativeQuery = true)
     Optional<Beer> findByFullnameAndVolume(String fullname, Double volume);
 
-    @Query(value = "SELECT b.id FROM beer b WHERE concat_ws(' ', brand, type) = ?1 AND volume = ?2", nativeQuery = true)
-    Long findIdByFullnameAndVolume(String fullname, Double volume);
+//    @Query(value = "SELECT b.id FROM beer b WHERE concat_ws(' ', brand, type) = ?1 AND volume = ?2", nativeQuery = true)
+//    Long findIdByFullnameAndVolume(String fullname, Double volume);
 
     @Query(value = "SELECT count(*) > 0 FROM beer b WHERE concat_ws(' ', brand, type) = ?1", nativeQuery = true)
     boolean existsByFullname(String fullname);
--- src/main/java/com/demo/alkolicznik/repositories/StoreRepository.java
+++ src/main/java/com/demo/alkolicznik/repositories/StoreRepository.java
@@ -24,6 +24,10 @@ public interface StoreRepository extends CrudRepository<Store, Long> {
     @Query("SELECT s FROM Store s LEFT JOIN FETCH s.prices WHERE s.city = :city ORDER BY s.id ASC")
     List<Store> findAllByCityOrderByIdAsc(@Param("city") String city);
 
+    @Query("SELECT s FROM Store s LEFT JOIN FETCH s.prices WHERE s.id = :id")
+    Optional<Store> findById(@Param("id") Long id);
+
+    @Query("SELECT s FROM Store s LEFT JOIN FETCH s.prices WHERE s.name = ?1 AND s.city = ?2 AND s.street = ?3")
     Optional<Store> findByNameAndCityAndStreet(String name, String city, String street);
 
     default Optional<Store> findByStoreRequest(StoreRequestDTO storeRequestDTO) {
