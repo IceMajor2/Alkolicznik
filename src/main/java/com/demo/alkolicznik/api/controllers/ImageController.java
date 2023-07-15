86d0ef6ea54abcf0ef6cc2a294162c215aa547c6
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-15 13:28:58 +0200

    Implement simple, currently abstract endpoints

--- src/main/java/com/demo/alkolicznik/api/controllers/ImageController.java
+++ src/main/java/com/demo/alkolicznik/api/controllers/ImageController.java
@@ -1,14 +1,31 @@
 package com.demo.alkolicznik.api.controllers;
 
 import com.demo.alkolicznik.api.services.ImageService;
-import org.springframework.web.bind.annotation.RestController;
+import com.demo.alkolicznik.models.Image;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/image")
 public class ImageController {
 
     private ImageService imageService;
 
     public ImageController(ImageService imageService) {
         this.imageService = imageService;
+    }
+
+    @GetMapping("/{beer_id}")
+    @SecurityRequirement(name = "Basic Authentication")
+    public Image get(@PathVariable("beer_id") Long beerId) {
+        return null;
+    }
+
+    @PostMapping("/beer/{beer_id}")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'ACCOUNTANT')")
+    public Image addBeerImageParam(@PathVariable("beer_id") Long beerId,
+                                   @RequestParam("path") String path) {
+        return null;
     }
 }
