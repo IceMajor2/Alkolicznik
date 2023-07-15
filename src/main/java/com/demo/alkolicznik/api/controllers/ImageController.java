a9e7a7621d550f3abe4368cfcc78bf107cad0bda
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-15 21:05:45 +0200

    Rename Image (previously) class

--- src/main/java/com/demo/alkolicznik/api/controllers/ImageController.java
+++ src/main/java/com/demo/alkolicznik/api/controllers/ImageController.java
@@ -1,8 +1,8 @@
 package com.demo.alkolicznik.api.controllers;
 
 import com.demo.alkolicznik.api.services.ImageService;
-import com.demo.alkolicznik.dto.responses.ImageResponseDTO;
-import com.demo.alkolicznik.models.Image;
+import com.demo.alkolicznik.dto.responses.ImageModelResponseDTO;
+import com.demo.alkolicznik.models.ImageModel;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -19,14 +19,14 @@ public class ImageController {
 
     @GetMapping("/beer/{beer_id}/image")
     @SecurityRequirement(name = "Basic Authentication")
-    public ImageResponseDTO get(@PathVariable("beer_id") Long beerId) {
-        return imageService.getBeerImage(beerId);
+    public ImageModelResponseDTO get(@PathVariable("beer_id") Long beerId) {
+        return imageService.getBeerImageLink(beerId);
     }
 
     @PostMapping("/beer/image/{beer_id}")
     @PreAuthorize("hasAnyAuthority('ADMIN', 'ACCOUNTANT')")
-    public Image addBeerImageParam(@PathVariable("beer_id") Long beerId,
-                                   @RequestParam("path") String path) {
+    public ImageModel addBeerImageParam(@PathVariable("beer_id") Long beerId,
+                                        @RequestParam("path") String path) {
         return null;
     }
 }
