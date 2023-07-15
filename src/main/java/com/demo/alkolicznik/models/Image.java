e7cf1b2a4691e4de5dc9d185a40956673204af38
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-15 13:29:22 +0200

    Ignore field in JSON response + change column name

--- src/main/java/com/demo/alkolicznik/models/Image.java
+++ src/main/java/com/demo/alkolicznik/models/Image.java
@@ -1,23 +1,30 @@
 package com.demo.alkolicznik.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity(name = "Image")
 @Table(name = "image")
 @NoArgsConstructor
 @Getter
 @Setter
+@ToString
 public class Image {
 
     @Id
     @Column(name = "beer_id")
     private Long id;
-    private String imageAddress;
+
+    @Column(name = "url")
+    private String imageUrl;
+
     @OneToOne(fetch = FetchType.LAZY)
     @MapsId
     @JoinColumn(name = "beer_id")
+    @JsonIgnore
     private Beer beer;
 }
