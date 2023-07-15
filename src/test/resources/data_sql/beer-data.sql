090bbc371b83114196cad46afa255ebb4c5cf923
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-15 12:35:32 +0200

    Change some beer details

--- src/test/resources/data_sql/beer-data.sql
+++ src/test/resources/data_sql/beer-data.sql
@@ -1,6 +1,6 @@
 INSERT INTO beer (ID, BRAND, TYPE) VALUES (1, 'Perla', 'Chmielowa Pils');
 INSERT INTO beer (ID, BRAND, TYPE) VALUES (2, 'Ksiazece', 'Zlote pszeniczne');
-INSERT INTO beer (ID, BRAND, TYPE, VOLUME) VALUES (3, 'Tyskie', 'Gronie', 0.6);
+INSERT INTO beer (ID, BRAND, TYPE, VOLUME) VALUES (3, 'Tyskie', 'Gronie', 0.65);
 INSERT INTO beer (ID, BRAND) VALUES (4, 'Zubr');
-INSERT INTO beer (ID, BRAND, TYPE, VOLUME) VALUES (5, 'Komes', 'Malinowe', 0.33);
+INSERT INTO beer (ID, BRAND, TYPE, VOLUME) VALUES (5, 'Komes', 'Porter Malinowy', 0.33);
 INSERT INTO beer (ID, BRAND, TYPE) VALUES (6, 'Miloslaw', 'Biale');
