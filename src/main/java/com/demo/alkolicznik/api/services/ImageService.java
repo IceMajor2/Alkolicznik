7c383107f248a47f0323179295da2a82fcb65edd
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-15 11:27:36 +0200

    Check for image's proportions at upload

--- src/main/java/com/demo/alkolicznik/api/services/ImageService.java
+++ src/main/java/com/demo/alkolicznik/api/services/ImageService.java
@@ -2,12 +2,13 @@ package com.demo.alkolicznik.api.services;
 
 import io.imagekit.sdk.ImageKit;
 import io.imagekit.sdk.config.Configuration;
-import io.imagekit.sdk.exceptions.*;
 import io.imagekit.sdk.models.FileCreateRequest;
 import io.imagekit.sdk.models.results.Result;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -19,35 +20,53 @@ public class ImageService {
     @SneakyThrows
     public static void main(String[] args) {
         ImageService imageService = new ImageService();
-        imageService.uploadImage();
+        imageService.uploadImage("C:\\Users\\IceMajor\\Downloads\\Zubr.png");
     }
 
     private ImageKit imageKit;
 
-    @SneakyThrows
     public ImageService() {
         this.imageKit = ImageKit.getInstance();
-        Configuration config = readConfig();
-        imageKit.setConfig(config);
-        config = null;
+        readConfig();
     }
 
-    public Result uploadImage() throws IOException, ForbiddenException, TooManyRequestsException, InternalServerException, UnauthorizedException, BadRequestException, UnknownException {
-        byte[] bytes = Files.readAllBytes(Paths.get("secure" + File.separator + "sample.jpg"));
+    @SneakyThrows
+    public Result uploadImage(String path) {
+        BufferedImage image = ImageIO.read(new File(path));
+        if(!areImageProportionsOk(image)) {
+            throw new RuntimeException("Obrazek jest niewlasciwych proporcji");
+        }
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
         FileCreateRequest fileCreateRequest = new FileCreateRequest(bytes, "sample_image.jpg");
         Result result = ImageKit.getInstance().upload(fileCreateRequest);
         return result;
     }
 
-    private Configuration readConfig() {
+    private void readConfig() {
         String endpoint = "https://ik.imagekit.io/icemajor";
         String publicKey = "public_YpQHYFb3+OX4R5aHScftYE0H0N8=";
         try {
-            return new Configuration(publicKey,
+            imageKit.setConfig(new Configuration(publicKey,
                     Files.readAllLines(Paths.get("secure" + File.separator + "api_key.txt")).get(0),
-                    endpoint);
+                    endpoint));
         } catch (IOException e) {
             throw new RuntimeException("Could not read secured file");
         }
+    }
+
+    private boolean areImageProportionsOk(BufferedImage image) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        double widthScaled = (double) width / 3;
+        double heightScaled = (double) height / 3;
+
+        double heightExact = (widthScaled  * 700) / 300;
+        double heightVicinity = ((widthScaled  * 700) / 300) * 0.1;
+
+        if(heightScaled >= heightExact - heightVicinity && heightScaled <= heightExact + heightVicinity) {
+            return true;
+        }
+        return false;
     }
 }
