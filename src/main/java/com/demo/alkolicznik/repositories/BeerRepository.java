feb243caee1e3f0219da3bc608c80ad569f41e34
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-17 15:54:47 +0200

    Remove commented code

--- src/main/java/com/demo/alkolicznik/repositories/BeerRepository.java
+++ src/main/java/com/demo/alkolicznik/repositories/BeerRepository.java
@@ -16,9 +16,6 @@ public interface BeerRepository extends CrudRepository<Beer, Long> {
     @Query(value = "SELECT * FROM beer b WHERE concat_ws(' ', brand, type) = ?1 AND volume = ?2", nativeQuery = true)
     Optional<Beer> findByFullnameAndVolume(String fullname, Double volume);
 
-//    @Query(value = "SELECT b.id FROM beer b WHERE concat_ws(' ', brand, type) = ?1 AND volume = ?2", nativeQuery = true)
-//    Long findIdByFullnameAndVolume(String fullname, Double volume);
-
     @Query(value = "SELECT count(*) > 0 FROM beer b WHERE concat_ws(' ', brand, type) = ?1", nativeQuery = true)
     boolean existsByFullname(String fullname);
 
