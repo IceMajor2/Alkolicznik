7285a516807310cfbb04f5915089e744a076f9a9
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-24 13:44:26 +0200

    Add logger and improve ImageKit resetting

--- src/test/java/com/demo/alkolicznik/config/ImageKitConfig.java
+++ src/test/java/com/demo/alkolicznik/config/ImageKitConfig.java
@@ -1,105 +1,141 @@
 package com.demo.alkolicznik.config;
 
-import com.demo.alkolicznik.utils.TestUtils;
-import io.imagekit.sdk.ImageKit;
-import io.imagekit.sdk.exceptions.*;
-import io.imagekit.sdk.models.BaseFile;
-import io.imagekit.sdk.models.FileCreateRequest;
-import io.imagekit.sdk.models.GetFileListRequest;
-import io.imagekit.sdk.models.results.ResultList;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.core.annotation.Order;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
+import io.imagekit.sdk.ImageKit;
+import io.imagekit.sdk.exceptions.BadRequestException;
+import io.imagekit.sdk.exceptions.ForbiddenException;
+import io.imagekit.sdk.exceptions.InternalServerException;
+import io.imagekit.sdk.exceptions.TooManyRequestsException;
+import io.imagekit.sdk.exceptions.UnauthorizedException;
+import io.imagekit.sdk.exceptions.UnknownException;
+import io.imagekit.sdk.models.BaseFile;
+import io.imagekit.sdk.models.FileCreateRequest;
+import io.imagekit.sdk.models.GetFileListRequest;
+import io.imagekit.sdk.models.results.Result;
+import io.imagekit.sdk.models.results.ResultList;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+import static com.demo.alkolicznik.utils.TestUtils.getRawPathToClassPathResource;
+
 @Configuration
 @Profile("image")
-@Order(1)
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class ImageKitConfig {
 
-    @Autowired
-    private ApplicationContext context;
+	private static String imageKitPath;
 
-    // TestUtils is autowired so that its beans are
-    // made sure to load before ImageKitConfig's
-    @Autowired
-    private TestUtils testUtils;
+	private ImageKit imageKit;
 
-    private ImageKit imageKit;
+	private static final Logger LOGGER = LoggerFactory.getLogger(ImageKitConfig.class);
 
-    private static final List<String> expectedBeerImages = List.of
-            ("tyskie-gronie-0.65.png", "zubr-0.5.png",
-                    "komes-porter-malinowy-0.33.jpg", "miloslaw-biale-0.5.jpg");
+	private static final List<String> expectedBeerImages = List.of
+			("tyskie-gronie-0.65.png", "zubr-0.5.png",
+					"komes-porter-malinowy-0.33.jpg", "miloslaw-biale-0.5.jpg");
 
-    @PostConstruct
-    public void init() throws ForbiddenException, TooManyRequestsException, InternalServerException, UnauthorizedException, BadRequestException, UnknownException, IOException, IllegalAccessException, InstantiationException {
-        setImageKit();
-        deletePostedByTestImages();
-        sendInitialImages();
-    }
+	@Autowired
+	public void setImageKitPath(String imageKitPath) {
+		ImageKitConfig.imageKitPath = imageKitPath;
+	}
 
-    private void deletePostedByTestImages() throws ForbiddenException, TooManyRequestsException, InternalServerException, UnauthorizedException, BadRequestException, UnknownException, IllegalAccessException, InstantiationException {
-        GetFileListRequest getFileListRequest = new GetFileListRequest();
-        getFileListRequest.setPath("/test/beer");
-        ResultList resultList = ImageKit.getInstance().getFileList(getFileListRequest);
+	@PostConstruct
+	public void init() throws ForbiddenException, TooManyRequestsException, InternalServerException, UnauthorizedException, BadRequestException, UnknownException, IOException, IllegalAccessException, InstantiationException {
+		LOGGER.info("Reloading ImageKit's directory");
+		setImageKit();
+		deletePostedByTestImages();
+		sendInitialImages();
+	}
 
-        for (BaseFile baseFile : resultList.getResults()) {
-            if (expectedBeerImages.contains(baseFile.getName())) {
-                continue;
-            }
-            this.imageKit.deleteFile(baseFile.getFileId());
-        }
-    }
+	private void deletePostedByTestImages() throws ForbiddenException, TooManyRequestsException, InternalServerException, UnauthorizedException, BadRequestException, UnknownException, IllegalAccessException, InstantiationException {
+		GetFileListRequest getFileListRequest = new GetFileListRequest();
+		getFileListRequest.setPath(imageKitPath);
+		ResultList resultList = this.imageKit.getFileList(getFileListRequest);
 
-    private void sendInitialImages() throws ForbiddenException, TooManyRequestsException, InternalServerException, UnauthorizedException, BadRequestException, UnknownException, IOException {
-        for (File file : new File(TestUtils.getRawPathToClassPathResource("/data_img/init_data")).listFiles()) {
-            byte[] bytes = Files.readAllBytes(file.toPath());
-            FileCreateRequest fileCreateRequest = new FileCreateRequest(bytes, file.getName());
-            fileCreateRequest.setUseUniqueFileName(false);
-            fileCreateRequest.setFolder("/test/beer");
-            this.imageKit.upload(fileCreateRequest);
-        }
-    }
+		for (BaseFile baseFile : resultList.getResults()) {
+			if (expectedBeerImages.contains(baseFile.getName())) {
+				LOGGER.info("'%s' was found. Not to be DELETED"
+						.formatted(baseFile.getName()));
+				continue;
+			}
+			LOGGER.info("Deleting '%s'... (ID: %s)"
+					.formatted(baseFile.getName(), baseFile.getFileId()));
+			this.imageKit.deleteFile(baseFile.getFileId());
+		}
+	}
 
-    private void setImageKit() {
-        this.imageKit = ImageKit.getInstance();
-        String endpoint = "https://ik.imagekit.io/icemajor";
-        String publicKey = "public_YpQHYFb3+OX4R5aHScftYE0H0N8=";
-        try {
-            this.imageKit.setConfig(new io.imagekit.sdk.config.Configuration(publicKey,
-                    Files.readAllLines(Paths.get("secure" + File.separator + "api_key.txt")).get(0),
-                    endpoint));
-        } catch (IOException e) {
-            throw new RuntimeException("Could not read secured file");
-        }
-    }
+	private void sendInitialImages() throws ForbiddenException, TooManyRequestsException, InternalServerException, UnauthorizedException, BadRequestException, UnknownException, IOException, IllegalAccessException, InstantiationException {
+		GetFileListRequest getFileListRequest = new GetFileListRequest();
+		getFileListRequest.setPath(imageKitPath);
 
-    static String extractFilenameFromUrl(String url) {
-        return url.substring(url.lastIndexOf('/') + 1);
-    }
+		List<String> remoteDir = this.imageKit.getFileList(getFileListRequest)
+				.getResults()
+				.stream()
+				.map(BaseFile::getName)
+				.toList();
+		File[] testDir =
+				new File(getRawPathToClassPathResource("/data_img/init_data")).listFiles();
 
-    static String getExternalId(String filename) {
-        GetFileListRequest getFileListRequest = new GetFileListRequest();
-        getFileListRequest.setPath("/test/beer");
-        ResultList resultList = null;
-        try {
-            resultList = ImageKit.getInstance().getFileList(getFileListRequest);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        for (BaseFile baseFile : resultList.getResults()) {
-            if (baseFile.getName().equals(filename)) {
-                return baseFile.getFileId();
-            }
-        }
-        return null;
-    }
+		for (File image : testDir) {
+			if (remoteDir.contains(image.getName())) {
+				LOGGER.info("'%s' was found. Not to be SENT"
+						.formatted(image.getName()));
+				continue;
+			}
+			byte[] bytes = Files.readAllBytes(image.toPath());
+			FileCreateRequest fileCreateRequest = new FileCreateRequest(bytes, image.getName());
+			fileCreateRequest.setUseUniqueFileName(false);
+			fileCreateRequest.setFolder(imageKitPath);
+			Result result = this.imageKit.upload(fileCreateRequest);
+			LOGGER.info("'%s' was successfully sent. (ID: %s)"
+					.formatted(result.getName(), result.getFileId()));
+		}
+	}
+
+	private void setImageKit() {
+		this.imageKit = ImageKit.getInstance();
+		String endpoint = "https://ik.imagekit.io/icemajor";
+		String publicKey = "public_YpQHYFb3+OX4R5aHScftYE0H0N8=";
+		try {
+			this.imageKit.setConfig(new io.imagekit.sdk.config.Configuration(publicKey,
+					Files.readAllLines(Paths.get("secure" + File.separator + "api_key.txt")).get(0),
+					endpoint));
+		}
+		catch (IOException e) {
+			throw new RuntimeException("Could not read secured file");
+		}
+	}
+
+	static String extractFilenameFromUrl(String url) {
+		return url.substring(url.lastIndexOf('/') + 1);
+	}
+
+	static String getExternalId(String filename) {
+		GetFileListRequest getFileListRequest = new GetFileListRequest();
+		getFileListRequest.setPath(imageKitPath);
+		ResultList resultList = null;
+		try {
+			resultList = ImageKit.getInstance().getFileList(getFileListRequest);
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		for (BaseFile baseFile : resultList.getResults()) {
+			if (baseFile.getName().equals(filename)) {
+				return baseFile.getFileId();
+			}
+		}
+		return null;
+	}
 }
