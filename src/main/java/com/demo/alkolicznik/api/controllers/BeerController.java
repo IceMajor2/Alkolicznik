45cec7513fdf41e864e07c359e54ac47b299d012
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-25 15:22:07 +0200

    Add some 'template' API documentation

--- src/main/java/com/demo/alkolicznik/api/controllers/BeerController.java
+++ src/main/java/com/demo/alkolicznik/api/controllers/BeerController.java
@@ -8,7 +8,12 @@ import com.demo.alkolicznik.dto.beer.BeerDeleteDTO;
 import com.demo.alkolicznik.dto.beer.BeerRequestDTO;
 import com.demo.alkolicznik.dto.beer.BeerResponseDTO;
 import com.demo.alkolicznik.dto.beer.BeerUpdateDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
@@ -27,72 +32,81 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api/beer")
+@Tag(name = "Beer Controller")
 public class BeerController {
 
-    private BeerService beerService;
+	private BeerService beerService;
 
-    public BeerController(BeerService beerService) {
-        this.beerService = beerService;
-    }
+	public BeerController(BeerService beerService) {
+		this.beerService = beerService;
+	}
 
-    @GetMapping("/{beer_id}")
-    public BeerResponseDTO get(@PathVariable("beer_id") Long id) {
-        return beerService.get(id);
-    }
+	@GetMapping("/{beer_id}")
+	public BeerResponseDTO get(@PathVariable("beer_id") Long id) {
+		return beerService.get(id);
+	}
 
-    @GetMapping(params = "city")
-    public List<BeerResponseDTO> getAllInCity(@RequestParam("city") String city) {
-        return beerService.getBeers(city);
-    }
+	@GetMapping(params = "city")
+	@Operation(summary = "Get a list of currently tracked beers",
+			description = "Average user is only enabled to get an array of beers from a "
+					+ "desired city. Accountants may retrieve all beers from database "
+					+ "simply by omitting the 'city' parameter.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+			@ApiResponse(responseCode = "404", description = "Not found - no such city")
+	})
+	public List<BeerResponseDTO> getAllInCity(@RequestParam("city") @Parameter(required = false) String city) {
+		return beerService.getBeers(city);
+	}
 
-    @GetMapping
-    @PreAuthorize("hasAnyAuthority('ADMIN', 'ACCOUNTANT')")
-    @SecurityRequirement(name = "Basic Authentication")
-    public List<BeerResponseDTO> getAll() {
-        return beerService.getBeers();
-    }
-
-    @PostMapping
-    // secured in SecurityConfig
-    @SecurityRequirement(name = "Basic Authentication")
-    public ResponseEntity<BeerResponseDTO> add(@RequestBody @Valid BeerRequestDTO beerRequestDTO) {
-        BeerResponseDTO savedDTO = beerService.add(beerRequestDTO);
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(savedDTO.getId())
-                .toUri();
-        return ResponseEntity
-                .created(location)
-                .body(savedDTO);
-    }
-
-    @PutMapping("/{beer_id}")
-    // secured in SecurityConfig
-    @SecurityRequirement(name = "Basic Authentication")
-    public BeerResponseDTO replace(@PathVariable("beer_id") Long beerId,
-                                                  @RequestBody @Valid BeerRequestDTO requestDTO) {
-        return beerService.replace(beerId, requestDTO);
-    }
-
-    @PatchMapping("/{beer_id}")
+	@GetMapping
+	@PreAuthorize("hasAnyAuthority('ADMIN', 'ACCOUNTANT')")
 	@SecurityRequirement(name = "Basic Authentication")
-    public BeerResponseDTO update(@PathVariable("beer_id") Long beerId,
-                                  @RequestBody @Valid BeerUpdateDTO updateDTO) {
-        return beerService.update(beerId, updateDTO);
-    }
+	public List<BeerResponseDTO> getAll() {
+		return beerService.getBeers();
+	}
 
-    @DeleteMapping("/{beer_id}")
-    @PreAuthorize("hasAnyAuthority('ADMIN', 'ACCOUNTANT')")
-    @SecurityRequirement(name = "Basic Authentication")
-    public BeerDeleteDTO delete(@PathVariable("beer_id") Long beerId) {
-        return beerService.delete(beerId);
-    }
+	@PostMapping
+	// secured in SecurityConfig
+	@SecurityRequirement(name = "Basic Authentication")
+	public ResponseEntity<BeerResponseDTO> add(@RequestBody @Valid BeerRequestDTO beerRequestDTO) {
+		BeerResponseDTO savedDTO = beerService.add(beerRequestDTO);
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(savedDTO.getId())
+				.toUri();
+		return ResponseEntity
+				.created(location)
+				.body(savedDTO);
+	}
 
-    @DeleteMapping
-    // secured in SecurityConfig
-    @SecurityRequirement(name = "Basic Authentication")
-    public BeerDeleteDTO delete(@RequestBody @Valid BeerRequestDTO requestDTO) {
-        return beerService.delete(requestDTO);
-    }
+	@PutMapping("/{beer_id}")
+	// secured in SecurityConfig
+	@SecurityRequirement(name = "Basic Authentication")
+	public BeerResponseDTO replace(@PathVariable("beer_id") Long beerId,
+			@RequestBody @Valid BeerRequestDTO requestDTO) {
+		return beerService.replace(beerId, requestDTO);
+	}
+
+	@PatchMapping("/{beer_id}")
+	@SecurityRequirement(name = "Basic Authentication")
+	public BeerResponseDTO update(@PathVariable("beer_id") Long beerId,
+			@RequestBody @Valid BeerUpdateDTO updateDTO) {
+		return beerService.update(beerId, updateDTO);
+	}
+
+	@DeleteMapping("/{beer_id}")
+	@PreAuthorize("hasAnyAuthority('ADMIN', 'ACCOUNTANT')")
+	@SecurityRequirement(name = "Basic Authentication")
+	public BeerDeleteDTO delete(@PathVariable("beer_id") Long beerId) {
+		return beerService.delete(beerId);
+	}
+
+	@DeleteMapping
+	// secured in SecurityConfig
+	@SecurityRequirement(name = "Basic Authentication")
+	public BeerDeleteDTO delete(@RequestBody @Valid BeerRequestDTO requestDTO) {
+		return beerService.delete(requestDTO);
+	}
 }
