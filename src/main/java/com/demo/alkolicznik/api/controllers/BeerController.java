b2cd3ae644a8ea840a28022181f724e6fd1e377f
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-25 17:28:52 +0200

    Write Swagger docs + write a todo

--- src/main/java/com/demo/alkolicznik/api/controllers/BeerController.java
+++ src/main/java/com/demo/alkolicznik/api/controllers/BeerController.java
@@ -9,7 +9,7 @@ import com.demo.alkolicznik.dto.beer.BeerRequestDTO;
 import com.demo.alkolicznik.dto.beer.BeerResponseDTO;
 import com.demo.alkolicznik.dto.beer.BeerUpdateDTO;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -42,6 +42,15 @@ public class BeerController {
 	}
 
 	@GetMapping("/{beer_id}")
+	@Operation(summary = "Get beer's details",
+			description = "Include id of beer you would like to see details of. "
+					+ "Details include: see response example below.")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "beer details retrieved"),
+			@ApiResponse(responseCode = "404", description = "resource not found - dummy response "
+					+ "(when unauthorized/unauthenticated user tries to fetch resources)", content = @Content),
+			@ApiResponse(responseCode = "404 (2)", description = "beer not found", content = @Content)
+	})
 	public BeerResponseDTO get(@PathVariable("beer_id") Long id) {
 		return beerService.get(id);
 	}
@@ -51,11 +60,13 @@ public class BeerController {
 			description = "Average user is only enabled to get an array of beers from a "
 					+ "desired city. Accountants may retrieve all beers from database "
 					+ "simply by omitting the 'city' parameter.")
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "Successfully retrieved"),
-			@ApiResponse(responseCode = "404", description = "Not found - no such city")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "beer list retrieved"),
+			@ApiResponse(responseCode = "404", description = "resource not found - dummy response"
+					+ "(when unauthorized/unauthenticated user tries to fetch resources)", content = @Content),
+			@ApiResponse(responseCode = "404 (2)", description = "city not found", content = @Content)
 	})
-	public List<BeerResponseDTO> getAllInCity(@RequestParam("city") @Parameter(required = false) String city) {
+	public List<BeerResponseDTO> getAllInCity(@RequestParam(value = "city", required = false) String city) {
 		return beerService.getBeers(city);
 	}
 
@@ -66,8 +77,16 @@ public class BeerController {
 		return beerService.getBeers();
 	}
 
+	@Operation(summary = "Add new beer",
+			description = "If you found a beer missing, feel free to add it")
+	@ApiResponses({
+			@ApiResponse(responseCode = "201", description = "beer successfully created"),
+			@ApiResponse(responseCode = "400", description = "provided data violates constraints", content = @Content),
+			@ApiResponse(responseCode = "404", description = "resource not found - dummy response"
+					+ "(when unauthorized/unauthenticated user tries to fetch resources)", content = @Content),
+			@ApiResponse(responseCode = "409", description = "such beer already exists", content = @Content)
+	})
 	@PostMapping
-	// secured in SecurityConfig
 	@SecurityRequirement(name = "Basic Authentication")
 	public ResponseEntity<BeerResponseDTO> add(@RequestBody @Valid BeerRequestDTO beerRequestDTO) {
 		BeerResponseDTO savedDTO = beerService.add(beerRequestDTO);
@@ -81,14 +100,40 @@ public class BeerController {
 				.body(savedDTO);
 	}
 
+	@Operation(summary = "Replace beer",
+			description = "Here you can replace an already existing beer with new one. "
+					+ "Features? You can keep the id! How cool is that?")
+	@ApiResponses({
+			@ApiResponse(responseCode = "201", description = "beer successfully replaced"),
+			@ApiResponse(responseCode = "200", description = "replacement is the same as original entity - nothing happens", content = @Content),
+			@ApiResponse(responseCode = "400", description = "provided data violates constraints", content = @Content),
+			@ApiResponse(responseCode = "404", description = "resource not found - dummy response"
+					+ "(when unauthorized/unauthenticated user tries to fetch resources)", content = @Content),
+			@ApiResponse(responseCode = "404 (2)", description = "beer of provided id was not found", content = @Content),
+			@ApiResponse(responseCode = "409", description = "such beer already exists", content = @Content)
+	})
 	@PutMapping("/{beer_id}")
-	// secured in SecurityConfig
 	@SecurityRequirement(name = "Basic Authentication")
 	public BeerResponseDTO replace(@PathVariable("beer_id") Long beerId,
 			@RequestBody @Valid BeerRequestDTO requestDTO) {
 		return beerService.replace(beerId, requestDTO);
 	}
 
+	@Operation(summary = "Update beer",
+			description = "If you are interested in updating just one or two - or even more - individual "
+					+ "fields, you've come to the right place. However, please be cautious "
+					+ "as updating beer's brand and/or type will also remove its image "
+					+ "(if there had already been one) and remove it from all the stores")
+	@ApiResponses({
+			@ApiResponse(responseCode = "201", description = "beer successfully updated"),
+			@ApiResponse(responseCode = "200", description = "replacement is the same as original entity - nothing happens", content = @Content),
+			@ApiResponse(responseCode = "400", description = "provided data violates constraints", content = @Content),
+			@ApiResponse(responseCode = "400 (2)", description = "there was not one single property to update specified", content = @Content),
+			@ApiResponse(responseCode = "404", description = "resource not found - dummy response"
+					+ "(when unauthorized/unauthenticated user tries to fetch resources)", content = @Content),
+			@ApiResponse(responseCode = "404 (2)", description = "beer of provided id was not found"),
+			@ApiResponse(responseCode = "409", description = "such beer already exists", content = @Content)
+	})
 	@PatchMapping("/{beer_id}")
 	@SecurityRequirement(name = "Basic Authentication")
 	public BeerResponseDTO update(@PathVariable("beer_id") Long beerId,
@@ -96,6 +141,16 @@ public class BeerController {
 		return beerService.update(beerId, updateDTO);
 	}
 
+	@Operation(summary = "Delete beer by id",
+			description = "Are you tired of selling a certain beer? You can't make "
+					+ "enough money or working with the provider is a pain in the ass? "
+					+ "Say no more! Just give me an ID...")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "beer successfully deleted"),
+			@ApiResponse(responseCode = "404", description = "resource not found - dummy response"
+					+ "(when unauthorized/unauthenticated user tries to fetch resources)", content = @Content),
+			@ApiResponse(responseCode = "404 (2)", description = "beer of provided id was not found", content = @Content)
+	})
 	@DeleteMapping("/{beer_id}")
 	@PreAuthorize("hasAnyAuthority('ADMIN', 'ACCOUNTANT')")
 	@SecurityRequirement(name = "Basic Authentication")
@@ -103,9 +158,20 @@ public class BeerController {
 		return beerService.delete(beerId);
 	}
 
+	@Operation(summary = "Delete beer by properties",
+			description = "You can't sell a beer and - even worse - you can't get its ID? "
+					+ "Not a problem! Try to describe it just as you'd create it de novo")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "beer successfully deleted", content = @Content),
+			@ApiResponse(responseCode = "400", description = "provided data violates constraints", content = @Content),
+			@ApiResponse(responseCode = "404", description = "resource not found - dummy response"
+					+ "(when unauthorized/unauthenticated user tries to fetch resources)", content = @Content),
+			@ApiResponse(responseCode = "404 (2)", description = "beer of provided details was not found", content = @Content)
+	})
 	@DeleteMapping
-	// secured in SecurityConfig
 	@SecurityRequirement(name = "Basic Authentication")
+	// TODO: change BeerRequestDTO to new DTO because 'BeerRequestDTO' contains
+	//		 unnecessary image path
 	public BeerDeleteDTO delete(@RequestBody @Valid BeerRequestDTO requestDTO) {
 		return beerService.delete(requestDTO);
 	}
