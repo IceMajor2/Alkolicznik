d2584f86ff9f00789a05b16ff836621956225034
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-26 09:39:43 +0200

    Change naming conventions + add 'deleteAllPrices()' method

--- src/main/java/com/demo/alkolicznik/models/Store.java
+++ src/main/java/com/demo/alkolicznik/models/Store.java
@@ -1,91 +1,116 @@
 package com.demo.alkolicznik.models;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Optional;
+import java.util.Set;
+
+import javax.money.Monetary;
+import javax.money.MonetaryAmount;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.money.Monetary;
-import javax.money.MonetaryAmount;
-import java.util.*;
-
 @Table(name = "store")
 @Entity(name = "Store")
-@JsonPropertyOrder({"id", "name", "city", "street"})
+@JsonPropertyOrder({ "id", "name", "city", "street" })
 @NoArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode
 public class Store {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private String name;
-    private String city;
-    private String street;
+	private String name;
 
-    @OneToMany(mappedBy = "store",
-            cascade = {CascadeType.ALL, CascadeType.MERGE},
-            orphanRemoval = true,
-            fetch = FetchType.LAZY)
-    @JsonIgnore
-    @EqualsAndHashCode.Exclude
-    private Set<BeerPrice> prices = new HashSet<>();
+	private String city;
 
-    public void addBeer(Beer beer, double price) {
-        addBeer(beer, Monetary.getDefaultAmountFactory()
-                .setCurrency("PLN").setNumber(price).create());
-    }
+	private String street;
 
-    public void addBeer(Beer beer, MonetaryAmount price) {
-        BeerPrice beerPrice = new BeerPrice(this, beer, price);
-        this.prices.add(beerPrice);
-        beer.getPrices().add(beerPrice);
-    }
+	@OneToMany(mappedBy = "store",
+			cascade = { CascadeType.ALL, CascadeType.MERGE },
+			orphanRemoval = true,
+			fetch = FetchType.LAZY)
+	@JsonIgnore
+	@EqualsAndHashCode.Exclude
+	private Set<BeerPrice> prices = new HashSet<>();
 
-    public BeerPrice removeBeer(Beer beer) {
-        for (Iterator<BeerPrice> iterator = prices.iterator();
-             iterator.hasNext(); ) {
-            BeerPrice beerPrice = iterator.next();
+	public void saveBeer(Beer beer, double price) {
+		saveBeer(beer, Monetary.getDefaultAmountFactory()
+				.setCurrency("PLN").setNumber(price).create());
+	}
 
-            if (beerPrice.getStore().equals(this) &&
-                    beerPrice.getBeer().equals(beer)) {
-                BeerPrice copy = beerPrice.clone();
-                iterator.remove();
-                beerPrice.getBeer().getPrices().remove(beerPrice);
-                beerPrice.setStore(null);
-                beerPrice.setBeer(null);
-                return copy;
-            }
-        }
-        return null;
-    }
+	public void saveBeer(Beer beer, MonetaryAmount price) {
+		BeerPrice beerPrice = new BeerPrice(this, beer, price);
+		this.prices.add(beerPrice);
+		beer.getPrices().add(beerPrice);
+	}
 
-    public Optional<BeerPrice> getBeer(String beerFullname) {
-        for (BeerPrice beer : prices) {
-            if (beer.getBeer().getFullName().equals(beerFullname)) {
-                return Optional.of(beer);
-            }
-        }
-        return Optional.empty();
-    }
+	public BeerPrice deleteBeer(Beer beer) {
+		for (Iterator<BeerPrice> iterator = prices.iterator();
+			 iterator.hasNext(); ) {
+			BeerPrice beerPrice = iterator.next();
 
-    public Optional<BeerPrice> getBeer(Long beerId) {
-        for (BeerPrice beer : prices) {
-            if (beer.getBeer().getId() == beerId) {
-                return Optional.of(beer);
-            }
-        }
-        return Optional.empty();
-    }
+			if (beerPrice.getStore().equals(this) &&
+					beerPrice.getBeer().equals(beer)) {
+				BeerPrice copy = beerPrice.clone();
+				iterator.remove();
+				beerPrice.getBeer().getPrices().remove(beerPrice);
+				beerPrice.setStore(null);
+				beerPrice.setBeer(null);
+				return copy;
+			}
+		}
+		return null;
+	}
 
-    @Override
-    public String toString() {
-        return this.name + ", " + this.city + " " + this.street + " (" + this.id + ")";
-    }
+	public boolean deleteAllPrices() {
+		for (Iterator<BeerPrice> iterator = prices.iterator();
+			 iterator.hasNext(); ) {
+			BeerPrice beerPrice = iterator.next();
+			iterator.remove();
+			beerPrice.getStore().getPrices().remove(beerPrice);
+			beerPrice.setStore(null);
+			beerPrice.setBeer(null);
+		}
+		return true;
+	}
+
+	public Optional<BeerPrice> findBeer(String beerFullname) {
+		for (BeerPrice beer : prices) {
+			if (beer.getBeer().getFullName().equals(beerFullname)) {
+				return Optional.of(beer);
+			}
+		}
+		return Optional.empty();
+	}
+
+	public Optional<BeerPrice> findBeer(Long beerId) {
+		for (BeerPrice beer : prices) {
+			if (beer.getBeer().getId() == beerId) {
+				return Optional.of(beer);
+			}
+		}
+		return Optional.empty();
+	}
+
+	@Override
+	public String toString() {
+		return this.name + ", " + this.city + " " + this.street + " (" + this.id + ")";
+	}
 }
