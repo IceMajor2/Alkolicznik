f87c3381a926e82bd0d945f7627d46c59f54c0fe
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-26 09:18:08 +0200

    Remove comments

--- src/main/java/com/demo/alkolicznik/api/controllers/StoreController.java
+++ src/main/java/com/demo/alkolicznik/api/controllers/StoreController.java
@@ -1,20 +1,29 @@
 package com.demo.alkolicznik.api.controllers;
 
+import java.net.URI;
+import java.util.List;
+
 import com.demo.alkolicznik.api.services.StoreService;
 import com.demo.alkolicznik.dto.store.StoreDeleteDTO;
-import com.demo.alkolicznik.dto.store.StoreUpdateDTO;
 import com.demo.alkolicznik.dto.store.StoreRequestDTO;
 import com.demo.alkolicznik.dto.store.StoreResponseDTO;
+import com.demo.alkolicznik.dto.store.StoreUpdateDTO;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/store")
@@ -44,7 +53,6 @@ public class StoreController {
     }
 
     @PostMapping
-    // secured in SecurityConfig
     @SecurityRequirement(name = "Basic Authentication")
     public ResponseEntity<StoreResponseDTO> add(@RequestBody @Valid StoreRequestDTO storeRequestDTO) {
         StoreResponseDTO saved = storeService.add(storeRequestDTO);
@@ -59,7 +67,6 @@ public class StoreController {
     }
 
     @PutMapping("/{store_id}")
-    // secured in SecurityConfig
     @SecurityRequirement(name = "Basic Authentication")
     public ResponseEntity<StoreResponseDTO> update(@PathVariable("store_id") Long storeId,
                                                    @RequestBody @Valid StoreUpdateDTO updateDTO) {
