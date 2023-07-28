f9add2bfad519221352339a62c47adb30a1c8777
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-28 10:02:18 +0200

    Add patch mapping

--- src/main/java/com/demo/alkolicznik/api/controllers/StoreController.java
+++ src/main/java/com/demo/alkolicznik/api/controllers/StoreController.java
@@ -11,11 +11,11 @@ import com.demo.alkolicznik.dto.store.StoreUpdateDTO;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -29,57 +29,62 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 @RequestMapping("/api/store")
 public class StoreController {
 
-    private StoreService storeService;
+	private StoreService storeService;
 
-    public StoreController(StoreService storeService) {
-        this.storeService = storeService;
-    }
+	public StoreController(StoreService storeService) {
+		this.storeService = storeService;
+	}
 
-    @GetMapping("/{store_id}")
-    public StoreResponseDTO get(@PathVariable("store_id") Long id) {
-        return storeService.get(id);
-    }
+	@GetMapping("/{store_id}")
+	public StoreResponseDTO get(@PathVariable("store_id") Long id) {
+		return storeService.get(id);
+	}
 
-    @GetMapping(params = "city")
-    public List<StoreResponseDTO> getAllInCity(@RequestParam("city") String city) {
-        return storeService.getStores(city);
-    }
+	@GetMapping(params = "city")
+	public List<StoreResponseDTO> getAllInCity(@RequestParam("city") String city) {
+		return storeService.getStores(city);
+	}
 
-    @GetMapping
-    @PreAuthorize("hasAnyAuthority('ADMIN', 'ACCOUNTANT')")
-    @SecurityRequirement(name = "Basic Authentication")
-    public List<StoreResponseDTO> getAll() {
-        return storeService.getStores();
-    }
+	@GetMapping
+	@PreAuthorize("hasAnyAuthority('ADMIN', 'ACCOUNTANT')")
+	@SecurityRequirement(name = "Basic Authentication")
+	public List<StoreResponseDTO> getAll() {
+		return storeService.getStores();
+	}
 
-    @PostMapping
-    @SecurityRequirement(name = "Basic Authentication")
-    public ResponseEntity<StoreResponseDTO> add(@RequestBody @Valid StoreRequestDTO storeRequestDTO) {
-        StoreResponseDTO saved = storeService.add(storeRequestDTO);
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(saved.getId())
-                .toUri();
-        return ResponseEntity
-                .created(location)
-                .body(saved);
-    }
+	@PostMapping
+	@SecurityRequirement(name = "Basic Authentication")
+	public ResponseEntity<StoreResponseDTO> add(@RequestBody @Valid StoreRequestDTO storeRequestDTO) {
+		StoreResponseDTO saved = storeService.add(storeRequestDTO);
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(saved.getId())
+				.toUri();
+		return ResponseEntity
+				.created(location)
+				.body(saved);
+	}
 
-    @PutMapping("/{store_id}")
-    @SecurityRequirement(name = "Basic Authentication")
-    public ResponseEntity<StoreResponseDTO> update(@PathVariable("store_id") Long storeId,
-                                                   @RequestBody @Valid StoreUpdateDTO updateDTO) {
-        return ResponseEntity
-                .status(HttpStatus.NO_CONTENT)
-                .body(storeService.update(storeId, updateDTO));
-    }
+	@PutMapping("/{store_id}")
+	@SecurityRequirement(name = "Basic Authentication")
+	public StoreResponseDTO replace(@PathVariable("store_id") Long storeId,
+			@RequestBody @Valid StoreRequestDTO requestDTO) {
+		return storeService.replace(storeId, requestDTO);
+	}
 
-    @DeleteMapping("/{store_id}")
-    @PreAuthorize("hasAnyAuthority('ADMIN', 'ACCOUNTANT')")
-    @SecurityRequirement(name = "Basic Authentication")
-    public StoreDeleteDTO delete(@PathVariable("store_id") Long storeId) {
-        return storeService.delete(storeId);
-    }
+	@PatchMapping("/{store_id}")
+	@SecurityRequirement(name = "Basic Authentication")
+	public StoreResponseDTO update(@PathVariable("store_id") Long storeId,
+			@RequestBody @Valid StoreUpdateDTO updateDTO) {
+		return storeService.update(storeId, updateDTO);
+	}
+
+	@DeleteMapping("/{store_id}")
+	@PreAuthorize("hasAnyAuthority('ADMIN', 'ACCOUNTANT')")
+	@SecurityRequirement(name = "Basic Authentication")
+	public StoreDeleteDTO delete(@PathVariable("store_id") Long storeId) {
+		return storeService.delete(storeId);
+	}
 
 }
--- src/main/java/com/demo/alkolicznik/api/services/StoreService.java
+++ src/main/java/com/demo/alkolicznik/api/services/StoreService.java
@@ -1,91 +1,133 @@
 package com.demo.alkolicznik.api.services;
 
+import java.util.List;
+
 import com.demo.alkolicznik.dto.store.StoreDeleteDTO;
-import com.demo.alkolicznik.dto.store.StoreUpdateDTO;
 import com.demo.alkolicznik.dto.store.StoreRequestDTO;
 import com.demo.alkolicznik.dto.store.StoreResponseDTO;
-import com.demo.alkolicznik.exceptions.classes.*;
+import com.demo.alkolicznik.dto.store.StoreUpdateDTO;
+import com.demo.alkolicznik.exceptions.classes.NoSuchCityException;
+import com.demo.alkolicznik.exceptions.classes.ObjectsAreEqualException;
+import com.demo.alkolicznik.exceptions.classes.PropertiesMissingException;
+import com.demo.alkolicznik.exceptions.classes.StoreAlreadyExistsException;
+import com.demo.alkolicznik.exceptions.classes.StoreNotFoundException;
 import com.demo.alkolicznik.models.Store;
 import com.demo.alkolicznik.repositories.StoreRepository;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
+import org.springframework.stereotype.Service;
 
 @Service
 public class StoreService {
 
-    private StoreRepository storeRepository;
+	private StoreRepository storeRepository;
 
-    public StoreService(StoreRepository storeRepository) {
-        this.storeRepository = storeRepository;
-    }
+	public StoreService(StoreRepository storeRepository) {
+		this.storeRepository = storeRepository;
+	}
 
-    public List<StoreResponseDTO> getStores(String city) {
-        if (!storeRepository.existsByCity(city)) {
-            throw new NoSuchCityException(city);
-        }
-        return this.mapToDto(storeRepository.findAllByCityOrderByIdAsc(city));
-    }
+	public List<StoreResponseDTO> getStores(String city) {
+		if (!storeRepository.existsByCity(city)) {
+			throw new NoSuchCityException(city);
+		}
+		return this.mapToDto(storeRepository.findAllByCityOrderByIdAsc(city));
+	}
 
-    public List<StoreResponseDTO> getStores() {
-        return this.mapToDto(storeRepository.findAllByOrderByIdAsc());
-    }
+	public List<StoreResponseDTO> getStores() {
+		return this.mapToDto(storeRepository.findAllByOrderByIdAsc());
+	}
 
-    public StoreResponseDTO add(StoreRequestDTO storeRequestDTO) {
-        Store store = storeRequestDTO.convertToModel();
-        if (storeRepository.existsByNameAndCityAndStreet(store.getName(), store.getCity(), store.getStreet())) {
-            throw new StoreAlreadyExistsException();
-        }
-        return new StoreResponseDTO(storeRepository.save(store));
-    }
+	public StoreResponseDTO add(StoreRequestDTO storeRequestDTO) {
+		Store store = storeRequestDTO.convertToModel();
+		if (storeRepository.existsByNameAndCityAndStreet(store.getName(), store.getCity(), store.getStreet())) {
+			throw new StoreAlreadyExistsException();
+		}
+		return new StoreResponseDTO(storeRepository.save(store));
+	}
 
-    public StoreResponseDTO get(Long storeId) {
-        return new StoreResponseDTO(storeRepository.findById(storeId).orElseThrow(() ->
-                new StoreNotFoundException(storeId))
-        );
-    }
+	public StoreResponseDTO get(Long storeId) {
+		return new StoreResponseDTO(storeRepository.findById(storeId).orElseThrow(() ->
+				new StoreNotFoundException(storeId))
+		);
+	}
 
-    public StoreResponseDTO update(Long storeId, StoreUpdateDTO updateDTO) {
-        if (updateDTO.propertiesMissing()) {
-            throw new PropertiesMissingException();
-        }
-        Store store = storeRepository.findById(storeId).orElseThrow(() ->
-                new StoreNotFoundException(storeId));
-        if (!updateDTO.anythingToUpdate(store)) {
-            throw new ObjectsAreEqualException();
-        }
-        String updatedName = updateDTO.getName();
-        String updatedCity = updateDTO.getCity();
-        String updatedStreet = updateDTO.getStreet();
-        if (updatedName != null) {
-            store.setName(updatedName);
-        }
-        if (updatedCity != null) {
-            store.setCity(updatedCity);
-        }
-        if (updatedStreet != null) {
-            store.setStreet(updatedStreet);
-        }
-        return new StoreResponseDTO(storeRepository.save(store));
-    }
+	public StoreResponseDTO replace(Long storeId, StoreRequestDTO requestDTO) {
+		Store toOverwrite = checkForPutConditions(storeId, requestDTO);
+		Store newStore = requestDTO.convertToModel();
 
-    public StoreDeleteDTO delete(Long storeId) {
-        Store toDelete = storeRepository.findById(storeId).orElseThrow(() ->
-                new StoreNotFoundException(storeId));
-        storeRepository.delete(toDelete);
-        return new StoreDeleteDTO(toDelete);
-    }
+		Store overwritten = updateFieldsOnPut(toOverwrite, newStore);
 
-    public StoreDeleteDTO delete(StoreRequestDTO store) {
-        Store toDelete = storeRepository.findByStoreRequest(store)
-                .orElseThrow(() -> new StoreNotFoundException(store.getName(), store.getCity(), store.getStreet()));
-        storeRepository.delete(toDelete);
-        return new StoreDeleteDTO(toDelete);
-    }
+		// for each PUT request all the previous
+		// beer prices in this store MUST be deleted
+		overwritten.deleteAllPrices();
+		return new StoreResponseDTO(storeRepository.save(toOverwrite));
+	}
 
-    private List<StoreResponseDTO> mapToDto(List<Store> stores) {
-        return stores.stream()
-                .map(StoreResponseDTO::new)
-                .toList();
-    }
+	private Store updateFieldsOnPut(Store toOverwrite, Store newStore) {
+		toOverwrite.setName(newStore.getName());
+		toOverwrite.setCity(newStore.getCity());
+		toOverwrite.setStreet(newStore.getStreet());
+		return toOverwrite;
+	}
+
+	private Store checkForPutConditions(Long storeId, StoreRequestDTO requestDTO) {
+		return checkForUpdateConditions(storeId, StoreUpdateDTO.convertFromRequest(requestDTO));
+	}
+
+	private Store checkForUpdateConditions(Long storeId, StoreUpdateDTO updateDTO) {
+		Store store = storeRepository.findById(storeId).orElseThrow(
+				() -> new StoreNotFoundException(storeId)
+		);
+		if (!updateDTO.anythingToUpdate(store)) {
+			throw new ObjectsAreEqualException();
+		}
+		Store converted = updateDTO.convertToModel();
+		if (converted != null && storeRepository.exists(converted)) {
+			throw new StoreAlreadyExistsException();
+		}
+		return store;
+	}
+
+	public StoreResponseDTO update(Long storeId, StoreUpdateDTO updateDTO) {
+		if (updateDTO.propertiesMissing()) {
+			throw new PropertiesMissingException();
+		}
+		Store store = storeRepository.findById(storeId).orElseThrow(() ->
+				new StoreNotFoundException(storeId));
+		if (!updateDTO.anythingToUpdate(store)) {
+			throw new ObjectsAreEqualException();
+		}
+		String updatedName = updateDTO.getName();
+		String updatedCity = updateDTO.getCity();
+		String updatedStreet = updateDTO.getStreet();
+		if (updatedName != null) {
+			store.setName(updatedName);
+		}
+		if (updatedCity != null) {
+			store.setCity(updatedCity);
+		}
+		if (updatedStreet != null) {
+			store.setStreet(updatedStreet);
+		}
+		return new StoreResponseDTO(storeRepository.save(store));
+	}
+
+	public StoreDeleteDTO delete(Long storeId) {
+		Store toDelete = storeRepository.findById(storeId).orElseThrow(() ->
+				new StoreNotFoundException(storeId));
+		storeRepository.delete(toDelete);
+		return new StoreDeleteDTO(toDelete);
+	}
+
+	public StoreDeleteDTO delete(StoreRequestDTO store) {
+		Store toDelete = storeRepository.findByStoreRequest(store)
+				.orElseThrow(() -> new StoreNotFoundException(store.getName(), store.getCity(), store.getStreet()));
+		storeRepository.delete(toDelete);
+		return new StoreDeleteDTO(toDelete);
+	}
+
+	private List<StoreResponseDTO> mapToDto(List<Store> stores) {
+		return stores.stream()
+				.map(StoreResponseDTO::new)
+				.toList();
+	}
 }
