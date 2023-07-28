bda04ba9ecac4c50ad40319de92133d787c0e453
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-28 11:18:59 +0200

    Add 2 new beers to test database

--- src/test/resources/data_sql/beer-data.sql
+++ src/test/resources/data_sql/beer-data.sql
@@ -5,3 +5,5 @@ INSERT INTO beer (ID, BRAND) VALUES (4, 'Zubr');
 INSERT INTO beer (ID, BRAND, TYPE, VOLUME) VALUES (5, 'Komes', 'Porter Malinowy', 0.33);
 INSERT INTO beer (ID, BRAND, TYPE) VALUES (6, 'Miloslaw', 'Biale');
 INSERT INTO beer (ID, BRAND) VALUES (7, 'Guinness');
+INSERT INTO beer (ID, BRAND, TYPE) VALUES (8, 'Perla', 'Miodowa');
+INSERT INTO beer (ID, BRAND, TYPE, VOLUME) VALUES (9, 'Perla', 'Chmielowa Pils', 0.33);
