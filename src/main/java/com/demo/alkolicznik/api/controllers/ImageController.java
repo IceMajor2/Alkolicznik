8ff1b16cde1f66f1010a627d8c0a2042ce977e56
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-01 10:12:28 +0200

    Remove an endpoint

--- src/main/java/com/demo/alkolicznik/api/controllers/ImageController.java
+++ src/main/java/com/demo/alkolicznik/api/controllers/ImageController.java
@@ -2,9 +2,11 @@ package com.demo.alkolicznik.api.controllers;
 
 import com.demo.alkolicznik.api.services.ImageService;
 import com.demo.alkolicznik.dto.image.ImageModelResponseDTO;
-import com.demo.alkolicznik.models.ImageModel;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
@@ -19,12 +21,5 @@ public class ImageController {
     @GetMapping("/beer/{beer_id}/image")
     public ImageModelResponseDTO get(@PathVariable("beer_id") Long beerId) {
         return imageService.getBeerImage(beerId);
-    }
-
-    @PostMapping("/beer/image/{beer_id}")
-    @PreAuthorize("hasAnyAuthority('ADMIN', 'ACCOUNTANT')")
-    public ImageModel addBeerImageParam(@PathVariable("beer_id") Long beerId,
-                                        @RequestParam("path") String path) {
-        return null;
     }
 }
