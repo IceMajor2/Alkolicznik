51a7fd7fdb5c65b09f67fefc8cbfca07ec0a9af8
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-02 08:38:57 +0200

    Nest test cases of anonymous user

--- src/test/java/com/demo/alkolicznik/security/SecuredEndpointTests.java
+++ src/test/java/com/demo/alkolicznik/security/SecuredEndpointTests.java
@@ -6,8 +6,10 @@ import com.demo.alkolicznik.dto.beer.BeerRequestDTO;
 import com.demo.alkolicznik.dto.beer.BeerUpdateDTO;
 import org.junit.jupiter.api.ClassOrderer;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.TestClassOrder;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -36,125 +38,130 @@ public class SecuredEndpointTests {
 	@TestClassOrder(ClassOrderer.Random.class)
 	class BeerController {
 
-		@ParameterizedTest
-		@CsvSource({
-				"/api/beer"
-		})
-		@DisplayName("[ANON]: restricted GET endpoints")
-		public void anonRestrictedGetEndpointsTest(String endpoint) {
-			var response = getRequest(endpoint);
-			String actualJson = response.getBody();
+		@Nested
+		@TestMethodOrder(MethodOrderer.Random.class)
+		class Anonymous {
 
-			assertIsError(actualJson,
-					HttpStatus.NOT_FOUND,
-					"Resource not found",
-					endpoint);
-		}
+			@ParameterizedTest
+			@CsvSource({
+					"/api/beer"
+			})
+			@DisplayName("[ANON]: restricted GET endpoints")
+			public void anonRestrictedGetEndpointsTest(String endpoint) {
+				var response = getRequest(endpoint);
+				String actualJson = response.getBody();
 
-		@ParameterizedTest
-		@CsvSource(value = {
-				"/api/beer, Ksiazece, IPA, null",
-				"/api/beer, '', null, -1.0"
-		}, nullValues = "null")
-		@DisplayName("[ANON]: restricted POST endpoints")
-		public void anonRestrictedPostEndpointsTest(String endpoint, String brand,
-				String type, Double volume) {
-			// given
-			BeerRequestDTO request = createBeerRequest(brand, type, volume);
-			// when
-			var response = postRequest(endpoint, request);
-			String actualJson = response.getBody();
+				assertIsError(actualJson,
+						HttpStatus.NOT_FOUND,
+						"Resource not found",
+						endpoint);
+			}
 
-			assertIsError(actualJson,
-					HttpStatus.NOT_FOUND,
-					"Resource not found",
-					endpoint);
-		}
+			@ParameterizedTest
+			@CsvSource(value = {
+					"/api/beer, Ksiazece, IPA, null",
+					"/api/beer, '', null, -1.0"
+			}, nullValues = "null")
+			@DisplayName("[ANON]: restricted POST endpoints")
+			public void anonRestrictedPostEndpointsTest(String endpoint, String brand,
+					String type, Double volume) {
+				// given
+				BeerRequestDTO request = createBeerRequest(brand, type, volume);
+				// when
+				var response = postRequest(endpoint, request);
+				String actualJson = response.getBody();
 
-		@ParameterizedTest
-		@CsvSource(value = {
-				"/api/beer/4, Manufaktura Piwna, Piwo na miodzie gryczanym, null",
-				"/api/beer/9218, null, IPA, 0",
-				"/api/beer/3, null, null, null"
-		}, nullValues = "null")
-		@DisplayName("[ANON]: restricted PUT endpoints")
-		public void anonRestrictedPutEndpointsTest(String endpoint, String brand,
-				String type, Double volume) {
-			// given
-			BeerRequestDTO request = createBeerRequest(brand, type, volume);
+				assertIsError(actualJson,
+						HttpStatus.NOT_FOUND,
+						"Resource not found",
+						endpoint);
+			}
 
-			// when
-			var response = putRequest(endpoint, request);
-			String actualJson = response.getBody();
+			@ParameterizedTest
+			@CsvSource(value = {
+					"/api/beer/4, Manufaktura Piwna, Piwo na miodzie gryczanym, null",
+					"/api/beer/9218, null, IPA, 0",
+					"/api/beer/3, null, null, null"
+			}, nullValues = "null")
+			@DisplayName("[ANON]: restricted PUT endpoints")
+			public void anonRestrictedPutEndpointsTest(String endpoint, String brand,
+					String type, Double volume) {
+				// given
+				BeerRequestDTO request = createBeerRequest(brand, type, volume);
 
-			assertIsError(actualJson,
-					HttpStatus.NOT_FOUND,
-					"Resource not found",
-					endpoint);
-		}
+				// when
+				var response = putRequest(endpoint, request);
+				String actualJson = response.getBody();
 
-		@ParameterizedTest
-		@CsvSource(value = {
-				"/api/beer/2, Miloslaw, null, 0.33",
-				"/api/beer/-5, Namyslow, null, -0.5",
-				"/api/beer/3, null, null, null"
-		}, nullValues = "null")
-		@DisplayName("[ANON]: restricted PATCH requests")
-		public void anonRestrictedPatchRequestsTest(String endpoint, String brand,
-				String type, Double volume) {
-			// given
-			BeerUpdateDTO request = createBeerUpdateRequest(brand, type, volume);
+				assertIsError(actualJson,
+						HttpStatus.NOT_FOUND,
+						"Resource not found",
+						endpoint);
+			}
 
-			// when
-			var response = patchRequest(endpoint, request);
-			String actualJson = response.getBody();
+			@ParameterizedTest
+			@CsvSource(value = {
+					"/api/beer/2, Miloslaw, null, 0.33",
+					"/api/beer/-5, Namyslow, null, -0.5",
+					"/api/beer/3, null, null, null"
+			}, nullValues = "null")
+			@DisplayName("[ANON]: restricted PATCH requests")
+			public void anonRestrictedPatchRequestsTest(String endpoint, String brand,
+					String type, Double volume) {
+				// given
+				BeerUpdateDTO request = createBeerUpdateRequest(brand, type, volume);
 
-			// then
-			assertIsError(actualJson,
-					HttpStatus.NOT_FOUND,
-					"Resource not found",
-					endpoint);
-		}
+				// when
+				var response = patchRequest(endpoint, request);
+				String actualJson = response.getBody();
 
-		@ParameterizedTest
-		@CsvSource(value = {
-				"/api/beer, Ksiazece, Zlote pszeniczne, null",
-				"/api/beer, null, Biale, 0",
-				"/api/beer, null, null, null"
-		}, nullValues = "null")
-		@DisplayName("[ANON]: restricted DELETE by object requests")
-		public void anonRestrictedDeleteByObjectRequestsTest(String endpoint, String brand,
-				String type, Double volume) {
-			// given
-			BeerDeleteRequestDTO request = createBeerDeleteRequest(brand, type, volume);
+				// then
+				assertIsError(actualJson,
+						HttpStatus.NOT_FOUND,
+						"Resource not found",
+						endpoint);
+			}
 
-			// when
-			var response = deleteRequest(endpoint, request);
-			String actualJson = response.getBody();
+			@ParameterizedTest
+			@CsvSource(value = {
+					"/api/beer, Ksiazece, Zlote pszeniczne, null",
+					"/api/beer, null, Biale, 0",
+					"/api/beer, null, null, null"
+			}, nullValues = "null")
+			@DisplayName("[ANON]: restricted DELETE by object requests")
+			public void anonRestrictedDeleteByObjectRequestsTest(String endpoint, String brand,
+					String type, Double volume) {
+				// given
+				BeerDeleteRequestDTO request = createBeerDeleteRequest(brand, type, volume);
 
-			// then
-			assertIsError(actualJson,
-					HttpStatus.NOT_FOUND,
-					"Resource not found",
-					endpoint);
-		}
+				// when
+				var response = deleteRequest(endpoint, request);
+				String actualJson = response.getBody();
 
-		@ParameterizedTest
-		@CsvSource(value = {
-				"/api/beer/1",
-				"/api/beer/-5"
-		})
-		@DisplayName("[ANON]: restricted DELETE by id requests")
-		public void anonRestrictedDeleteByIdRequestsTest(String endpoint) {
-			// when
-			var response = deleteRequest(endpoint);
-			String actualJson = response.getBody();
+				// then
+				assertIsError(actualJson,
+						HttpStatus.NOT_FOUND,
+						"Resource not found",
+						endpoint);
+			}
 
-			// then
-			assertIsError(actualJson,
-					HttpStatus.NOT_FOUND,
-					"Resource not found",
-					endpoint);
+			@ParameterizedTest
+			@CsvSource(value = {
+					"/api/beer/1",
+					"/api/beer/-5"
+			})
+			@DisplayName("[ANON]: restricted DELETE by id requests")
+			public void anonRestrictedDeleteByIdRequestsTest(String endpoint) {
+				// when
+				var response = deleteRequest(endpoint);
+				String actualJson = response.getBody();
+
+				// then
+				assertIsError(actualJson,
+						HttpStatus.NOT_FOUND,
+						"Resource not found",
+						endpoint);
+			}
 		}
 	}
 }
