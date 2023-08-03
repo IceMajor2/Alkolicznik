daf8c9aa901a7fceef0ae6f481d5a89dcc17c179
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-03 10:44:33 +0200

    Write tests checking secured beer endpoints when accessing by authorized users

--- src/test/java/com/demo/alkolicznik/security/SecuredEndpointTests.java
+++ src/test/java/com/demo/alkolicznik/security/SecuredEndpointTests.java
@@ -2,7 +2,6 @@ package com.demo.alkolicznik.security;
 
 import java.util.Map;
 
-import com.demo.alkolicznik.config.DisabledVaadinContext;
 import com.demo.alkolicznik.dto.beer.BeerDeleteRequestDTO;
 import com.demo.alkolicznik.dto.beer.BeerRequestDTO;
 import com.demo.alkolicznik.dto.beer.BeerUpdateDTO;
@@ -18,7 +17,6 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -36,10 +34,11 @@ import static com.demo.alkolicznik.utils.requests.SimpleRequests.getRequest;
 import static com.demo.alkolicznik.utils.requests.SimpleRequests.patchRequest;
 import static com.demo.alkolicznik.utils.requests.SimpleRequests.postRequest;
 import static com.demo.alkolicznik.utils.requests.SimpleRequests.putRequest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("main")
-@Import(DisabledVaadinContext.class)
+//@Import(DisabledVaadinContext.class)
 @TestClassOrder(ClassOrderer.Random.class)
 public class SecuredEndpointTests {
 
@@ -316,12 +315,87 @@ public class SecuredEndpointTests {
 
 
 			@ParameterizedTest
-			@CsvSource({
-					"/api/beer"
-			})
+			@ValueSource(strings = { "accountant", "admin" })
 			@DisplayName("[AUTHORIZED]: restricted GET endpoints")
-			public void authorizedRestrictedEndpointsTest(String endpoint) {
+			public void authorizedRestrictedEndpointsTest(String credentials) {
+				String endpoint = "/api/beer";
+				var response = getRequestAuth(credentials, credentials, endpoint);
+				assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+			}
 
+			@ParameterizedTest
+			@CsvSource({
+					"accountant, Lomza, Jasne, 0.33",
+					"admin, Manufaktura Piwna, Piwo na miodzie gryczanym, null"
+			})
+			@DisplayName("[AUTHORIZED]: restricted POST endpoints")
+			public void authorizedRestrictedPostEndpointsTest(String credentials,
+					String brand, String type, Double volume) {
+				String endpoint = "/api/beer";
+				BeerRequestDTO request = createBeerRequest(brand, type, volume);
+
+				var response = postRequestAuth(credentials, credentials, endpoint, request);
+				assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+			}
+
+			@ParameterizedTest
+			@CsvSource(value = {
+					"admin, 3, Manufaktura Piwna, Piwo na miodzie gryczanym, null",
+					"accountant, 5, null, IPA, 0.33"
+			}, nullValues = "null")
+			@DisplayName("[AUTHORIZED]: restricted PUT endpoints")
+			public void authorizedRestrictedPutEndpointsTest(String credentials, Long beerId,
+					String brand, String type, Double volume) {
+				String endpoint = "/api/beer" + beerId;
+				BeerRequestDTO request = createBeerRequest(brand, type, volume);
+
+				var response = postRequestAuth(credentials, credentials, endpoint, request);
+				assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+			}
+
+			@ParameterizedTest
+			@CsvSource(value = {
+					"admin, 2, Miloslaw, null, 0.33",
+					"accountant, 1, null, Porter, null"
+			}, nullValues = "null")
+			@DisplayName("[AUTHORIZED]: restricted PATCH requests")
+			public void authorizedRestrictedPatchRequestsTest(String credentials, Long beerId,
+					String brand, String type, Double volume) {
+				String endpoint = "/api/beer/" + beerId;
+				BeerUpdateDTO request = createBeerUpdateRequest(brand, type, volume);
+
+				var response = patchRequestAuth(credentials, credentials, endpoint, request);
+				assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+			}
+
+			@ParameterizedTest
+			@CsvSource(value = {
+					"admin, Ksiazece, Zlote pszeniczne, null",
+					"accountant, Zubr, null, 0.5"
+			}, nullValues = "null")
+			@DisplayName("[AUTHORIZED]: restricted DELETE by object requests")
+			public void authorizedRestrictedDeleteByObjectRequestsTest(String credentials,
+					String brand, String type, Double volume) {
+				// given
+				String endpoint = "/api/beer";
+				BeerDeleteRequestDTO request = createBeerDeleteRequest(brand, type, volume);
+
+				// when
+				var response = deleteRequestAuth(credentials, credentials, endpoint, request);
+				assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+			}
+
+			@ParameterizedTest
+			@CsvSource(value = {
+					"admin, 7",
+					"accountant, 4"
+			})
+			@DisplayName("[UNAUTHORIZED]: restricted DELETE by id requests")
+			public void unauthorizedRestrictedDeleteByIdRequestsTest(String credentials, Long beerId) {
+				// when
+				String endpoint = "/api/beer/" + beerId;
+				var response = deleteRequestAuth(credentials, credentials, endpoint);
+				assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 			}
 		}
 	}
