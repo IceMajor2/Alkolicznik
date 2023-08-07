b918c00a4e1d4f474e0f69384daef2437e4673a6
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-07 16:44:38 +0200

    Change store image sql data

--- src/test/resources/data_sql/image-data.sql
+++ src/test/resources/data_sql/image-data.sql
@@ -3,6 +3,6 @@ INSERT INTO beer_image (BEER_ID, URL) VALUES (4, 'https://ik.imagekit.io/icemajo
 INSERT INTO beer_image (BEER_ID, URL) VALUES (5, 'https://ik.imagekit.io/icemajor/tr:n-get_beer/test/beer/komes-porter-malinowy-0.33.jpg');
 INSERT INTO beer_image (BEER_ID, URL) VALUES (6, 'https://ik.imagekit.io/icemajor/tr:n-get_beer/test/beer/miloslaw-biale-0.5.jpg');
 
-INSERT INTO store_image (ID, STORE_NAME, URL) VALUES (1, 'Zabka', 'https://ik.imagekit.io/icemajor/test/store/tesco.png');
-INSERT INTO store_image (ID, STORE_NAME, URL) VALUES (2, 'Carrefour', 'https://ik.imagekit.io/icemajor/test/store/abc.png');
-INSERT INTO store_image (ID, STORE_NAME, URL) VALUES (3, 'Lidl', 'https://ik.imagekit.io/icemajor/test/store/lidl.png');
+INSERT INTO store_image (ID, STORE_NAME, URL) VALUES (1, 'Zabka', 'https://ik.imagekit.io/icemajor/tr:n-get_store/test/store/zabka.png');
+INSERT INTO store_image (ID, STORE_NAME, URL) VALUES (2, 'Carrefour', 'https://ik.imagekit.io/icemajor/tr:n-get_store/test/store/carrefour.png');
+INSERT INTO store_image (ID, STORE_NAME, URL) VALUES (3, 'Lidl', 'https://ik.imagekit.io/icemajor/tr:n-get_store/test/store/lidl.png');
