c1997558bf72448e66259f87467937523e3a6969
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-07 11:58:55 +0200

    Remove commented out methods

--- src/test/java/com/demo/alkolicznik/config/TestConfig.java
+++ src/test/java/com/demo/alkolicznik/config/TestConfig.java
@@ -69,22 +69,6 @@ public class TestConfig {
 		return beerImages;
 	}
 
-	private void updateBeerImageRemoteId(List<BeerImage> beerImages) {
-		LOGGER.info("Updating 'beer_image' table with remote IDs...");
-		for (var image : beerImages) {
-			String sql = "UPDATE beer_image SET remote_id = ? WHERE beer_id = ?";
-			jdbcTemplate.update(sql, image.getRemoteId(), image.getId());
-		}
-	}
-
-	private void updateStoreImageRemoteId(List<StoreImage> storeImages) {
-		LOGGER.info("Updating 'store_image' table with remote IDs...");
-		for(var image : storeImages) {
-			String sql = "UPDATE store_image SET remote_id = ? WHERE store_name = ?";
-			jdbcTemplate.update(sql, image.getRemoteId(), image.getStoreName());
-		}
-	}
-
 	@Bean
 	@DependsOn("stores")
 	public List<StoreImage> storeImages() {
@@ -123,28 +107,6 @@ public class TestConfig {
 		return "/test/beer";
 	}
 
-//	private void updateBeersWithPrices(List<BeerPrice> prices) {
-//		List<Beer> beers = (List<Beer>) context.getBean("beers");
-//		for (Beer beer : beers) {
-//			for (BeerPrice price : prices) {
-//				if (price.getBeer().equals(beer)) {
-//					beer.getPrices().add(price);
-//				}
-//			}
-//		}
-//	}
-//
-//	private void updateStoresWithPrices(List<BeerPrice> prices) {
-//		List<Store> stores = (List<Store>) context.getBean("stores");
-//		for (Store store : stores) {
-//			for (BeerPrice price : prices) {
-//				if (price.getStore().equals(store)) {
-//					store.saveBeer(price.getBeer(), price.getPrice());
-//				}
-//			}
-//		}
-//	}
-
 	@Bean("dataSource")
 	@ConditionalOnProperty(prefix = "enable.image", name = "database", havingValue = "true")
 	public DataSource dataSource2() {
@@ -182,115 +144,19 @@ public class TestConfig {
 		return randomPasswords;
 	}
 
-//	private RowMapper<Store> mapToStore() {
-//		return new RowMapper<Store>() {
-//			@Override
-//			public Store mapRow(ResultSet rs, int rowNum) throws SQLException {
-//				Store store = new Store();
-//				store.setId(rs.getLong("id"));
-//				store.setName(rs.getString("name"));
-//				store.setCity(rs.getString("city"));
-//				store.setStreet(rs.getString("street"));
-//
-//				String sql = "SELECT * FROM store_image WHERE store_name = ?";
-//				StoreImage image = jdbcTemplate.query(sql, new ResultSetExtractor<StoreImage>() {
-//					@Override
-//					public StoreImage extractData(ResultSet rs) throws SQLException, DataAccessException {
-//						// at beginning ResultSet is pointed *BEFORE* the 1st row
-//						if (!rs.next()) {
-//							return null;
-//						}
-//						StoreImage image = new StoreImage();
-//						Long imgId = rs.getLong("id");
-//						String url = rs.getString("url");
-//						String storeName = rs.getString("store_name");
-//						String remoteId = getRemoteId(extractFilenameFromUrl(url));
-//
-//						image.setId(imgId);
-//						image.setImageUrl(url);
-//						image.setStoreName(storeName);
-//						// update image table with ImageKit's remote id of this image
-//						String updateQuery = "UPDATE store_image SET remote_id = ? WHERE store_name = ?";
-//						jdbcTemplate.update(updateQuery, remoteId, storeName);
-//						image.setRemoteId(remoteId);
-//						return image;
-//					}
-//				}, store.getName());
-//				if (image != null) {
-//					store.setImage(image);
-//					image.getStores().add(store);
-//				}
-//				return store;
-//			}
-//		};
-//	}
+	private void updateBeerImageRemoteId(List<BeerImage> beerImages) {
+		LOGGER.info("Updating 'beer_image' table with remote IDs...");
+		for (var image : beerImages) {
+			String sql = "UPDATE beer_image SET remote_id = ? WHERE beer_id = ?";
+			jdbcTemplate.update(sql, image.getRemoteId(), image.getId());
+		}
+	}
 
-//	private StoreImage mapToStoreImage(ResultSet rs) {
-//
-//	}
-
-//	private RowMapper<Beer> mapToBeer() {
-//		return new RowMapper<Beer>() {
-//			@Override
-//			public Beer mapRow(ResultSet rs, int rowNum) throws SQLException {
-//				Beer beer = new Beer();
-//				beer.setId(rs.getLong("id"));
-//				beer.setBrand(rs.getString("brand"));
-//				beer.setType(rs.getString("type"));
-//				beer.setVolume(rs.getDouble("volume"));
-//				String sql = "SELECT * FROM beer_image i WHERE i.beer_id = " + beer.getId();
-//				BeerImage image = jdbcTemplate.query(sql, new ResultSetExtractor<BeerImage>() {
-//					@Override
-//					public BeerImage extractData(ResultSet rs) throws SQLException, DataAccessException {
-//						// at beginning ResultSet is pointed *BEFORE* the 1st row
-//						// due to the fact that this ResultSet may return at most 1 row (ID is UNIQUE)
-//						// we move the pointer to the next row with rs.next() command
-//						// if it returns false, then there's no image assigned to the entity
-//						// thus returning null below
-//						if (!rs.next()) {
-//							return null;
-//						}
-//						BeerImage image = new BeerImage();
-//						String url = rs.getString("url");
-//						Long beerId = rs.getLong("beer_id");
-//						String remoteId = getRemoteId(extractFilenameFromUrl(url));
-//
-//						image.setImageUrl(url);
-//						image.setId(beerId);
-//						// update image table with ImageKit's remote id of this image
-//						String updateQuery = "UPDATE beer_image SET remote_id = ? WHERE beer_id = ?";
-//						jdbcTemplate.update(updateQuery, remoteId, beerId);
-//						image.setRemoteId(remoteId);
-//						return image;
-//					}
-//				});
-//				if (image != null) {
-//					beer.setImage(image);
-//					image.setBeer(beer);
-//				}
-//				return beer;
-//			}
-//		};
-//	}
-
-//	private RowMapper<BeerPrice> mapToBeerPrice() {
-//		return new RowMapper<BeerPrice>() {
-//			@Override
-//			public BeerPrice mapRow(ResultSet rs, int rowNum) throws SQLException {
-//				BeerPrice beerPrice = new BeerPrice();
-//
-//				Beer beer = beers.get(rs.getLong("beer_id"));
-//				beerPrice.setBeer(beer);
-//
-//				Store store = stores.get(rs.getLong("store_id"));
-//				beerPrice.setStore(store);
-//
-//				MonetaryAmount price = Monetary.getDefaultAmountFactory()
-//						.setCurrency(rs.getString("price_currency"))
-//						.setNumber(rs.getBigDecimal("price_amount")).create();
-//				beerPrice.setPrice(price);
-//				return beerPrice;
-//			}
-//		};
-//	}
+	private void updateStoreImageRemoteId(List<StoreImage> storeImages) {
+		LOGGER.info("Updating 'store_image' table with remote IDs...");
+		for(var image : storeImages) {
+			String sql = "UPDATE store_image SET remote_id = ? WHERE store_name = ?";
+			jdbcTemplate.update(sql, image.getRemoteId(), image.getStoreName());
+		}
+	}
 }
