eed0999609e64520bce3120a2b6616b61f928dff
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-08 11:04:50 +0200

    Write new tests for GET endpoint

--- src/test/java/com/demo/alkolicznik/api/StoreImageTest.java
+++ src/test/java/com/demo/alkolicznik/api/StoreImageTest.java
@@ -18,10 +18,12 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 
+import static com.demo.alkolicznik.utils.CustomAssertions.assertIsError;
 import static com.demo.alkolicznik.utils.JsonUtils.createImageResponse;
 import static com.demo.alkolicznik.utils.JsonUtils.toJsonString;
 import static com.demo.alkolicznik.utils.JsonUtils.toModel;
@@ -64,6 +66,35 @@ class StoreImageTest {
 			String expectedJson = toJsonString(expected);
 			assertThat(actualJson).isEqualTo(expectedJson);
 			assertThat(actual).isEqualTo(expected);
+		}
+
+		@ParameterizedTest
+		@ValueSource(longs = {-349, 0, 129048})
+		@DisplayName("GET: '/api/store/{store_id}/image' [STORE_NOT_FOUND]")
+		public void shouldReturnNotFoundOnInvalidStoreIdTest(Long storeId) {
+			var getResponse = getRequest("/api/store/" + storeId + "/image");
+
+			String actualJson = getResponse.getBody();
+
+			assertIsError(actualJson,
+					HttpStatus.NOT_FOUND,
+					"Unable to find store of '%d' id".formatted(storeId),
+					"/api/store/" + storeId + "/image"
+			);
+		}
+
+		@ParameterizedTest
+		@ValueSource(longs = {2, 4, 5})
+		@DisplayName("GET: '/api/store/{store_id}/image' [NO_IMAGE]")
+		public void shouldReturnNotFoundOnBeerWithNoImageTest(Long storeId) {
+			var getResponse = getRequest("/api/store/" + storeId + "/image");
+
+			String actualJson = getResponse.getBody();
+
+			assertIsError(actualJson,
+					HttpStatus.NOT_FOUND,
+					"Unable to find image for this store",
+					"/api/store/" + storeId + "/image");
 		}
 	}
 }
