e340a17d759645636074da917caa9c7e69574af6
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-15 11:53:42 +0200

    Change endpoint locations + add a new one

--- src/main/java/com/demo/alkolicznik/api/controllers/ImageController.java
+++ src/main/java/com/demo/alkolicznik/api/controllers/ImageController.java
@@ -15,9 +15,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,6 +43,11 @@ public class ImageController {
 		return beerImageService.getAll();
 	}
 
+	@PostMapping("/beer/image")
+	public ImageModelResponseDTO addBeerImage(@RequestBody @Valid ImageRequestDTO request) {
+		return null;
+	}
+
 	@DeleteMapping("/beer/{beer_id}/image")
 	@PreAuthorize("hasAnyAuthority('ADMIN', 'ACCOUNTANT')")
 	public ImageDeleteDTO deleteBeerImage(@PathVariable("beer_id") Long beerId) {
@@ -60,13 +65,13 @@ public class ImageController {
 		return storeImageService.getAll();
 	}
 
-	@GetMapping("/image")
+	@GetMapping("/store/image")
 	public ImageModelResponseDTO getStoreImage(@RequestParam("store_name") String storeName) {
 		storeName = storeName.replace("%20", " ");
 		return storeImageService.get(storeName);
 	}
 
-	@PostMapping("/image")
+	@PostMapping("/store/image")
 	public ResponseEntity<ImageModelResponseDTO> addStoreImage(
 			@RequestParam("store_name") String storeName,
 			@RequestBody @Valid ImageRequestDTO imageRequestDTO) {
@@ -84,7 +89,7 @@ public class ImageController {
 				.body(response);
 	}
 
-	@PatchMapping("/image")
+	@PutMapping("/store/image")
 	public ImageModelResponseDTO updateStoreImage(
 			@RequestParam("store_name") String storeName,
 			@RequestBody @Valid ImageRequestDTO imageRequestDTO) {
@@ -92,7 +97,7 @@ public class ImageController {
 		return storeImageService.update(storeName, imageRequestDTO);
 	}
 
-	@DeleteMapping("/image")
+	@DeleteMapping("/store/image")
 	@PreAuthorize("hasAnyAuthority('ADMIN', 'ACCOUNTANT')")
 	public ImageDeleteDTO deleteStoreImage(
 			@RequestParam("store_name") String storeName) {
