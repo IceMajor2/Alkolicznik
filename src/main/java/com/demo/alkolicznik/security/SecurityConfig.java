25cb10e2f283cad919ec44784c12e0d62ae3f39e
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-17 11:37:00 +0200

    Add new configurations for JWT

--- src/main/java/com/demo/alkolicznik/security/SecurityConfig.java
+++ src/main/java/com/demo/alkolicznik/security/SecurityConfig.java
@@ -7,11 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,33 +22,36 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final RestAuthenticationEntryPoint authenticationEntryPoint;
+	private final RestAuthenticationEntryPoint authenticationEntryPoint;
 
-    private static final String[] ACCOUNTANT_AUTHORITIES = new String[]{"ADMIN", "ACCOUNTANT"};
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    @Order(Ordered.HIGHEST_PRECEDENCE + 8)
-    @Bean
-    public SecurityFilterChain restApiSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .httpBasic((httpBasic) -> httpBasic
-                        .authenticationEntryPoint(authenticationEntryPoint))
-                // or withDefaults()???
-                .exceptionHandling(excHandler -> excHandler
-                        .authenticationEntryPoint(authenticationEntryPoint)
-                        .accessDeniedHandler(authenticationEntryPoint))
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .permitAll())
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers(HttpMethod.PUT, "/api/beer/*").hasAnyAuthority(ACCOUNTANT_AUTHORITIES)
-                        .requestMatchers(HttpMethod.DELETE, "/api/beer").hasAnyAuthority(ACCOUNTANT_AUTHORITIES)
-                        .requestMatchers(HttpMethod.POST, "/api/beer").hasAnyAuthority(ACCOUNTANT_AUTHORITIES)
+	private final AuthenticationProvider authenticationProvider;
+
+	private static final String[] ACCOUNTANT_AUTHORITIES = new String[] { "ADMIN", "ACCOUNTANT" };
+
+	@Order(Ordered.HIGHEST_PRECEDENCE + 8)
+	@Bean
+	public SecurityFilterChain restApiSecurityFilterChain(HttpSecurity http) throws Exception {
+		http
+//                .httpBasic((httpBasic) -> httpBasic
+//                        .authenticationEntryPoint(authenticationEntryPoint))
+				.exceptionHandling(excHandler -> excHandler
+						.authenticationEntryPoint(authenticationEntryPoint)
+						.accessDeniedHandler(authenticationEntryPoint))
+				.formLogin((form) -> form
+						.loginPage("/login")
+						.permitAll())
+				.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests((auth) -> auth
+						.requestMatchers(HttpMethod.PUT, "/api/beer/*").hasAnyAuthority(ACCOUNTANT_AUTHORITIES)
+						.requestMatchers(HttpMethod.DELETE, "/api/beer").hasAnyAuthority(ACCOUNTANT_AUTHORITIES)
+						.requestMatchers(HttpMethod.POST, "/api/beer").hasAnyAuthority(ACCOUNTANT_AUTHORITIES)
 						.requestMatchers(HttpMethod.PATCH, "/api/beer/*").hasAnyAuthority(ACCOUNTANT_AUTHORITIES)
 						.requestMatchers(HttpMethod.PUT, "/api/store/*").hasAnyAuthority(ACCOUNTANT_AUTHORITIES)
-                        .requestMatchers(HttpMethod.POST, "/api/store").hasAnyAuthority(ACCOUNTANT_AUTHORITIES)
+						.requestMatchers(HttpMethod.POST, "/api/store").hasAnyAuthority(ACCOUNTANT_AUTHORITIES)
 						.requestMatchers(HttpMethod.PATCH, "/api/store/*").hasAnyAuthority(ACCOUNTANT_AUTHORITIES)
-                        .requestMatchers(HttpMethod.POST, "/api/store/*/beer-price").hasAnyAuthority(ACCOUNTANT_AUTHORITIES)
+						.requestMatchers(HttpMethod.POST, "/api/store/*/beer-price").hasAnyAuthority(ACCOUNTANT_AUTHORITIES)
 						.requestMatchers(HttpMethod.PATCH, "/api/beer-price/*").hasAnyAuthority(ACCOUNTANT_AUTHORITIES)
 						.requestMatchers(HttpMethod.POST, "/api/store/image/*").hasAnyAuthority(ACCOUNTANT_AUTHORITIES)
 						.requestMatchers(HttpMethod.PUT, "/api/store/image/*").hasAnyAuthority(ACCOUNTANT_AUTHORITIES)
@@ -53,13 +59,16 @@ public class SecurityConfig {
 						.requestMatchers(HttpMethod.POST, "/api/beer/image/*").hasAnyAuthority(ACCOUNTANT_AUTHORITIES)
 						.requestMatchers(HttpMethod.PUT, "/api/beer/image/*").hasAnyAuthority(ACCOUNTANT_AUTHORITIES)
 						.requestMatchers(HttpMethod.PUT, "/api/beer/image/*").hasAnyAuthority(ACCOUNTANT_AUTHORITIES)
-                        .anyRequest().permitAll()
-                );
-        return http.build();
-    }
+						.anyRequest().permitAll()
+				).sessionManagement(sessionManager -> sessionManager
+						.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authenticationProvider(authenticationProvider)
+				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+		return http.build();
+	}
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(13);
-    }
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(13);
+	}
 }
