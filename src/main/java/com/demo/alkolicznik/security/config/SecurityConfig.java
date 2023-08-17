c9bdbae73a648a3c0e75f71cf5d2ec07894a94a3
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-17 12:56:48 +0200

    Make authentication config beans be specified in one Java class (i.e. BeanConfig)

--- src/main/java/com/demo/alkolicznik/security/config/SecurityConfig.java
+++ src/main/java/com/demo/alkolicznik/security/config/SecurityConfig.java
@@ -7,14 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -36,8 +33,6 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain restApiSecurityFilterChain(HttpSecurity http) throws Exception {
 		http
-//                .httpBasic((httpBasic) -> httpBasic
-//                        .authenticationEntryPoint(authenticationEntryPoint))
 				.exceptionHandling(excHandler -> excHandler
 						.authenticationEntryPoint(authenticationEntryPoint)
 						.accessDeniedHandler(authenticationEntryPoint))
@@ -68,15 +63,5 @@ public class SecurityConfig {
 				.authenticationProvider(authenticationProvider)
 				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
-	}
-
-	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-		return configuration.getAuthenticationManager();
-	}
-
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder(13);
 	}
 }
