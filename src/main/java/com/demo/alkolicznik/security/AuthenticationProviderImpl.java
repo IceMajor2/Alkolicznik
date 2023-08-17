8aabf36381e739d9aa3975a4450be5351a353d7f
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-17 11:51:01 +0200

    Implement AuthenticationProvider as DAOAuthenticationProvider

---
+++ src/main/java/com/demo/alkolicznik/security/AuthenticationProviderImpl.java
@@ -0,0 +1,16 @@
+package com.demo.alkolicznik.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class AuthenticationProviderImpl extends DaoAuthenticationProvider {
+
+	@Autowired
+	public AuthenticationProviderImpl(UserDetailsService userDetailsService,
+			PasswordEncoder passwordEncoder) {
+		super.setUserDetailsService(userDetailsService);
+		super.setPasswordEncoder(passwordEncoder);
+	}
+}
