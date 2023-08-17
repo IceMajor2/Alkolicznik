7b765e08a65c819a1374d081776fb831dc7fc12c
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-17 10:40:48 +0200

    Create first JWT components

---
+++ src/main/java/com/demo/alkolicznik/security/JwtAuthenticationFilter.java
@@ -0,0 +1,35 @@
+package com.demo.alkolicznik.security;
+
+import java.io.IOException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+	private JwtService jwtService;
+
+	@Override
+	protected void doFilterInternal(
+			@NonNull HttpServletRequest request,
+			@NonNull HttpServletResponse response,
+			@NonNull FilterChain filterChain) throws ServletException, IOException {
+		final String authHeader = request.getHeader("Authorization");
+
+		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		final String jwt = authHeader.substring(7);
+		final String username = jwtService.extractUsername(jwt);
+	}
+}
---
+++ src/main/java/com/demo/alkolicznik/security/JwtService.java
@@ -0,0 +1,11 @@
+package com.demo.alkolicznik.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+	public String extractUsername(String jwt) {
+		return null;
+	}
+}
