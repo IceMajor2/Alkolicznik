d668c0e125cac3ed0c992f398223e16d0b7d8064
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-18 10:32:18 +0200

    Update endpoint security

--- src/main/java/com/demo/alkolicznik/security/config/SecurityVaadinConfig.java
+++ src/main/java/com/demo/alkolicznik/security/config/SecurityVaadinConfig.java
@@ -3,30 +3,26 @@ package com.demo.alkolicznik.security.config;
 import com.demo.alkolicznik.gui.LoginView;
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-//@Configuration
-//@EnableWebSecurity
-//@EnableVaadin
+@Configuration
+@EnableWebSecurity
 public class SecurityVaadinConfig extends VaadinWebSecurity {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((auth) -> auth
-				.requestMatchers(new AntPathRequestMatcher("/api/**")).anonymous()
-				.requestMatchers("/error").anonymous()
-                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll());
-        super.configure(http);
-        setLoginView(http, LoginView.class);
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((auth) -> auth
+				.requestMatchers(new AntPathRequestMatcher("/**")).permitAll());
+		super.configure(http);
+		setLoginView(http, LoginView.class);
+	}
 
-    @Override
-    protected void configure(WebSecurity web) throws Exception {
-		web.ignoring()
-				.requestMatchers(new AntPathRequestMatcher("/api/**"))
-				.requestMatchers("/error");
-        super.configure(web);
-    }
+	@Override
+	protected void configure(WebSecurity web) throws Exception {
+		super.configure(web);
+	}
 }
