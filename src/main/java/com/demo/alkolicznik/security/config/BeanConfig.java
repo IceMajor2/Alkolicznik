3efb6c62ebb44c4f8e5202c15935d35c6fef7fed
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-19 17:46:20 +0200

    Add new security beans. 	- 'webClient': client to make HTTPS requests with trust store disabled (TODO: enable trust store and add this app's certificate) 	- 'servletWebServerFactory' bean responsible for redirecting HTTP calls to HTTPS

--- src/main/java/com/demo/alkolicznik/security/config/BeanConfig.java
+++ src/main/java/com/demo/alkolicznik/security/config/BeanConfig.java
@@ -1,10 +1,23 @@
 package com.demo.alkolicznik.security.config;
 
-import com.demo.alkolicznik.repositories.UserRepository;
-import lombok.RequiredArgsConstructor;
+import javax.net.ssl.SSLException;
 
+import com.demo.alkolicznik.repositories.UserRepository;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import reactor.netty.http.client.HttpClient;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -13,10 +26,19 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 @RequiredArgsConstructor
 public class BeanConfig {
+
+	private final static String SECURITY_USER_CONSTRAINT = "CONFIDENTIAL";
+
+	private final static String REDIRECT_PATTERN = "/*";
+
+	private final static String CONNECTOR_PROTOCOL = "org.apache.coyote.http11.Http11NioProtocol";
+
+	private final static String CONNECTOR_SCHEME = "http";
 
 	private final UserRepository userRepository;
 
@@ -44,5 +66,49 @@ public class BeanConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder(13);
+	}
+
+	@Bean
+	public WebClient webClient(WebClient.Builder webClientBuilder) throws SSLException {
+		SslContext sslContext = SslContextBuilder
+				.forClient()
+				// TODO: only this app's certificate should be in trust store
+				.trustManager(InsecureTrustManagerFactory.INSTANCE)
+				.build();
+		ClientHttpConnector httpConnector = new ReactorClientHttpConnector(
+				HttpClient.create().secure(t -> t.sslContext(sslContext)));
+		return webClientBuilder
+				.clientConnector(httpConnector)
+				// TODO: base url should be passed as argument (property)
+				.baseUrl("https://127.0.0.1:8433")
+				.build();
+	}
+
+	@Bean
+	public TomcatServletWebServerFactory servletWebServerFactory() {
+		TomcatServletWebServerFactory tomcat =
+				new TomcatServletWebServerFactory() {
+					@Override
+					protected void postProcessContext(Context context) {
+						SecurityConstraint constraint = new SecurityConstraint();
+						constraint.setUserConstraint(SECURITY_USER_CONSTRAINT);
+						SecurityCollection collection = new SecurityCollection();
+						collection.addPattern(REDIRECT_PATTERN);
+						constraint.addCollection(collection);
+						context.addConstraint(constraint);
+					}
+				};
+		tomcat.addAdditionalTomcatConnectors(createHttpConnector());
+		return tomcat;
+	}
+
+	private Connector createHttpConnector() {
+		Connector connector =
+				new Connector(CONNECTOR_PROTOCOL);
+		connector.setScheme(CONNECTOR_SCHEME);
+		connector.setSecure(false);
+		connector.setPort(8080);
+		connector.setRedirectPort(8433);
+		return connector;
 	}
 }
