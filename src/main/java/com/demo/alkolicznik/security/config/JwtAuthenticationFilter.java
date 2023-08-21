989f507d9501a0ba3a8609436ab907330329c1ab
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-21 14:24:10 +0200

    Jwt filter also checks for cookie, not only header

--- src/main/java/com/demo/alkolicznik/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/demo/alkolicznik/security/config/JwtAuthenticationFilter.java
@@ -11,10 +11,12 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -32,15 +34,17 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 			@NonNull HttpServletResponse response,
 			@NonNull FilterChain filterChain) throws ServletException, IOException {
 		final String authHeader = request.getHeader("Authorization");
-
-		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+		final Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (SecurityContextHolder.getContext().getAuthentication() == null &&
+				(authHeader == null || !authHeader.startsWith("Bearer "))) {
 			filterChain.doFilter(request, response);
 			return;
 		}
-		final String jwt = authHeader.substring(7);
+
+		final String jwt = extractJWT(authHeader, auth);
 		final String username = jwtService.extractUsername(jwt);
 		if (username != null
-				&& SecurityContextHolder.getContext().getAuthentication() == null) {
+			/*&& SecurityContextHolder.getContext().getAuthentication() == null*/) {
 			UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 			if (jwtService.isTokenValid(jwt, userDetails)) {
 				var authToken = new UsernamePasswordAuthenticationToken(userDetails,
@@ -51,5 +55,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 			}
 		}
 		filterChain.doFilter(request, response);
+	}
+
+	private String extractJWT(String authHeader, Authentication authObject) {
+		// PreAuthenticatedAuthenticationToken would be a cookie
+		if (authObject instanceof PreAuthenticatedAuthenticationToken)
+			return (String) authObject.getPrincipal();
+			// and here's the in-header JWT
+		else return authHeader.substring(7);
 	}
 }
