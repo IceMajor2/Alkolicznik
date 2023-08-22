e3298e4910e3f5d268d273c1432a2ad52789b59b
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-22 12:27:29 +0200

    Exchange LocalDateTime for ZonedDateTime

--- src/main/java/com/demo/alkolicznik/exceptions/ApiError.java
+++ src/main/java/com/demo/alkolicznik/exceptions/ApiError.java
@@ -1,12 +1,14 @@
 package com.demo.alkolicznik.exceptions;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.http.HttpStatus;
 
-import java.time.LocalDateTime;
+import org.springframework.http.HttpStatus;
 
 @JsonPropertyOrder({"timestamp", "status", "error", "message", "path"})
 @NoArgsConstructor
@@ -14,14 +16,15 @@ import java.time.LocalDateTime;
 @Setter
 public class ApiError {
 
-    private LocalDateTime timestamp;
+    private ZonedDateTime timestamp;
     private int status;
     private String error;
     private String message;
     private String path;
 
     public ApiError(HttpStatus status, String message, String path) {
-        this.timestamp = LocalDateTime.now();
+		// TODO: adjust zone id to user
+		this.timestamp = ZonedDateTime.now(ZoneId.of("Poland"));
         this.status = status.value();
         this.error = status.getReasonPhrase();
         this.message = message;
