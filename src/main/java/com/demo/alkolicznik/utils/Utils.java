e6c379af9b66976ed8ab6aeea7dc1ea2741022a9
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-27 16:14:40 +0200

    Fix buffered image creating

--- src/main/java/com/demo/alkolicznik/utils/Utils.java
+++ src/main/java/com/demo/alkolicznik/utils/Utils.java
@@ -1,22 +1,33 @@
 package com.demo.alkolicznik.utils;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
-import javax.imageio.ImageIO;
+import java.net.URL;
 
 public class Utils {
 
-	public static String getExtensionFromPath(String path) {
-		return path.substring(path.lastIndexOf('.') + 1);
-	}
+    public static String getExtensionFromPath(String path) {
+        return path.substring(path.lastIndexOf('.') + 1);
+    }
 
-	public static BufferedImage getImage(File file) {
-		try {
-			return ImageIO.read(file);
-		} catch(IOException e) {
-			return null;
-		}
-	}
+    public static BufferedImage getBufferedImageFromWeb(String url) {
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new URL(url));
+            return image;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    public static BufferedImage getBufferedImageFromLocal(String path) {
+        try {
+            BufferedImage image = ImageIO.read(new File(path));
+            return image;
+        } catch (IOException e) {
+            return null;
+        }
+    }
 }
