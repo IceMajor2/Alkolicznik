19641d337f22f4e7a06b2ab34bddd82873edd23d
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-27 16:05:13 +0200

    Add LEFT JOIN FETCH to findAll query

--- src/main/java/com/demo/alkolicznik/repositories/StoreImageRepository.java
+++ src/main/java/com/demo/alkolicznik/repositories/StoreImageRepository.java
@@ -1,19 +1,20 @@
 package com.demo.alkolicznik.repositories;
 
+import com.demo.alkolicznik.models.image.StoreImage;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
 import java.util.List;
 import java.util.Optional;
 
-import com.demo.alkolicznik.models.image.StoreImage;
-
-import org.springframework.data.repository.CrudRepository;
-
 public interface StoreImageRepository extends CrudRepository<StoreImage, Long> {
 
-	List<StoreImage> findAll();
+    @Query("SELECT i FROM StoreImage i LEFT JOIN FETCH i.stores")
+    List<StoreImage> findAll();
 
-	Optional<StoreImage> findByImageUrl(String imageUrl);
+    Optional<StoreImage> findByImageUrl(String imageUrl);
 
-	Optional<StoreImage> findByStoreName(String storeName);
+    Optional<StoreImage> findByStoreName(String storeName);
 
-	boolean existsByStoreName(String storeName);
+    boolean existsByStoreName(String storeName);
 }
