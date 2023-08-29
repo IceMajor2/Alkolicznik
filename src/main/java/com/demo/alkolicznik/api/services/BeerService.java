23acd7b3e6a103e1e9e2e903e2c649cb6bf46b2b
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-29 10:36:16 +0200

    Do not remove image when beer is updated with changed volume AND explicitly stated other values that are not differing from original entity

--- src/main/java/com/demo/alkolicznik/api/services/BeerService.java
+++ src/main/java/com/demo/alkolicznik/api/services/BeerService.java
@@ -1,15 +1,6 @@
 package com.demo.alkolicznik.api.services;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
-
-import com.demo.alkolicznik.dto.beer.BeerDeleteRequestDTO;
-import com.demo.alkolicznik.dto.beer.BeerDeleteResponseDTO;
-import com.demo.alkolicznik.dto.beer.BeerRequestDTO;
-import com.demo.alkolicznik.dto.beer.BeerResponseDTO;
-import com.demo.alkolicznik.dto.beer.BeerUpdateDTO;
+import com.demo.alkolicznik.dto.beer.*;
 import com.demo.alkolicznik.exceptions.classes.NoSuchCityException;
 import com.demo.alkolicznik.exceptions.classes.ObjectsAreEqualException;
 import com.demo.alkolicznik.exceptions.classes.PropertiesMissingException;
@@ -23,167 +14,172 @@ import com.demo.alkolicznik.repositories.StoreRepository;
 import com.demo.alkolicznik.utils.ModelDtoConverter;
 import com.vaadin.flow.component.html.Image;
 import lombok.AllArgsConstructor;
-
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 @AllArgsConstructor
 public class BeerService {
 
-	private BeerRepository beerRepository;
+    private BeerRepository beerRepository;
 
-	private StoreRepository storeRepository;
+    private StoreRepository storeRepository;
 
-	private BeerImageService imageService;
+    private BeerImageService imageService;
 
-	public BeerResponseDTO get(Long beerId) {
-		Beer beer = beerRepository.findById(beerId).orElseThrow(
-				() -> new BeerNotFoundException(beerId)
-		);
-		return new BeerResponseDTO(beer);
-	}
+    public BeerResponseDTO get(Long beerId) {
+        Beer beer = beerRepository.findById(beerId).orElseThrow(
+                () -> new BeerNotFoundException(beerId)
+        );
+        return new BeerResponseDTO(beer);
+    }
 
-	public List<BeerResponseDTO> getBeers(String city) {
-		if (!storeRepository.existsByCity(city)) {
-			throw new NoSuchCityException(city);
-		}
-		List<Store> cityStores = storeRepository.findAllByCityOrderByIdAsc(city);
-		// order by id ascending
-		Set<Beer> beersInCity = new TreeSet<>(Comparator.comparing(Beer::getId));
-		for (Store store : cityStores) {
-			beersInCity.addAll(
-					store.getPrices().stream()
-							.map(BeerPrice::getBeer)
-							.toList()
-			);
-		}
-		return ModelDtoConverter.beerListToDtoList(beersInCity);
-	}
+    public List<BeerResponseDTO> getBeers(String city) {
+        if (!storeRepository.existsByCity(city)) {
+            throw new NoSuchCityException(city);
+        }
+        List<Store> cityStores = storeRepository.findAllByCityOrderByIdAsc(city);
+        // order by id ascending
+        Set<Beer> beersInCity = new TreeSet<>(Comparator.comparing(Beer::getId));
+        for (Store store : cityStores) {
+            beersInCity.addAll(
+                    store.getPrices().stream()
+                            .map(BeerPrice::getBeer)
+                            .toList()
+            );
+        }
+        return ModelDtoConverter.beerListToDtoList(beersInCity);
+    }
 
-	public List<BeerResponseDTO> getBeers() {
-		return ModelDtoConverter.beerListToDtoList(beerRepository.findAllByOrderByIdAsc());
-	}
+    public List<BeerResponseDTO> getBeers() {
+        return ModelDtoConverter.beerListToDtoList(beerRepository.findAllByOrderByIdAsc());
+    }
 
-	public BeerResponseDTO add(BeerRequestDTO requestDTO) {
-		Beer beer = ModelDtoConverter.convertToModelNoImage(requestDTO);
-		if (beerRepository.exists(beer)) {
-			throw new BeerAlreadyExistsException();
-		}
-		return new BeerResponseDTO(beerRepository.save(beer));
-	}
+    public BeerResponseDTO add(BeerRequestDTO requestDTO) {
+        Beer beer = ModelDtoConverter.convertToModelNoImage(requestDTO);
+        if (beerRepository.exists(beer)) {
+            throw new BeerAlreadyExistsException();
+        }
+        return new BeerResponseDTO(beerRepository.save(beer));
+    }
 
-	public BeerResponseDTO replace(Long beerId, BeerRequestDTO requestDTO) {
-		Beer toOverwrite = checkForPutConditions(beerId, requestDTO);
-		Beer newBeer = ModelDtoConverter.convertToModelNoImage(requestDTO);
-		Beer overwritten = updateFieldsOnPut(toOverwrite, newBeer);
-		if (beerRepository.exists(overwritten)) throw new BeerAlreadyExistsException();
-		// for each PUT request all the previous
-		// beer prices for this beer MUST be deleted
-		toOverwrite.deleteAllPrices();
-		// for each PUT request the previous image MUST be deleted
-		if (toOverwrite.getImage().isPresent()) {
-			imageService.delete(toOverwrite.getImage().get());
-		}
-		return new BeerResponseDTO(beerRepository.save(toOverwrite));
-	}
+    public BeerResponseDTO replace(Long beerId, BeerRequestDTO requestDTO) {
+        Beer toOverwrite = checkForPutConditions(beerId, requestDTO);
+        Beer newBeer = ModelDtoConverter.convertToModelNoImage(requestDTO);
+        Beer overwritten = updateFieldsOnPut(toOverwrite, newBeer);
+        if (beerRepository.exists(overwritten)) throw new BeerAlreadyExistsException();
+        // for each PUT request all the previous
+        // beer prices for this beer MUST be deleted
+        toOverwrite.deleteAllPrices();
+        // for each PUT request the previous image MUST be deleted
+        if (toOverwrite.getImage().isPresent()) {
+            imageService.delete(toOverwrite.getImage().get());
+        }
+        return new BeerResponseDTO(beerRepository.save(toOverwrite));
+    }
 
-	public BeerResponseDTO update(Long beerId, BeerUpdateDTO updateDTO) {
-		Beer beer = checkForPatchConditions(beerId, updateDTO);
-		Beer updated = updateFieldsOnPatch(beer, updateDTO);
+    public BeerResponseDTO update(Long beerId, BeerUpdateDTO updateDTO) {
+        Beer beer = checkForPatchConditions(beerId, updateDTO);
+        Beer updated = updateFieldsOnPatch(beer, updateDTO);
 
-		if (beerRepository.exists(updated))
-			throw new BeerAlreadyExistsException();
-		// deleting prices on conditions
-		if (this.pricesToDelete(updateDTO)) {
-			beer.deleteAllPrices();
-		}
-		// deleting image on conditions and
-		// reuploading if it was requested
-		if (this.imageToDelete(updated, updateDTO)) {
-			imageService.delete(beerId);
-		}
-		return new BeerResponseDTO(beerRepository.save(beer));
-	}
+        if (beerRepository.exists(updated))
+            throw new BeerAlreadyExistsException();
+        // deleting prices on conditions
+        if (this.pricesToDelete(updateDTO)) {
+            beer.deleteAllPrices();
+        }
+        // deleting image on conditions and
+        if (this.imageToDelete(beer, updateDTO)) {
+            imageService.delete(beerId);
+        }
+        return new BeerResponseDTO(beerRepository.save(beer));
+    }
 
-	public BeerDeleteResponseDTO delete(Long beerId) {
-		Beer toDelete = beerRepository.findById(beerId).orElseThrow(() ->
-				new BeerNotFoundException(beerId));
-		beerRepository.delete(toDelete);
-		if (toDelete.getImage().isPresent()) imageService.delete(toDelete.getImage().get());
-		return new BeerDeleteResponseDTO(toDelete);
-	}
+    public BeerDeleteResponseDTO delete(Long beerId) {
+        Beer toDelete = beerRepository.findById(beerId).orElseThrow(() ->
+                new BeerNotFoundException(beerId));
+        beerRepository.delete(toDelete);
+        if (toDelete.getImage().isPresent()) imageService.delete(toDelete.getImage().get());
+        return new BeerDeleteResponseDTO(toDelete);
+    }
 
-	public BeerDeleteResponseDTO delete(BeerDeleteRequestDTO request) {
-		Beer toDelete = beerRepository.findByFullnameAndVolume(request.getFullName(), request.getVolume())
-				.orElseThrow(() -> new BeerNotFoundException(request.getFullName(), request.getVolume()));
-		return this.delete(toDelete.getId());
-	}
+    public BeerDeleteResponseDTO delete(BeerDeleteRequestDTO request) {
+        Beer toDelete = beerRepository.findByFullnameAndVolume(request.getFullName(), request.getVolume())
+                .orElseThrow(() -> new BeerNotFoundException(request.getFullName(), request.getVolume()));
+        return this.delete(toDelete.getId());
+    }
 
-	public Image getImageComponent(Long beerId) {
-		Beer beer = beerRepository.findById(beerId)
-				.orElseThrow(() -> new BeerNotFoundException(beerId));
-		return imageService.getComponent(beer);
-	}
+    public Image getImageComponent(Long beerId) {
+        Beer beer = beerRepository.findById(beerId)
+                .orElseThrow(() -> new BeerNotFoundException(beerId));
+        return imageService.getComponent(beer);
+    }
 
-	private Beer updateFieldsOnPatch(Beer toUpdate, BeerUpdateDTO updateDTO) {
-		String updatedBrand = updateDTO.getBrand();
-		String updatedType = updateDTO.getType();
-		Double updatedVolume = updateDTO.getVolume();
+    private Beer updateFieldsOnPatch(Beer toUpdate, BeerUpdateDTO updateDTO) {
+        String updatedBrand = updateDTO.getBrand();
+        String updatedType = updateDTO.getType();
+        Double updatedVolume = updateDTO.getVolume();
 
-		if (updatedBrand != null) {
-			toUpdate.setBrand(updatedBrand);
-		}
-		if (updatedType != null) {
-			if (updatedType.isBlank()) toUpdate.setType(null);
-			else toUpdate.setType(updatedType);
-		}
-		if (updatedVolume != null) {
-			toUpdate.setVolume(updatedVolume);
-		}
-		return toUpdate;
-	}
+        if (updatedBrand != null) {
+            toUpdate.setBrand(updatedBrand);
+        }
+        if (updatedType != null) {
+            if (updatedType.isBlank()) toUpdate.setType(null);
+            else toUpdate.setType(updatedType);
+        }
+        if (updatedVolume != null) {
+            toUpdate.setVolume(updatedVolume);
+        }
+        return toUpdate;
+    }
 
-	private Beer updateFieldsOnPut(Beer toOverwrite, Beer newBeer) {
-		toOverwrite.setBrand(newBeer.getBrand());
-		toOverwrite.setType(newBeer.getType());
-		toOverwrite.setVolume(newBeer.getVolume());
-		return toOverwrite;
-	}
+    private Beer updateFieldsOnPut(Beer toOverwrite, Beer newBeer) {
+        toOverwrite.setBrand(newBeer.getBrand());
+        toOverwrite.setType(newBeer.getType());
+        toOverwrite.setVolume(newBeer.getVolume());
+        return toOverwrite;
+    }
 
-	private Beer checkForUpdateConditions(Long beerId, BeerUpdateDTO updateDTO) {
-		Beer beer = beerRepository.findById(beerId).orElseThrow(
-				() -> new BeerNotFoundException(beerId)
-		);
-		if (!updateDTO.anythingToUpdate(beer)) {
-			throw new ObjectsAreEqualException();
-		}
-		return beer;
-	}
+    private Beer checkForUpdateConditions(Long beerId, BeerUpdateDTO updateDTO) {
+        Beer beer = beerRepository.findById(beerId).orElseThrow(
+                () -> new BeerNotFoundException(beerId)
+        );
+        if (!updateDTO.anythingToUpdate(beer)) {
+            throw new ObjectsAreEqualException();
+        }
+        return beer;
+    }
 
-	private Beer checkForPatchConditions(Long beerId, BeerUpdateDTO updateDTO) {
-		if (updateDTO.propertiesMissing()) {
-			throw new PropertiesMissingException();
-		}
-		return checkForUpdateConditions(beerId, updateDTO);
-	}
+    private Beer checkForPatchConditions(Long beerId, BeerUpdateDTO updateDTO) {
+        if (updateDTO.propertiesMissing()) {
+            throw new PropertiesMissingException();
+        }
+        return checkForUpdateConditions(beerId, updateDTO);
+    }
 
-	private Beer checkForPutConditions(Long beerId, BeerRequestDTO requestDTO) {
-		return checkForUpdateConditions(beerId, new BeerUpdateDTO(requestDTO));
-	}
+    private Beer checkForPutConditions(Long beerId, BeerRequestDTO requestDTO) {
+        return checkForUpdateConditions(beerId, new BeerUpdateDTO(requestDTO));
+    }
 
-	private boolean imageToDelete(Beer toUpdate, BeerUpdateDTO updateDTO) {
-		// if updated field is ONLY volume, then do not delete image
-		if (updateDTO.getBrand() == null
-				&& updateDTO.getType() == null) {
-			return false;
-		}
-		if (toUpdate.getImage().isPresent()) {
-			return true;
-		}
-		return false;
-	}
+    private boolean imageToDelete(Beer toUpdate, BeerUpdateDTO updateDTO) {
+        // if updated field is ONLY volume, then do not delete image
+        // OR if volume is the only field differing
+        if (updateDTO.getBrand() == null
+                && updateDTO.getType() == null) {
+            return false;
+        }
+        if (Objects.equals(toUpdate.getBrand(), updateDTO.getBrand())
+                && Objects.equals(toUpdate.getType(), updateDTO.getType())) {
+            return false;
+        }
+        if (toUpdate.getImage().isPresent()) {
+            return true;
+        }
+        return false;
+    }
 
-	private boolean pricesToDelete(BeerUpdateDTO updateDTO) {
-		return updateDTO.getBrand() != null || updateDTO.getType() != null;
-	}
+    private boolean pricesToDelete(BeerUpdateDTO updateDTO) {
+        return updateDTO.getBrand() != null || updateDTO.getType() != null;
+    }
 }
