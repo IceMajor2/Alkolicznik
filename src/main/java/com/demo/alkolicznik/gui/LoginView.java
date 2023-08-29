1a1b587b11405a31ee3ab444365f54f777f48297
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-29 10:53:07 +0200

    The password is now covered when typing

--- src/main/java/com/demo/alkolicznik/gui/LoginView.java
+++ src/main/java/com/demo/alkolicznik/gui/LoginView.java
@@ -9,32 +9,26 @@ import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.VaadinService;
 import org.springframework.http.HttpMethod;
 
-@PageTitle("Login")
+@PageTitle("Login | Alkolicznik")
 @Route(value = "login")
 public class LoginView extends VerticalLayout {
 
-//  TODO: fix fetching of properties
-//	@Value("${trust.store}")
-//	private Resource trustStore;
-//
-//	@Value("${trust.store.password}")
-//	private String trustStorePassword;
-
     private TextField username;
 
-    private TextField password;
+    private PasswordField password;
 
     private Button login;
 
     public LoginView() {
         this.username = new TextField("Username");
-        this.password = new TextField("Password");
+        this.password = new PasswordField("Password");
         this.login = new Button("Login", loginEvent());
 
         add(username, password, login);
