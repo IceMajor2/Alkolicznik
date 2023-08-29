be18446777550bf3f0ef0a179abb505a283efe57
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-29 09:56:30 +0200

    Update config class for disabling of vaadin context

--- src/test/java/com/demo/alkolicznik/config/DisabledVaadinContext.java
+++ src/test/java/com/demo/alkolicznik/config/DisabledVaadinContext.java
@@ -1,31 +1,19 @@
 package com.demo.alkolicznik.config;
 
-import com.demo.alkolicznik.security.AuthController;
-import com.demo.alkolicznik.security.AuthService;
 import com.demo.alkolicznik.security.config.SecurityVaadinConfig;
 import com.vaadin.flow.spring.SpringBootAutoConfiguration;
 import com.vaadin.flow.spring.SpringSecurityAutoConfiguration;
 import com.vaadin.flow.spring.VaadinScopesConfig;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.authentication.AuthenticationManager;
 
 @EnableAutoConfiguration(exclude = {
         VaadinScopesConfig.class,
         SpringBootAutoConfiguration.class,
-        SpringSecurityAutoConfiguration.class
+        SpringSecurityAutoConfiguration.class,
 })
 public class DisabledVaadinContext {
 
     @MockBean
-    private AuthController authController;
-
-    @MockBean
-    private AuthService authService;
-
-    @MockBean
-    private SecurityVaadinConfig securityVaadinConfig;
-
-    @MockBean
-    private AuthenticationManager authenticationManager;
+    SecurityVaadinConfig securityVaadinConfig;
 }
