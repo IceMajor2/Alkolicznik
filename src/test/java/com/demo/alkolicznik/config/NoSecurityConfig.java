eba684f43921f80306ac81567e10fce22d5783b6
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-30 11:53:05 +0200

    Remove authentication entry point as all the endpoints are available

--- src/test/java/com/demo/alkolicznik/config/NoSecurityConfig.java
+++ src/test/java/com/demo/alkolicznik/config/NoSecurityConfig.java
@@ -1,8 +1,6 @@
 package com.demo.alkolicznik.config;
 
-import com.demo.alkolicznik.security.config.RestAuthenticationEntryPoint;
 import org.junit.jupiter.api.Order;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -20,21 +18,21 @@ import org.springframework.security.web.SecurityFilterChain;
 @Profile("no-security")
 public class NoSecurityConfig {
 
-    private final RestAuthenticationEntryPoint authenticationEntryPoint;
-
-    @Autowired
-    public NoSecurityConfig(RestAuthenticationEntryPoint authenticationEntryPoint) {
-        this.authenticationEntryPoint = authenticationEntryPoint;
-    }
+//    private final RestAuthenticationEntryPoint authenticationEntryPoint;
+//
+//    @Autowired
+//    public NoSecurityConfig(RestAuthenticationEntryPoint authenticationEntryPoint) {
+//        this.authenticationEntryPoint = authenticationEntryPoint;
+//    }
 
     @Bean
     @Primary
     @Order(Ordered.HIGHEST_PRECEDENCE - 8)
     public SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
         http
-                .exceptionHandling(excHandler -> excHandler
-                        .authenticationEntryPoint(authenticationEntryPoint)
-                        .accessDeniedHandler(authenticationEntryPoint))
+//                .exceptionHandling(excHandler -> excHandler
+//                        .authenticationEntryPoint(authenticationEntryPoint)
+//                        .accessDeniedHandler(authenticationEntryPoint))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/**").permitAll()
