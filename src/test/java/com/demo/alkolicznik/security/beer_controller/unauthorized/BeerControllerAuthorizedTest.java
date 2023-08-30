9f74d1089a1471a8fe264a500610793e3d23d136
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-30 15:08:28 +0200

    Rename pkg

--- src/test/java/com/demo/alkolicznik/security/beer_controller/unauthorized/BeerControllerAuthorizedTest.java
+++ src/test/java/com/demo/alkolicznik/security/beer_controller/unauthorized/BeerControllerAuthorizedTest.java
@@ -1,4 +1,4 @@
-package com.demo.alkolicznik.security.beercontroller.unauthorized;
+package com.demo.alkolicznik.security.beer_controller.unauthorized;
 
 import com.demo.alkolicznik.config.DisabledVaadinContext;
 import com.demo.alkolicznik.dto.beer.BeerDeleteRequestDTO;
