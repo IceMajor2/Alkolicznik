90339bd3cfa33446eaf4a98fa9bfb0256504973e
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-31 19:58:32 +0200

    Move beans to its profiles; explicitly state beans' names; create more conditionals

--- src/test/java/com/demo/alkolicznik/config/profiles/ImageProfile.java
+++ src/test/java/com/demo/alkolicznik/config/profiles/ImageProfile.java
@@ -1,9 +1,12 @@
 package com.demo.alkolicznik.config.profiles;
 
+import com.demo.alkolicznik.models.Beer;
+import com.demo.alkolicznik.models.Store;
 import com.demo.alkolicznik.models.image.BeerImage;
 import com.demo.alkolicznik.models.image.ImageModel;
 import com.demo.alkolicznik.models.image.StoreImage;
-import com.demo.alkolicznik.utils.FindingUtils;
+import com.demo.alkolicznik.utils.TestUtils;
+import com.demo.alkolicznik.utils.mappers.DatabaseTableConverters;
 import io.imagekit.sdk.ImageKit;
 import io.imagekit.sdk.exceptions.*;
 import io.imagekit.sdk.models.BaseFile;
@@ -15,10 +18,11 @@ import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.*;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.io.File;
 import java.io.IOException;
@@ -28,143 +32,211 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static com.demo.alkolicznik.utils.TestUtils.*;
+import static com.demo.alkolicznik.utils.TestUtils.getRawPathToClassPathResource;
 
 @Configuration
 @Profile("image")
+@PropertySource("classpath:profiles/image.properties")
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class ImageProfile {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(ImageProfile.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ImageProfile.class);
 
-	private static final List<String> BEER_IMAGES = List.of(
-			"tyskie-gronie-0.65.png", "zubr-0.5.png",
-			"komes-porter-malinowy-0.33.jpg", "miloslaw-biale-0.5.jpg"
-	);
+    // TODO: replace with method
+    private static final List<String> BEER_IMAGES = List.of(
+            "tyskie-gronie-0.65.png", "zubr-0.5.png",
+            "komes-porter-malinowy-0.33.jpg", "miloslaw-biale-0.5.jpg"
+    );
 
-	private static final List<String> STORE_IMAGES = List.of(
-			"carrefour.png", "lubi.jpg", "abc.png"
-	);
+    private static final List<String> STORE_IMAGES = List.of(
+            "carrefour.png", "lubi.jpg", "abc.png"
+    );
 
-	private FindingUtils findingUtils;
+    private static String imageKitPath;
 
-	private static String imageKitPath;
+    private ImageKit imageKit;
 
-	private ImageKit imageKit;
+    private JdbcTemplate jdbcTemplate;
 
-	@Autowired
-	public ImageProfile(FindingUtils findingUtils) {
-		this.findingUtils = findingUtils;
-	}
+    private TestUtils testUtils;
 
-	@Autowired
-	public void setImageKitPath(String imageKitPath) {
-		ImageProfile.imageKitPath = imageKitPath;
-	}
+    @Autowired
+    public ImageProfile(TestUtils testUtils, JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.testUtils = testUtils;
+    }
 
-	@PostConstruct
-	public void init() throws ForbiddenException, TooManyRequestsException, InternalServerException, UnauthorizedException, BadRequestException, UnknownException, IOException, IllegalAccessException, InstantiationException {
-		String remoteBeerImgPath = imageKitPath + "/beer";
-		String remoteStoreImgPath = imageKitPath + "/store";
-		LOGGER.info("Reloading ImageKit's directory...");
-		setImageKit();
-		LOGGER.info("Deleting unwanted images from '%s' directory...".formatted(remoteBeerImgPath));
-		deleteFilesIn(remoteBeerImgPath, false);
-		LOGGER.info("Deleting unwanted images from '%s' directory...".formatted(remoteStoreImgPath));
-		deleteFilesIn(remoteStoreImgPath, true);
-		LOGGER.info("Sending BEER images to remote directory '%s'...".formatted(remoteBeerImgPath));
-		sendImages("/data_img/beer_at_launch", remoteBeerImgPath);
-		LOGGER.info("Sending STORE images to remote directory '%s'...".formatted(remoteStoreImgPath));
-		sendImages("/data_img/store_at_launch", remoteStoreImgPath);
-	}
+    @Bean("beerImages")
+    @DependsOn("beers")
+    @ConditionalOnProperty(prefix = "database.table.image", name = "enabled", havingValue = "true")
+    public List<BeerImage> beerImages(List<Beer> beers) {
+        LOGGER.info("Creating 'beerImages' bean...");
+        String sql = "SELECT * FROM beer_image WHERE beer_id = ?";
+        List<BeerImage> beerImages = DatabaseTableConverters
+                .convertToBeerImageList(sql, beers);
+        updateBeerImageRemoteId(beerImages);
+        updateUrlsWithUpdatedAt(beerImages, "beer_image", "beer_id");
+        return beerImages;
+    }
 
-	/**
-	 * Deletes all files in a remote, ImageKit's directory.
-	 * You can actually leave out test data (specified in a
-	 * static variable) by passing a boolean parameter.
-	 * @param path remote path that is to be deleted
-	 * @param deleteTestData if true, will delete everything
-	 * from directory - including test data
-	 */
-	private void deleteFilesIn(String path, boolean deleteTestData) throws ForbiddenException, TooManyRequestsException, InternalServerException, UnauthorizedException, BadRequestException, UnknownException, IllegalAccessException, InstantiationException {
-		GetFileListRequest getFileListRequest = new GetFileListRequest();
-		getFileListRequest.setPath(path);
-		ResultList resultList = this.imageKit.getFileList(getFileListRequest);
+    @Bean("storeImages")
+    @DependsOn("stores")
+    @ConditionalOnProperty(prefix = "database.table.image", name = "enabled", havingValue = "true")
+    public List<StoreImage> storeImages(List<Store> stores) {
+        LOGGER.info("Creating 'storeImages' bean...");
+        String sql = "SELECT * FROM store_image";
+        List<StoreImage> storeImages = DatabaseTableConverters
+                .convertToStoreImageList(sql, stores);
+        updateStoreImageRemoteId(storeImages);
+        updateUrlsWithUpdatedAt(storeImages, "store_image", "id");
+        return storeImages;
+    }
 
-		for (BaseFile baseFile : resultList.getResults()) {
-			if (!deleteTestData && (BEER_IMAGES.contains(baseFile.getName())
-					|| STORE_IMAGES.contains(baseFile.getName()))) {
-				LOGGER.info("No need to DELETE. '%s' was found (ID: %s)"
-						.formatted(baseFile.getName(), baseFile.getFileId()));
-				continue;
-			}
-			LOGGER.info("Deleting... '%s'"
-					.formatted(baseFile.getName()));
-			this.imageKit.deleteFile(baseFile.getFileId());
-		}
-	}
+    @Autowired
+    public void setImageKitPath(String imageKitPath) {
+        ImageProfile.imageKitPath = imageKitPath;
+    }
 
-	private void sendImages(String srcPath, String remotePath) throws ForbiddenException, TooManyRequestsException, InternalServerException, UnauthorizedException, BadRequestException, UnknownException, IOException, IllegalAccessException, InstantiationException {
-		GetFileListRequest getFileListRequest = new GetFileListRequest();
-		getFileListRequest.setPath(remotePath);
+    @PostConstruct
+    public void init() throws ForbiddenException, TooManyRequestsException, InternalServerException, UnauthorizedException, BadRequestException, UnknownException, IOException, IllegalAccessException, InstantiationException {
+        String remoteBeerImgPath = imageKitPath + "/beer";
+        String remoteStoreImgPath = imageKitPath + "/store";
+        LOGGER.info("Reloading ImageKit's directory...");
+        setImageKit();
+        LOGGER.info("Deleting unwanted images from '%s' directory...".formatted(remoteBeerImgPath));
+        deleteFilesIn(remoteBeerImgPath, false);
+        LOGGER.info("Deleting unwanted images from '%s' directory...".formatted(remoteStoreImgPath));
+        deleteFilesIn(remoteStoreImgPath, true);
+        LOGGER.info("Sending BEER images to remote directory '%s'...".formatted(remoteBeerImgPath));
+        sendImages("/data_img/beer_at_launch", remoteBeerImgPath);
+        LOGGER.info("Sending STORE images to remote directory '%s'...".formatted(remoteStoreImgPath));
+        sendImages("/data_img/store_at_launch", remoteStoreImgPath);
+    }
 
-		Map<String, String> baseFiles = this.imageKit.getFileList(getFileListRequest)
-				.getResults()
-				.stream()
-				.collect(Collectors.toMap(BaseFile::getName, BaseFile::getFileId));
-		File[] testDir =
-				new File(getRawPathToClassPathResource(srcPath)).listFiles();
 
-		for (File image : testDir) {
-			if (baseFiles.containsKey(image.getName())) {
-				continue;
-			}
-			byte[] bytes = Files.readAllBytes(image.toPath());
-			FileCreateRequest fileCreateRequest = new FileCreateRequest(bytes, image.getName());
-			fileCreateRequest.setUseUniqueFileName(false);
-			fileCreateRequest.setFolder(remotePath);
-			Result result = this.imageKit.upload(fileCreateRequest);
-			LOGGER.info("'%s' was successfully sent (ID: %s)"
-					.formatted(result.getName(), result.getFileId()));
-		}
-	}
+    /**
+     * Deletes all files in a remote, ImageKit's directory.
+     * You can actually leave out test data (specified in a
+     * static variable) by passing a boolean parameter.
+     *
+     * @param path           remote path that is to be deleted
+     * @param deleteTestData if true, will delete everything
+     *                       from directory - including test data
+     */
+    private void deleteFilesIn(String path, boolean deleteTestData) throws ForbiddenException, TooManyRequestsException, InternalServerException, UnauthorizedException, BadRequestException, UnknownException, IllegalAccessException, InstantiationException {
+        GetFileListRequest getFileListRequest = new GetFileListRequest();
+        getFileListRequest.setPath(path);
+        ResultList resultList = this.imageKit.getFileList(getFileListRequest);
 
-	private void setImageKit() {
-		this.imageKit = ImageKit.getInstance();
-		String endpoint = "https://ik.imagekit.io/alkolicznik";
-		String publicKey = "public_9bnA9mQhgiGpder50E8rqIB98uM=";
-		try {
-			this.imageKit.setConfig(new io.imagekit.sdk.config.Configuration(publicKey,
-					Files.readAllLines(Paths.get("secure" + File.separator + "api_key.txt")).get(0),
-					endpoint));
-		}
-		catch (IOException e) {
-			throw new RuntimeException("Could not read secured file");
-		}
-	}
+        for (BaseFile baseFile : resultList.getResults()) {
+            if (!deleteTestData && (BEER_IMAGES.contains(baseFile.getName())
+                    || STORE_IMAGES.contains(baseFile.getName()))) {
+                LOGGER.info("No need to DELETE. '%s' was found (ID: %s)"
+                        .formatted(baseFile.getName(), baseFile.getFileId()));
+                continue;
+            }
+            LOGGER.info("Deleting... '%s'"
+                    .formatted(baseFile.getName()));
+            this.imageKit.deleteFile(baseFile.getFileId());
+        }
+    }
 
-	public static String extractFilenameFromUrl(String url) {
-		return url.substring(url.lastIndexOf('/') + 1);
-	}
+    private void sendImages(String srcPath, String remotePath) throws ForbiddenException, TooManyRequestsException, InternalServerException, UnauthorizedException, BadRequestException, UnknownException, IOException, IllegalAccessException, InstantiationException {
+        GetFileListRequest getFileListRequest = new GetFileListRequest();
+        getFileListRequest.setPath(remotePath);
 
-	public static String getRemoteId(String filename, Class<? extends ImageModel> imgClass) {
-		GetFileListRequest getFileListRequest = new GetFileListRequest();
-		String remotePath = imageKitPath + (imgClass.equals(StoreImage.class)
-				? "/store" : imgClass.equals(BeerImage.class)
-				? "/beer" : null);
-		getFileListRequest.setPath(remotePath);
-		ResultList resultList = null;
-		try {
-			resultList = ImageKit.getInstance().getFileList(getFileListRequest);
-		}
-		catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-		for (BaseFile baseFile : resultList.getResults()) {
-			if (baseFile.getName().equals(filename)) {
-				return baseFile.getFileId();
-			}
-		}
-		return null;
-	}
+        Map<String, String> baseFiles = this.imageKit.getFileList(getFileListRequest)
+                .getResults()
+                .stream()
+                .collect(Collectors.toMap(BaseFile::getName, BaseFile::getFileId));
+        File[] testDir =
+                new File(getRawPathToClassPathResource(srcPath)).listFiles();
+
+        for (File image : testDir) {
+            if (baseFiles.containsKey(image.getName())) {
+                continue;
+            }
+            byte[] bytes = Files.readAllBytes(image.toPath());
+            FileCreateRequest fileCreateRequest = new FileCreateRequest(bytes, image.getName());
+            fileCreateRequest.setUseUniqueFileName(false);
+            fileCreateRequest.setFolder(remotePath);
+            Result result = this.imageKit.upload(fileCreateRequest);
+            LOGGER.info("'%s' was successfully sent (ID: %s)"
+                    .formatted(result.getName(), result.getFileId()));
+        }
+    }
+
+    private void setImageKit() {
+        this.imageKit = ImageKit.getInstance();
+        String endpoint = "https://ik.imagekit.io/alkolicznik";
+        String publicKey = "public_9bnA9mQhgiGpder50E8rqIB98uM=";
+        try {
+            this.imageKit.setConfig(new io.imagekit.sdk.config.Configuration(publicKey,
+                    Files.readAllLines(Paths.get("secure" + File.separator + "api_key.txt")).get(0),
+                    endpoint));
+        } catch (IOException e) {
+            throw new RuntimeException("Could not read secured file");
+        }
+    }
+
+    public static String extractFilenameFromUrl(String url) {
+        return url.substring(url.lastIndexOf('/') + 1);
+    }
+
+    public static String getRemoteId(String filename, Class<? extends ImageModel> imgClass) {
+        GetFileListRequest getFileListRequest = new GetFileListRequest();
+        String remotePath = imageKitPath + (imgClass.equals(StoreImage.class)
+                ? "/store" : imgClass.equals(BeerImage.class)
+                ? "/beer" : null);
+        getFileListRequest.setPath(remotePath);
+        ResultList resultList = null;
+        try {
+            resultList = ImageKit.getInstance().getFileList(getFileListRequest);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        for (BaseFile baseFile : resultList.getResults()) {
+            if (baseFile.getName().equals(filename)) {
+                return baseFile.getFileId();
+            }
+        }
+        return null;
+    }
+
+    private void updateBeerImageRemoteId(List<BeerImage> beerImages) {
+        LOGGER.info("Updating 'beer_image' table with remote IDs...");
+        for (var image : beerImages) {
+            String sql = "UPDATE beer_image SET remote_id = ? WHERE beer_id = ?";
+            jdbcTemplate.update(sql, image.getRemoteId(), image.getId());
+        }
+    }
+
+    private void updateStoreImageRemoteId(List<StoreImage> storeImages) {
+        LOGGER.info("Updating 'store_image' table with remote IDs...");
+        for (var image : storeImages) {
+            String sql = "UPDATE store_image SET remote_id = ? WHERE store_name = ?";
+            jdbcTemplate.update(sql, image.getRemoteId(), image.getStoreName());
+        }
+    }
+
+    private void updateUrlsWithUpdatedAt(List<? extends ImageModel> images, String tableName, String idColumn) {
+        LOGGER.info("Updating '%s' table with 'updatedAt' key...".formatted(tableName));
+        for (var image : images) {
+            long updatedAt = getUpdatedAt(image.getRemoteId());
+            String newURL = image.getImageUrl() + "?updatedAt=" + updatedAt;
+            String sql = "UPDATE %s m SET url = ? WHERE %s = ?".formatted(tableName, idColumn);
+            jdbcTemplate.update(sql, newURL, image.getId());
+            image.setImageUrl(newURL);
+        }
+    }
+
+    private long getUpdatedAt(String fileId) {
+        try {
+            Result result = ImageKit.getInstance().getFileDetail(fileId);
+            return result.getUpdatedAt().toInstant().getEpochSecond();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- src/test/java/com/demo/alkolicznik/config/profiles/MainProfile.java
+++ src/test/java/com/demo/alkolicznik/config/profiles/MainProfile.java
@@ -1,183 +1,104 @@
 package com.demo.alkolicznik.config.profiles;
 
-import com.demo.alkolicznik.utils.mappers.DatabaseTableConverters;
 import com.demo.alkolicznik.models.Beer;
 import com.demo.alkolicznik.models.BeerPrice;
 import com.demo.alkolicznik.models.Store;
 import com.demo.alkolicznik.models.User;
-import com.demo.alkolicznik.models.image.BeerImage;
-import com.demo.alkolicznik.models.image.ImageModel;
-import com.demo.alkolicznik.models.image.StoreImage;
-import io.imagekit.sdk.ImageKit;
-import io.imagekit.sdk.models.results.Result;
+import com.demo.alkolicznik.utils.mappers.DatabaseTableConverters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
 import javax.sql.DataSource;
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
 @Profile("main")
+@PropertySource("classpath:application.properties")
 public class MainProfile {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(MainProfile.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainProfile.class);
 
-	@Autowired
-	private ApplicationContext context;
+    private static JdbcTemplate jdbcTemplate;
 
-	private static JdbcTemplate jdbcTemplate;
+    @Bean("jdbcTemplate")
+    @DependsOn("dataSource")
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        MainProfile.jdbcTemplate = new JdbcTemplate(dataSource);
+        return MainProfile.jdbcTemplate;
+    }
 
-	@Bean
-	public void setJdbcTemplate() {
-		MainProfile.jdbcTemplate = new JdbcTemplate(dataSource());
-	}
+    @Bean("stores")
+    public List<Store> stores() {
+        LOGGER.info("Creating 'stores' bean...");
+        String sql = "SELECT * FROM store";
+        List<Store> stores = DatabaseTableConverters.convertToStoreList(sql);
+        return stores;
+    }
 
-	@Bean
-	public List<Store> stores() {
-		LOGGER.info("Creating 'stores' bean...");
-		String sql = "SELECT * FROM store";
-		List<Store> stores = DatabaseTableConverters.convertToStoreList(sql);
-		return stores;
-	}
+    @Bean("beers")
+    public List<Beer> beers() {
+        LOGGER.info("Creating 'beers' bean...");
+        String sql = "SELECT * FROM beer";
+        List<Beer> beers = DatabaseTableConverters.convertToBeerList(sql);
+        return beers;
+    }
 
-	@Bean
-	public List<Beer> beers() {
-		LOGGER.info("Creating 'beers' bean...");
-		String sql = "SELECT * FROM beer";
-		List<Beer> beers = DatabaseTableConverters.convertToBeerList(sql);
-		return beers;
-	}
+    @Bean("beerPrices")
+    @DependsOn({"stores", "beers"})
+    public List<BeerPrice> beerPrices(List<Store> stores, List<Beer> beers) {
+        LOGGER.info("Creating 'beerPrices' bean...");
+        String sql = "SELECT * FROM beer_price WHERE beer_id = ? AND store_id = ?";
+        List<BeerPrice> beerPrices = DatabaseTableConverters
+                .convertToBeerPriceList(sql, beers, stores);
+        return beerPrices;
+    }
 
-	@Bean
-	@DependsOn("beers")
-	public List<BeerImage> beerImages() {
-		LOGGER.info("Creating 'beerImages' bean...");
-		List<Beer> beers = (List<Beer>) context.getBean("beers");
-		String sql = "SELECT * FROM beer_image WHERE beer_id = ?";
-		List<BeerImage> beerImages = DatabaseTableConverters
-				.convertToBeerImageList(sql, beers);
-		updateBeerImageRemoteId(beerImages);
-		updateUrlsWithUpdatedAt(beerImages, "beer_image", "beer_id");
-		return beerImages;
-	}
+    @Bean("users")
+    @ConditionalOnProperty(prefix = "security.config", name = "enabled", havingValue = "true", matchIfMissing = true)
+    public List<User> users() {
+        LOGGER.info("Creating 'users' bean...");
+        String sql = "SELECT * FROM users";
+        List<User> users = DatabaseTableConverters.convertToUserList(sql);
+        return users;
+    }
 
-	@Bean
-	@DependsOn("stores")
-	public List<StoreImage> storeImages() {
-		LOGGER.info("Creating 'storeImages' bean...");
-		List<Store> stores = (List<Store>) context.getBean("stores");
-		String sql = "SELECT * FROM store_image";
-		List<StoreImage> storeImages = DatabaseTableConverters
-				.convertToStoreImageList(sql, stores);
-		updateStoreImageRemoteId(storeImages);
-		updateUrlsWithUpdatedAt(storeImages, "store_image", "id");
-		return storeImages;
-	}
+    @Bean("imageKitPath")
+    @Primary
+    public String imageKitPath2() {
+        return "/test";
+    }
 
-	@Bean
-	@DependsOn({ "stores", "beers" })
-	public List<BeerPrice> beerPrices() {
-		LOGGER.info("Creating 'beerPrices' bean...");
-		List<Beer> beers = (List<Beer>) context.getBean("beers");
-		List<Store> stores = (List<Store>) context.getBean("stores");
-		String sql = "SELECT * FROM beer_price WHERE beer_id = ? AND store_id = ?";
-		List<BeerPrice> beerPrices = DatabaseTableConverters
-				.convertToBeerPriceList(sql, beers, stores);
-		return beerPrices;
-	}
+    @Bean("dataSource")
+    @ConditionalOnProperty(prefix = "database.table.image", name = "enabled", havingValue = "true")
+    public DataSource dataSource2() {
+        LOGGER.info("Loading data source with images included...");
+        return
+                (new EmbeddedDatabaseBuilder())
+                        .addScript("classpath:data_sql/schema.sql")
+                        .addScript("classpath:data_sql/beer-data.sql")
+                        .addScript("classpath:data_sql/store-data.sql")
+                        .addScript("classpath:data_sql/beer-price-data.sql")
+                        .addScript("classpath:data_sql/user-data.sql")
+                        .addScript("classpath:data_sql/image-data.sql")
+                        .build();
+    }
 
-	@Bean
-	public List<User> users() {
-		LOGGER.info("Creating 'users' bean...");
-		String sql = "SELECT * FROM users";
-		List<User> users = DatabaseTableConverters.convertToUserList(sql);
-		return users;
-	}
-
-	@Bean
-	@Primary
-	public String imageKitPath2() {
-		return "/test";
-	}
-
-	@Bean("dataSource")
-	@ConditionalOnProperty(prefix = "enable.image", name = "database", havingValue = "true")
-	public DataSource dataSource2() {
-		LOGGER.info("Loading data source with images included...");
-		return
-				(new EmbeddedDatabaseBuilder())
-						.addScript("classpath:data_sql/schema.sql")
-						.addScript("classpath:data_sql/beer-data.sql")
-						.addScript("classpath:data_sql/store-data.sql")
-						.addScript("classpath:data_sql/beer-price-data.sql")
-						.addScript("classpath:data_sql/user-data.sql")
-						.addScript("classpath:data_sql/image-data.sql")
-						.build();
-	}
-
-	@Bean("dataSource")
-	public DataSource dataSource() {
-		LOGGER.info("Loading data source with images excluded...");
-		return
-				(new EmbeddedDatabaseBuilder())
-						.addScript("classpath:data_sql/schema.sql")
-						.addScript("classpath:data_sql/beer-data.sql")
-						.addScript("classpath:data_sql/store-data.sql")
-						.addScript("classpath:data_sql/beer-price-data.sql")
-						.addScript("classpath:data_sql/user-data.sql")
-						.build();
-	}
-
-	@Bean("correctPasswords")
-	public List<String> correctPasswords() {
-//		LOGGER.info("Creating 'correctPasswords' bean...");
-		return Arrays.asList("kl;jdvba;gbirjea", "3rt90qw4gmkvsvr",
-				"ojpeaipqe4903-qAP[WC", "IJWQ[O;EJFIVKvjifdibs3", "2jiof43qpv4kcvlsA",
-				"dsamkfaiovero33", "FOKJp[ewc[vrewvrv", "j39dasvp4q2adcfrvbEWSF",
-				"32dsajivq4oipvfeWK");
-	}
-
-	private void updateBeerImageRemoteId(List<BeerImage> beerImages) {
-		LOGGER.info("Updating 'beer_image' table with remote IDs...");
-		for (var image : beerImages) {
-			String sql = "UPDATE beer_image SET remote_id = ? WHERE beer_id = ?";
-			jdbcTemplate.update(sql, image.getRemoteId(), image.getId());
-		}
-	}
-
-	private void updateStoreImageRemoteId(List<StoreImage> storeImages) {
-		LOGGER.info("Updating 'store_image' table with remote IDs...");
-		for (var image : storeImages) {
-			String sql = "UPDATE store_image SET remote_id = ? WHERE store_name = ?";
-			jdbcTemplate.update(sql, image.getRemoteId(), image.getStoreName());
-		}
-	}
-
-	private void updateUrlsWithUpdatedAt(List<? extends ImageModel> images, String tableName, String idColumn) {
-		LOGGER.info("Updating '%s' table with 'updatedAt' key...".formatted(tableName));
-		for (var image : images) {
-			long updatedAt = getUpdatedAt(image.getRemoteId());
-			String newURL = image.getImageUrl() + "?updatedAt=" + updatedAt;
-			String sql = "UPDATE %s m SET url = ? WHERE %s = ?".formatted(tableName, idColumn);
-			jdbcTemplate.update(sql, newURL, image.getId());
-			image.setImageUrl(newURL);
-		}
-	}
-
-	private long getUpdatedAt(String fileId) {
-		try {
-			Result result = ImageKit.getInstance().getFileDetail(fileId);
-			return result.getUpdatedAt().toInstant().getEpochSecond();
-		}
-		catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+    @Bean("dataSource")
+    @ConditionalOnMissingBean(DataSource.class)
+    public DataSource dataSource() {
+        LOGGER.info("Loading data source with images excluded...");
+        return
+                (new EmbeddedDatabaseBuilder())
+                        .addScript("classpath:data_sql/schema.sql")
+                        .addScript("classpath:data_sql/beer-data.sql")
+                        .addScript("classpath:data_sql/store-data.sql")
+                        .addScript("classpath:data_sql/beer-price-data.sql")
+                        .addScript("classpath:data_sql/user-data.sql")
+                        .build();
+    }
 }
