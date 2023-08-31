10901bb7f60c890123ffaa2707766849d4e7f06f
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-31 15:16:18 +0200

    Adjust error response on wrong credentials when authenticating

--- src/main/java/com/demo/alkolicznik/exceptions/CustomExceptionHandler.java
+++ src/main/java/com/demo/alkolicznik/exceptions/CustomExceptionHandler.java
@@ -1,17 +1,12 @@
 package com.demo.alkolicznik.exceptions;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,6 +14,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 
 @RestControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
@@ -29,18 +29,31 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
                                                                   HttpStatusCode status,
                                                                   WebRequest request) {
         String message = getMessage(e.getFieldErrors());
-        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+        String path = getPath(request);
         ApiException error = new ApiException(HttpStatus.BAD_REQUEST, message, path);
         return ResponseEntity.badRequest().body(error);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ApiException> handleConstraintViolationException(ConstraintViolationException e,
-                                                                     WebRequest request) {
+                                                                           WebRequest request) {
         String message = getMessage(e.getConstraintViolations());
-        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+        String path = getPath(request);
         ApiException error = new ApiException(HttpStatus.BAD_REQUEST, message, path);
         return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiException> handleBadCredentialsException(BadCredentialsException e,
+                                                                      WebRequest request) {
+        String message = "Could not log in: wrong credentials";
+        String path = getPath(request);
+        ApiException error = new ApiException(HttpStatus.NOT_FOUND, message, path);
+        return ResponseEntity.status(404).body(error);
+    }
+
+    private String getPath(WebRequest request) {
+        return ((ServletWebRequest) request).getRequest().getRequestURI();
     }
 
     private String getMessage(List<FieldError> violations) {
@@ -58,7 +71,7 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     private String getMessage(Set<ConstraintViolation<?>> violations) {
         List<String> messages = new ArrayList<>();
         var iterator = violations.stream().iterator();
-        while(iterator.hasNext()) {
+        while (iterator.hasNext()) {
             var violation = iterator.next();
             messages.add(violation.getMessage());
         }
