37f1ab91312058d365967cef17a153db777724e8
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-06 10:28:48 +0200

    Create 2 separate image response dtos for both beer and store model

---
+++ src/main/java/com/demo/alkolicznik/dto/image/BeerImageResponseDTO.java
@@ -0,0 +1,38 @@
+package com.demo.alkolicznik.dto.image;
+
+import com.demo.alkolicznik.models.image.BeerImage;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.*;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@JsonPropertyOrder({"beer_name", "remote_id", "url"})
+public class BeerImageResponseDTO {
+
+    @JsonProperty("beer_name")
+    private String beerFullName;
+    @JsonProperty("remote_id")
+    private String remoteId;
+    @JsonProperty("url")
+    private String url;
+
+    public BeerImageResponseDTO(BeerImage beerImage) {
+        this.beerFullName = beerImage.getBeer().getFullName();
+        this.remoteId = beerImage.getRemoteId();
+        this.url = beerImage.getImageUrl();
+    }
+
+    public static List<BeerImageResponseDTO> asList(Collection<BeerImage> beerImages) {
+        return beerImages.stream()
+                .map(BeerImageResponseDTO::new)
+                .toList();
+    }
+}
---
+++ src/main/java/com/demo/alkolicznik/dto/image/StoreImageResponseDTO.java
@@ -0,0 +1,38 @@
+package com.demo.alkolicznik.dto.image;
+
+import com.demo.alkolicznik.models.image.StoreImage;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.*;
+
+import java.util.Collection;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@JsonPropertyOrder({"store_name", "remote_id", "url"})
+public class StoreImageResponseDTO {
+
+    @JsonProperty("store_name")
+    private String storeName;
+    @JsonProperty("remote_id")
+    private String remoteId;
+    @JsonProperty("url")
+    private String url;
+
+    public StoreImageResponseDTO(StoreImage storeImage) {
+        this.storeName = storeImage.getStoreName();
+        this.remoteId = storeImage.getRemoteId();
+        this.url = storeImage.getImageUrl();
+    }
+
+    public static List<StoreImageResponseDTO> asList(Collection<StoreImage> storeImages) {
+        return storeImages.stream()
+                .map(StoreImageResponseDTO::new)
+                .toList();
+    }
+}
