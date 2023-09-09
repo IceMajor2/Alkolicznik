2dd3942d31a3a7b03998d3cab10fde20385fbb82
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-09 18:46:51 +0200

    Add 'home' button

--- src/main/java/com/demo/alkolicznik/gui/auth/LoginView.java
+++ src/main/java/com/demo/alkolicznik/gui/auth/LoginView.java
@@ -7,27 +7,43 @@ import com.demo.alkolicznik.utils.request.CookieUtils;
 import com.demo.alkolicznik.utils.request.RequestUtils;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.login.AbstractLogin;
 import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.VaadinService;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
 import org.springframework.http.HttpMethod;
 
 @PageTitle("Login | Alkolicznik")
 @Route(value = "login")
+@AnonymousAllowed
 public class LoginView extends VerticalLayout {
 
+    private VerticalLayout loginWrapper;
     private LoginForm loginForm;
 
-    public LoginView() {
-        setSizeFull();
-        setAlignItems(Alignment.CENTER);
-        setJustifyContentMode(JustifyContentMode.CENTER);
+    private HorizontalLayout toolbar;
 
-        this.loginForm = configureLoginForm();
-        add(loginForm);
+    public LoginView() {
+        toolbar = getToolbar();
+
+        loginForm = configureLoginForm();
+        loginWrapper = getLoginFormWrapped();
+
+        add(toolbar, loginWrapper);
+    }
+    
+    private VerticalLayout getLoginFormWrapped() {
+        VerticalLayout verticalLayout = new VerticalLayout();
+        verticalLayout.setSizeFull();
+        verticalLayout.setAlignItems(Alignment.CENTER);
+
+        verticalLayout.add(loginForm);
+        return verticalLayout;
     }
 
     private LoginForm configureLoginForm() {
@@ -35,6 +51,16 @@ public class LoginView extends VerticalLayout {
         loginForm.addLoginListener(loginEvent());
         loginForm.setForgotPasswordButtonVisible(false);
         return loginForm;
+    }
+
+    private HorizontalLayout getToolbar() {
+        HorizontalLayout horizontalLayout = new HorizontalLayout();
+        horizontalLayout.setWidthFull();
+        horizontalLayout.setJustifyContentMode(JustifyContentMode.END);
+
+        Button home = new Button("Home", event -> UI.getCurrent().navigate("/"));
+        horizontalLayout.add(home);
+        return horizontalLayout;
     }
 
     private ComponentEventListener<AbstractLogin.LoginEvent> loginEvent() {
