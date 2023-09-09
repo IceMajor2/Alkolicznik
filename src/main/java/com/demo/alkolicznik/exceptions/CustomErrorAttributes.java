ce515ce5b56d5b2226a70de1b643f3ea10c5f5e8
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-10 00:29:55 +0200

    Override error-attributes-creating method to standardize error's through application

---
+++ src/main/java/com/demo/alkolicznik/exceptions/CustomErrorAttributes.java
@@ -0,0 +1,27 @@
+package com.demo.alkolicznik.exceptions;
+
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+@Component
+public class CustomErrorAttributes extends DefaultErrorAttributes {
+
+    @Override
+    public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
+        Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, options);
+        errorAttributes.put("timestamp", getStringRepresentationOfDate());
+        return errorAttributes;
+    }
+
+    private String getStringRepresentationOfDate() {
+        LocalDateTime dateTime = LocalDateTime.now(Clock.systemUTC());
+        return dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+}
