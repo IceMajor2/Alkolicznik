9b36eabc79ab60d187a1633128f54656714d3341
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-09 14:37:58 +0200

    Add a comment explanation; change the way date is formatted

--- src/main/java/com/demo/alkolicznik/exceptions/ApiException.java
+++ src/main/java/com/demo/alkolicznik/exceptions/ApiException.java
@@ -1,5 +1,6 @@
 package com.demo.alkolicznik.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIncludeProperties;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
@@ -8,17 +9,19 @@ import lombok.Setter;
 import lombok.ToString;
 import org.springframework.http.HttpStatus;
 
-import java.time.ZoneId;
+import java.time.Clock;
 import java.time.ZonedDateTime;
 
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
+// the annotation below prevents from showing additional fields of 'RuntimeException' class
 @JsonIncludeProperties({"timestamp", "status", "error", "message", "path"})
 @JsonPropertyOrder({"timestamp", "status", "error", "message", "path"})
 public class ApiException extends RuntimeException {
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private ZonedDateTime timestamp;
     private int status;
     private String error;
@@ -26,8 +29,7 @@ public class ApiException extends RuntimeException {
     private String path;
 
     public ApiException(HttpStatus status, String message, String path) {
-		// TODO: adjust zone id to user
-		this.timestamp = ZonedDateTime.now(ZoneId.of("Poland"));
+		this.timestamp = ZonedDateTime.now(Clock.systemUTC());
         this.status = status.value();
         this.error = status.getReasonPhrase();
         this.message = message;
