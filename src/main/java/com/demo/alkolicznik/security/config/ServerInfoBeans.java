f6037914e8a8a6843d922fe6cdfd57d0c5ccce81
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-09 16:14:00 +0200

    Create a class with a few beans that provide information about server's properties

---
+++ src/main/java/com/demo/alkolicznik/security/config/ServerInfoBeans.java
@@ -0,0 +1,40 @@
+package com.demo.alkolicznik.security.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Component
+public class ServerInfoBeans {
+
+    @Bean
+    public String baseUrl(String schema, String ipAddress, int serverPort) {
+        return schema + "://" + ipAddress + ":" + serverPort;
+    }
+
+    @Bean
+    public String ipAddress() throws UnknownHostException {
+        return InetAddress.getLocalHost().getHostAddress();
+    }
+
+    @Bean
+    public int serverPort(ServerProperties serverProperties) {
+        return serverProperties.getPort();
+    }
+
+    @Bean("schema")
+    @ConditionalOnProperty(prefix = "server.ssl", name = "enabled", havingValue = "true", matchIfMissing = false)
+    public String schema() {
+        return "https";
+    }
+
+    @Bean("schema")
+    @ConditionalOnProperty(prefix = "server.ssl", name = "enabled", havingValue = "false", matchIfMissing = true)
+    public String schema2() {
+        return "http";
+    }
+}
