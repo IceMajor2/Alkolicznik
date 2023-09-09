2f337029c181ae8b2b61980ea1a148ae1c8d99f8
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-10 00:30:11 +0200

    Change date mapping

--- src/main/java/com/demo/alkolicznik/exceptions/ApiException.java
+++ src/main/java/com/demo/alkolicznik/exceptions/ApiException.java
@@ -1,16 +1,17 @@
 package com.demo.alkolicznik.exceptions;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIncludeProperties;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 import java.time.Clock;
-import java.time.ZonedDateTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @NoArgsConstructor
 @Getter
@@ -21,15 +22,15 @@ import java.time.ZonedDateTime;
 @JsonPropertyOrder({"timestamp", "status", "error", "message", "path"})
 public class ApiException extends RuntimeException {
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
-    private ZonedDateTime timestamp;
+    private String timestamp;
     private int status;
     private String error;
     private String message;
     private String path;
 
+    @Autowired
     public ApiException(HttpStatus status, String message, String path) {
-		this.timestamp = ZonedDateTime.now(Clock.systemUTC());
+        this.timestamp = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now(Clock.systemUTC()));
         this.status = status.value();
         this.error = status.getReasonPhrase();
         this.message = message;
