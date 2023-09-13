752ab26966d8bfcfe8fb19323d95ad2b3f8ce69f
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-13 10:40:42 +0200

    Divide TestUtils into FileUtils

---
+++ src/test/java/com/demo/alkolicznik/utils/FileUtils.java
@@ -0,0 +1,93 @@
+package com.demo.alkolicznik.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class FileUtils {
+
+    private static ResourceLoader resourceLoader;
+
+    @Autowired
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        FileUtils.resourceLoader = resourceLoader;
+    }
+    public static BufferedImage getBufferedImageFromWeb(String url) {
+        try {
+            return ImageIO.read(new URL(url));
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    public static BufferedImage getBufferedImageFromLocal(String path) {
+        try {
+            return ImageIO.read(new File(path));
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    public static boolean imageEquals(BufferedImage imgA, BufferedImage imgB) {
+        if (!dimensionsSame(imgA, imgB)) return false;
+        int width = imgA.getWidth();
+        int height = imgA.getHeight();
+        // Loop over every pixel.
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                // Compare the pixels for equality.
+                if (imgA.getRGB(x, y) != imgB.getRGB(x, y)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean dimensionsSame(BufferedImage imgA, BufferedImage imgB) {
+        return imgA.getWidth() == imgB.getWidth() && imgA.getHeight() == imgB.getHeight();
+    }
+
+    public static String getRawPathToClassPathResource(String resource) {
+        URI uri = null;
+        try {
+            uri = resourceLoader.getResource("classpath:" + resource).getURI();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return Paths.get(uri).toAbsolutePath().toString();
+    }
+
+    public static String getRawPathToImage(String imageFilename) {
+        URI uri = null;
+        try {
+            uri = resourceLoader.getResource("classpath:data_img/" + imageFilename).getURI();
+        } catch (IOException e) {
+            try {
+                uri = resourceLoader.getResource("classpath:data_img").getURI();
+                return Paths.get(uri).toAbsolutePath().toString() + '\\' + imageFilename;
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        String rawPath = Paths.get(uri).toAbsolutePath().toString();
+        return rawPath;
+    }
+
+    public static List<String> convertToFilenamesList(File[] files) {
+        return Arrays.stream(files)
+                .map(File::getName)
+                .toList();
+    }
+}
--- src/test/java/com/demo/alkolicznik/utils/TestUtils.java
+++ src/test/java/com/demo/alkolicznik/utils/TestUtils.java
@@ -2,29 +2,11 @@ package com.demo.alkolicznik.utils;
 
 import com.demo.alkolicznik.utils.request.CookieUtils;
 import jakarta.servlet.http.Cookie;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URL;
-import java.nio.file.Paths;
 import java.util.Map;
 
-@Component
 public class TestUtils {
-
-    private static ResourceLoader resourceLoader;
-
-    @Autowired
-    public void setResourceLoader(ResourceLoader resourceLoader) {
-        TestUtils.resourceLoader = resourceLoader;
-    }
 
     public static Cookie createTokenCookie(String token) {
         return CookieUtils.createTokenCookie(token);
@@ -38,68 +20,6 @@ public class TestUtils {
         }
         String urlTemplate = builder.encode().toUriString();
         return urlTemplate;
-    }
-
-    public static BufferedImage getBufferedImageFromWeb(String url) {
-        try {
-            return ImageIO.read(new URL(url));
-        } catch (IOException e) {
-            return null;
-        }
-    }
-
-    public static BufferedImage getBufferedImageFromLocal(String path) {
-        try {
-            return ImageIO.read(new File(path));
-        } catch (IOException e) {
-            return null;
-        }
-    }
-
-    public static boolean imageEquals(BufferedImage imgA, BufferedImage imgB) {
-        if (!dimensionsSame(imgA, imgB)) return false;
-        int width = imgA.getWidth();
-        int height = imgA.getHeight();
-        // Loop over every pixel.
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                // Compare the pixels for equality.
-                if (imgA.getRGB(x, y) != imgB.getRGB(x, y)) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    public static boolean dimensionsSame(BufferedImage imgA, BufferedImage imgB) {
-        return imgA.getWidth() == imgB.getWidth() && imgA.getHeight() == imgB.getHeight();
-    }
-
-    public static String getRawPathToClassPathResource(String resource) {
-        URI uri = null;
-        try {
-            uri = resourceLoader.getResource("classpath:" + resource).getURI();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return Paths.get(uri).toAbsolutePath().toString();
-    }
-
-    public static String getRawPathToImage(String imageFilename) {
-        URI uri = null;
-        try {
-            uri = resourceLoader.getResource("classpath:data_img/" + imageFilename).getURI();
-        } catch (IOException e) {
-            try {
-                uri = resourceLoader.getResource("classpath:data_img").getURI();
-                return Paths.get(uri).toAbsolutePath().toString() + '\\' + imageFilename;
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
-        String rawPath = Paths.get(uri).toAbsolutePath().toString();
-        return rawPath;
     }
 
     public static String removeTransformationFromURL(String url) {
