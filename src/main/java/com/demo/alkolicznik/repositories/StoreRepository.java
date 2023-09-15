f90b130f1e3a45b116997a1bc0f4ba39dc16233a
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-15 15:56:18 +0200

    Reorder methods (aesthetics) and create new method

--- src/main/java/com/demo/alkolicznik/repositories/StoreRepository.java
+++ src/main/java/com/demo/alkolicznik/repositories/StoreRepository.java
@@ -1,6 +1,5 @@
 package com.demo.alkolicznik.repositories;
 
-import com.demo.alkolicznik.dto.store.StoreRequestDTO;
 import com.demo.alkolicznik.models.Store;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -8,52 +7,52 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 public interface StoreRepository extends CrudRepository<Store, Long> {
 
-	@Query("SELECT s FROM Store s LEFT JOIN FETCH s.prices ORDER BY s.id ASC")
-	List<Store> findAllByOrderByIdAsc();
+    @Query("SELECT s FROM Store s LEFT JOIN FETCH s.prices ORDER BY s.id ASC")
+    List<Store> findAllByOrderByIdAsc();
 
-	@Query("SELECT s FROM Store s LEFT JOIN FETCH s.prices")
-	List<Store> findAll();
+    @Query("SELECT s FROM Store s LEFT JOIN FETCH s.prices")
+    List<Store> findAll();
 
-	@Query("SELECT s FROM Store s LEFT JOIN FETCH s.prices WHERE s.name = :name")
-	Set<Store> findAllByName(@Param("name") String name);
+    @Query("SELECT s FROM Store s LEFT JOIN FETCH s.prices "
+            + "WHERE s.city = :city ORDER BY s.id ASC")
+    List<Store> findAllByCityOrderByIdAsc(@Param("city") String city);
 
-	@Query("SELECT s FROM Store s LEFT JOIN FETCH s.prices "
-			+ "WHERE s.city = :city ORDER BY s.id ASC")
-	List<Store> findAllByCityOrderByIdAsc(@Param("city") String city);
+    @Query("SELECT s FROM Store s LEFT JOIN FETCH s.prices WHERE s.id = :id")
+    Optional<Store> findById(@Param("id") Long id);
 
-	@Query("SELECT s FROM Store s LEFT JOIN FETCH s.prices WHERE s.id = :id")
-	Optional<Store> findById(@Param("id") Long id);
+    @Query("SELECT s FROM Store s LEFT JOIN FETCH s.prices "
+            + "WHERE s.name = ?1 AND s.city = ?2 AND s.street = ?3")
+    Optional<Store> findByNameAndCityAndStreet(String name, String city, String street);
 
-	@Query("SELECT s FROM Store s LEFT JOIN FETCH s.prices "
-			+ "WHERE s.name = ?1 AND s.city = ?2 AND s.street = ?3")
-	Optional<Store> findByNameAndCityAndStreet(String name, String city, String street);
+    @Query("SELECT DISTINCT(s.name) FROM Store s")
+    List<String> findDistinctNames();
 
-	@Query("SELECT DISTINCT(s.name) FROM Store s")
-	List<String> findDistinctNames();
+    @Query("SELECT DISTINCT(s.city) FROM Store s")
+    List<String> findDistinctCities();
 
-	@Query("SELECT DISTINCT(s.city) FROM Store s")
-	List<String> findDistinctCities();
+    long countByName(String name);
 
-	long countByName(String name);
+    default Optional<Store> find(Store store) {
+        return findByNameAndCityAndStreet(store.getName(),
+                store.getCity(),
+                store.getStreet());
+    }
 
-	default Optional<Store> findByStoreRequest(StoreRequestDTO storeRequestDTO) {
-		return findByNameAndCityAndStreet(storeRequestDTO.getName(),
-				storeRequestDTO.getCity(),
-				storeRequestDTO.getStreet());
-	}
+    boolean existsByCity(String city);
 
-	boolean existsByCity(String city);
+    boolean existsByNameAndCityAndStreet(String name, String city, String street);
 
-	boolean existsByNameAndCityAndStreet(String name, String city, String street);
+    boolean existsByName(String name);
 
-	boolean existsByName(String name);
+    default boolean exists(Store store) {
+        return existsByNameAndCityAndStreet
+                (store.getName(), store.getCity(), store.getStreet());
+    }
 
-	default boolean exists(Store store) {
-		return existsByNameAndCityAndStreet
-				(store.getName(), store.getCity(), store.getStreet());
-	}
+    default boolean isNameUnique(String name) {
+        return countByName(name) == 1;
+    }
 }
