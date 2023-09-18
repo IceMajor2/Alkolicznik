357581b477115721ed1febbe7f4255b698e11349
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-18 08:36:57 +0200

    Configure ExceptionResolver not to log specified exception classes

---
+++ src/main/java/com/demo/alkolicznik/security/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.demo.alkolicznik.security.config;
+
+import com.demo.alkolicznik.exceptions.CustomResponseStatusExceptionResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.annotation.ResponseStatusExceptionResolver;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private ResponseStatusExceptionResolver exceptionResolver;
+
+    @Autowired
+    public WebConfig(CustomResponseStatusExceptionResolver exceptionResolver) {
+        this.exceptionResolver = exceptionResolver;
+    }
+
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(0, exceptionResolver);
+    }
+}
