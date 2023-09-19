a8f8da65d89425c132d7b1c7e6de39495956fd38
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-19 17:42:49 +0200

    Rename package with test images + remove SQL script for persisting image data at test start

--- src/test/resources/data_sql/image-data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO beer_image (BEER_ID, URL) VALUES (3, 'https://ik.imagekit.io/alkolicznik/tr:n-get_beer/test/beer/tyskie-gronie-0.65.png');
-INSERT INTO beer_image (BEER_ID, URL) VALUES (4, 'https://ik.imagekit.io/alkolicznik/tr:n-get_beer/test/beer/zubr-0.5.png');
-INSERT INTO beer_image (BEER_ID, URL) VALUES (5, 'https://ik.imagekit.io/alkolicznik/tr:n-get_beer/test/beer/komes-porter-malinowy-0.33.jpg');
-INSERT INTO beer_image (BEER_ID, URL) VALUES (6, 'https://ik.imagekit.io/alkolicznik/tr:n-get_beer/test/beer/miloslaw-biale-0.5.jpg');
-
-INSERT INTO store_image (ID, STORE_NAME, URL) VALUES (1, 'ABC', 'https://ik.imagekit.io/alkolicznik/test/store/abc.png');
-INSERT INTO store_image (ID, STORE_NAME, URL) VALUES (2, 'Lubi', 'https://ik.imagekit.io/alkolicznik/test/store/lubi.jpg');
-INSERT INTO store_image (ID, STORE_NAME, URL) VALUES (3, 'Carrefour', 'https://ik.imagekit.io/alkolicznik/test/store/carrefour.png');
