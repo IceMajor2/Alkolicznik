5955d36a9c6b9a90954d39cd2d24449c93efd4b1
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-19 17:47:29 +0200

    Make 'image' profile load and reload data differently. 	- URL is not loaded through SQL script anymore; instead close-to-production logic is used: image tables are updated programmatically 	- add another layer for 'image' profile initialization logic - ImageDataReloader is used for delegating the dirty work to ImageKitRequests and directly updating db tables

--- src/test/java/com/demo/alkolicznik/utils/requests/ImageKitRequests.java
+++ src/test/java/com/demo/alkolicznik/utils/requests/ImageKitRequests.java
@@ -1,15 +1,11 @@
 package com.demo.alkolicznik.utils.requests;
 
-import com.demo.alkolicznik.models.image.BeerImage;
-import com.demo.alkolicznik.models.image.ImageModel;
-import com.demo.alkolicznik.models.image.StoreImage;
 import io.imagekit.sdk.ImageKit;
 import io.imagekit.sdk.exceptions.*;
 import io.imagekit.sdk.models.BaseFile;
 import io.imagekit.sdk.models.FileCreateRequest;
 import io.imagekit.sdk.models.GetFileListRequest;
 import io.imagekit.sdk.models.results.Result;
-import io.imagekit.sdk.models.results.ResultList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,17 +16,12 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static com.demo.alkolicznik.utils.FileUtils.getRawPathToClassPathResource;
 
 @Component
 public class ImageKitRequests {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ImageKitRequests.class);
 
-    private static ImageKit imageKit = ImageKit.getInstance();
     private static String imageKitPath;
 
     @Autowired
@@ -38,75 +29,33 @@ public class ImageKitRequests {
         ImageKitRequests.imageKitPath = env.getProperty("imageKit.path");
     }
 
-    /**
-     * Deletes all files in an ImageKit's directory.
-     * You can choose to omit deleting some files by specifying a
-     * boolean parameter and a list of filenames.
-     *
-     * @param path          remote path whose contents are to be deleted
-     * @param deleteAll     if true, will empty the directory
-     * @param omitFilenames list of filenames that are not to be deleted
-     *                      (if {@code deleteAll} was true, then it is ignored)
-     */
-    public static void deleteFilesIn(String path, boolean deleteAll, List<String> omitFilenames) throws ForbiddenException, TooManyRequestsException, InternalServerException, UnauthorizedException, BadRequestException, UnknownException, IllegalAccessException, InstantiationException {
+    public static List<BaseFile> getDirectory(String path) throws ForbiddenException, TooManyRequestsException, InternalServerException, UnauthorizedException, BadRequestException, UnknownException, IllegalAccessException, InstantiationException {
         GetFileListRequest getFileListRequest = new GetFileListRequest();
         getFileListRequest.setPath(path);
-        ResultList resultList = imageKit.getFileList(getFileListRequest);
-
-        for (BaseFile baseFile : resultList.getResults()) {
-            if (!deleteAll && (omitFilenames != null && (omitFilenames.contains(baseFile.getName())))) {
-                LOGGER.info("No need to DELETE. '%s' was found (ID: %s)"
-                        .formatted(baseFile.getName(), baseFile.getFileId()));
-                continue;
-            }
-            LOGGER.info("Deleting... '%s'"
-                    .formatted(baseFile.getName()));
-            imageKit.deleteFile(baseFile.getFileId());
-        }
+        return ImageKit.getInstance().getFileList(getFileListRequest).getResults();
     }
 
-    public static void sendImages(String srcPath, String remotePath) throws ForbiddenException, TooManyRequestsException, InternalServerException, UnauthorizedException, BadRequestException, UnknownException, IOException, IllegalAccessException, InstantiationException {
-        GetFileListRequest getFileListRequest = new GetFileListRequest();
-        getFileListRequest.setPath(remotePath);
-        Map<String, String> baseFiles = imageKit.getFileList(getFileListRequest)
-                .getResults()
-                .stream()
-                .collect(Collectors.toMap(BaseFile::getName, BaseFile::getFileId));
-        File[] testDir =
-                new File(getRawPathToClassPathResource(srcPath)).listFiles();
-
-        for (File image : testDir) {
-            if (baseFiles.containsKey(image.getName())) {
-                continue;
-            }
-            byte[] bytes = Files.readAllBytes(image.toPath());
-            FileCreateRequest fileCreateRequest = new FileCreateRequest(bytes, image.getName());
-            fileCreateRequest.setUseUniqueFileName(false);
-            fileCreateRequest.setFolder(remotePath);
-            Result result = imageKit.upload(fileCreateRequest);
-            LOGGER.info("'%s' was successfully sent (ID: %s)"
-                    .formatted(result.getName(), result.getFileId()));
+    public static Result upload(String srcPath, String remotePath) throws ForbiddenException, TooManyRequestsException, InternalServerException, UnauthorizedException, BadRequestException, UnknownException, IOException, IllegalAccessException, InstantiationException {
+        File toSend = new File(srcPath);
+        List<String> baseFiles = getDirectory(remotePath).stream()
+                .map(BaseFile::getName)
+                .toList();
+        if (baseFiles.contains(toSend.getName())) {
+            return null;
         }
+        return send(toSend, remotePath);
     }
 
-    public static String getRemoteId(String filename, Class<? extends ImageModel> imgClass) {
-        GetFileListRequest getFileListRequest = new GetFileListRequest();
-        String remotePath = imageKitPath + (imgClass.equals(StoreImage.class)
-                ? "/store" : imgClass.equals(BeerImage.class)
-                ? "/beer" : null);
-        getFileListRequest.setPath(remotePath);
-        ResultList resultList = null;
-        try {
-            resultList = ImageKit.getInstance().getFileList(getFileListRequest);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        for (BaseFile baseFile : resultList.getResults()) {
-            if (baseFile.getName().equals(filename)) {
-                return baseFile.getFileId();
-            }
-        }
-        return null;
+    public static Result send(File file, String remotePath) throws IOException, ForbiddenException, TooManyRequestsException, InternalServerException, UnauthorizedException, BadRequestException, UnknownException {
+        byte[] bytes = Files.readAllBytes(file.toPath());
+        FileCreateRequest fileCreateRequest = new FileCreateRequest(bytes, file.getName());
+        fileCreateRequest.setUseUniqueFileName(false);
+        fileCreateRequest.setFolder(remotePath);
+        return ImageKit.getInstance().upload(fileCreateRequest);
+    }
+
+    public static void delete(String fileId) throws ForbiddenException, TooManyRequestsException, InternalServerException, UnauthorizedException, BadRequestException, UnknownException {
+        ImageKit.getInstance().deleteFile(fileId);
     }
 
     public static long getUpdatedAt(String fileId) {
