52d6d91f38a3d76f101727df208e2df2fa57b10c
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-20 16:51:38 +0200

    Remove PWA

--- src/main/java/com/demo/alkolicznik/AlkolicznikApplication.java
+++ src/main/java/com/demo/alkolicznik/AlkolicznikApplication.java
@@ -1,19 +1,12 @@
 package com.demo.alkolicznik;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Theme(value = "style")
-@PWA(
-        name = "Alkolicznik",
-        shortName = "Alkolicznik",
-        offlinePath = "offline.html",
-        offlineResources = {"images/drunk_man.png", "icon/icon.png"}
-)
 public class AlkolicznikApplication implements AppShellConfigurator {
 
     public static void main(String[] args) {
