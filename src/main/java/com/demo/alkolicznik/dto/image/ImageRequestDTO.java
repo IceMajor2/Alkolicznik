01fb828209829de4271b6374f4aa329c32e276a1
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-20 12:26:58 +0200

    Lombok refactor + change JSON property name

--- src/main/java/com/demo/alkolicznik/dto/image/ImageRequestDTO.java
+++ src/main/java/com/demo/alkolicznik/dto/image/ImageRequestDTO.java
@@ -1,15 +1,15 @@
 package com.demo.alkolicznik.dto.image;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
-@ToString
+@Data
 public class ImageRequestDTO {
 
 	@NotBlank
+	@JsonProperty("image_path")
 	private String imagePath;
 }
