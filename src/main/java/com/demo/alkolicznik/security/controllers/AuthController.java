efd66aa18e82400f0c2f546d64904a1f7554012e
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-21 14:06:58 +0200

    Create documentation for register endpoint

--- src/main/java/com/demo/alkolicznik/security/controllers/AuthController.java
+++ src/main/java/com/demo/alkolicznik/security/controllers/AuthController.java
@@ -3,11 +3,17 @@ package com.demo.alkolicznik.security.controllers;
 import com.demo.alkolicznik.dto.security.*;
 import com.demo.alkolicznik.security.services.AuthService;
 import com.demo.alkolicznik.utils.request.CookieUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +31,41 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/signup")
+    @Operation(
+            summary = "Register an account",
+            description =
+                    "Create account for this application." +
+                    "<br><b>CONSTRAINTS:</b><br>" +
+                    "- password must be at least 12 characters long<br>" +
+                    "- username must not exist in database",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            examples = @ExampleObject(
+                                    value = "{\"username\":\"john\",\"password\":\"twelve_chars_long\"}"
+                            )
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "account was registered",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = SignupResponseDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "validation failed",
+                            content = @Content
+                    ),
+                    @ApiResponse(
+                            responseCode = "409",
+                            description = "username is taken",
+                            content = @Content
+                    )
+            }
+    )
     public ResponseEntity<SignupResponseDTO> signup
             (@RequestBody @Valid SignupRequestDTO request) {
         SignupResponseDTO response = authService.register(request);
