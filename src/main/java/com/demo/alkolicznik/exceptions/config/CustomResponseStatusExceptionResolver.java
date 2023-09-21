63e4f5061b38b86cdf4c948bf294f573f299cf64
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-21 14:57:18 +0200

    Do not log NoLoggedInUserException

--- src/main/java/com/demo/alkolicznik/exceptions/config/CustomResponseStatusExceptionResolver.java
+++ src/main/java/com/demo/alkolicznik/exceptions/config/CustomResponseStatusExceptionResolver.java
@@ -10,6 +10,7 @@ import com.demo.alkolicznik.exceptions.classes.city.NoSuchCityException;
 import com.demo.alkolicznik.exceptions.classes.image.ImageNotFoundException;
 import com.demo.alkolicznik.exceptions.classes.store.StoreAlreadyExistsException;
 import com.demo.alkolicznik.exceptions.classes.store.StoreNotFoundException;
+import com.demo.alkolicznik.exceptions.classes.user.NoLoggedInUserException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.annotation.ResponseStatusExceptionResolver;
@@ -31,6 +32,7 @@ public class CustomResponseStatusExceptionResolver extends ResponseStatusExcepti
         if (ex instanceof PropertiesMissingException) return;
         if (ex instanceof ObjectsAreEqualException) return;
         if (ex instanceof ImageNotFoundException) return;
+        if (ex instanceof NoLoggedInUserException) return;
         super.logException(ex, request);
     }
 }
