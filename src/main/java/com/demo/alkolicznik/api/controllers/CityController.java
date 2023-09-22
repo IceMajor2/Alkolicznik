618174355908c67f4267246327d5696fe60a66e3
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-22 11:38:06 +0200

    Add documentation

--- src/main/java/com/demo/alkolicznik/api/controllers/CityController.java
+++ src/main/java/com/demo/alkolicznik/api/controllers/CityController.java
@@ -2,6 +2,10 @@ package com.demo.alkolicznik.api.controllers;
 
 import com.demo.alkolicznik.api.services.StoreService;
 import com.demo.alkolicznik.dto.city.CityDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -19,6 +23,24 @@ public class CityController {
 
     private final StoreService storeService;
 
+    @Operation(
+            summary = "Get a list of all cities",
+            description = "Each authenticated user is free to know all the currently-tracked cities in the application.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "City list retrieved"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Unauthorized (dummy response)",
+                            content = @Content
+                    )
+            },
+            security = @SecurityRequirement(
+                    name = "JWT Authentication"
+            )
+    )
     @GetMapping
     @PreAuthorize("isAuthenticated()")
     public List<CityDTO> getAll() {
