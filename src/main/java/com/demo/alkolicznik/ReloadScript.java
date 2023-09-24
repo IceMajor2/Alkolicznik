4191f1e89da71cfb8e04876309655da39f9f4082
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-24 22:05:58 +0200

    Make resource fetching (initial img data) more universal

--- src/main/java/com/demo/alkolicznik/ReloadScript.java
+++ src/main/java/com/demo/alkolicznik/ReloadScript.java
@@ -18,6 +18,8 @@ import com.demo.alkolicznik.utils.Utils;
 import io.imagekit.sdk.exceptions.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -27,6 +29,9 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.stereotype.Component;
@@ -34,6 +39,7 @@ import org.springframework.stereotype.Component;
 import javax.sql.DataSource;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Launching this class reloads database both from
@@ -94,34 +100,42 @@ public class ReloadScript implements CommandLineRunner {
             log.info("Deleting remote directory: '%s'...".formatted(imageKitPath));
             deleteFolder("");
             log.info("Reloading BEER images...");
-            sendAll("/images/beer", BeerImage.class);
+            sendAll("images/beer", BeerImage.class);
             log.info("Sending STORE images to remote...");
-            sendAll("/images/store", StoreImage.class);
+            sendAll("images/store", StoreImage.class);
             log.info("Successfully reloaded ImageKit directory");
         }
     }
 
     private <T extends ImageModel> void sendAll(String srcPath, Class<T> imgClass) throws IOException {
-        File[] imageDirectory = new File(new ClassPathResource(srcPath).getURI().getRawPath()).listFiles();
-        for (File image : imageDirectory) {
-            String absolutePath = image.getAbsolutePath();
-            String srcFilename = image.getName();
+        ResourcePatternResolver resourcePatResolver = new PathMatchingResourcePatternResolver();
+        Resource[] directory = resourcePatResolver.getResources("classpath:" + srcPath + "/*");
+
+        for (Resource file : directory) {
+            String srcFilename = FilenameUtils.getName(file.getURI().toString());
+            String filenameNoExtension = FilenameUtils.removeExtension(srcFilename);
+            String extension = FilenameUtils.getExtension(srcFilename);
+            InputStream inputStream = file.getInputStream();
+            File tempFile = File.createTempFile(filenameNoExtension, '.' + extension);
+            FileUtils.copyInputStreamToFile(inputStream, tempFile);
+            String absolutePath = tempFile.getAbsolutePath();
+
             Object model = null;
             try {
-                model = getAssociatedModel(srcFilename, imgClass);
+                model = getAssociatedModel(filenameNoExtension, imgClass);
             } catch (BeerNotFoundException e) {
                 log.warn("Beer image ('{}/{}') was not loaded. Possible reasons:\n1) Failed to determine beer ID " +
                                 "from filename: ID needs to be specified as first characters in the filename\n" +
                                 "2) There is no beer of specified id",
-                        RELATIVE_TO_BEER, srcFilename);
+                        RELATIVE_TO_BEER, filenameNoExtension);
             } catch (BeerAlreadyExistsException e) {
                 log.warn("You have duplicated IDs in '{}'. Image '{}' was not initialized",
-                        RELATIVE_TO_BEER, srcFilename);
+                        RELATIVE_TO_BEER, filenameNoExtension);
             } catch (StoreNotFoundException e) {
                 log.warn("Store image ('{}/{}') was not loaded. Possible reasons:\n1) Failed to determine store name " +
                                 "from filename: filename should only consist of exact store name (case insensitive)\n" +
                                 "2) There is no store of specified name",
-                        RELATIVE_TO_STORE, srcFilename);
+                        RELATIVE_TO_STORE, filenameNoExtension);
             }
             if (model != null)
                 addImage(model, absolutePath);
