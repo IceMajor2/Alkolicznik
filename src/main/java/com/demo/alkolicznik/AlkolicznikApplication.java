ee7cb9aad02f110cab0c855f438f56ef91f6452c
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-26 14:46:55 +0200

    Restore Vaadin theme

--- src/main/java/com/demo/alkolicznik/AlkolicznikApplication.java
+++ src/main/java/com/demo/alkolicznik/AlkolicznikApplication.java
@@ -1,10 +1,12 @@
 package com.demo.alkolicznik;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Theme(value = "style")
 public class AlkolicznikApplication implements AppShellConfigurator {
 
     public static void main(String[] args) {
