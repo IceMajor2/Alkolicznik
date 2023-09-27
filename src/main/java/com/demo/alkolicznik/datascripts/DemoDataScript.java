0c0add3e01dda1929ff64ad39484a74c281395af
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-27 16:54:42 +0200

    Check for imageKit.path collision (= same value) with production directory

--- src/main/java/com/demo/alkolicznik/datascripts/DemoDataScript.java
+++ src/main/java/com/demo/alkolicznik/datascripts/DemoDataScript.java
@@ -4,6 +4,7 @@ import com.demo.alkolicznik.datascripts.workers.ImageKitReloader;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
@@ -13,6 +14,11 @@ import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.Properties;
 
 @Component
 @Profile("demo")
@@ -22,8 +28,12 @@ public class DemoDataScript implements CommandLineRunner {
 
     private final ImageKitReloader imageKitReloader;
 
+    @Value("${imageKit.path}")
+    private String imageKitPath;
+
     @Bean
-    public DataSourceInitializer dataSourceInitializer(@Qualifier("dataSource") final DataSource dataSource) {
+    public DataSourceInitializer dataSourceInitializer(@Qualifier("dataSource") final DataSource dataSource) throws IOException {
+        checkRemotePathCollision();
         log.info("Reloading application's data to demonstration data... (using embedded database)");
         final String schemaScript = "data_sql/schema-demo-hsql.sql";
         final String dataScript = "data_sql/data-demo-hsql.sql";
@@ -43,7 +53,43 @@ public class DemoDataScript implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         log.info("Reloading ImageKit directory...");
-        imageKitReloader.reload();
+        imageKitReloader.reload(imageKitPath);
         log.info("Successfully reloaded ImageKit directory");
+    }
+
+    private void checkRemotePathCollision() throws IOException {
+        final String property = "imageKit.path";
+        String mainPath = getAbsolutePathToClassPathResource("imageKit.properties");
+        String demoPath = getAbsolutePathToClassPathResource("application-demo.properties");
+        Properties mainImageKit = getProperties(mainPath);
+        Properties demoImageKit = getProperties(demoPath);
+        String mainImageKitPath = mainImageKit.getProperty(property);
+        String demoImageKitPath = demoImageKit.getProperty(property);
+
+        if (mainImageKitPath == null || mainImageKitPath.isBlank())
+            throw new IllegalStateException("Property '%s' was missing from '%s'".formatted(property, mainPath));
+        if (demoImageKitPath == null || demoImageKitPath.isBlank())
+            throw new IllegalStateException("Property '%s' was missing from '%s'".formatted(property, demoPath));
+        if (Objects.equals(mainImageKitPath, demoImageKitPath))
+            throw new IllegalStateException("Property '%s' has the same value in production ('%s') and demo ('%s'). "
+                    .formatted(property, mainPath, demoPath) + "Please, make sure they differ");
+    }
+
+    private Properties getProperties(String path) {
+        try {
+            Properties properties = new Properties();
+            properties.load(new FileInputStream(path));
+            return properties;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    private String getAbsolutePathToClassPathResource(String file) {
+        try {
+            return Paths.get(new ClassPathResource(file).getURI()).toAbsolutePath().toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
