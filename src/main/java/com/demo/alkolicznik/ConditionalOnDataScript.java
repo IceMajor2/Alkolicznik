3a01120de664f7593fb6664ade4554987ffead95
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-27 15:14:00 +0200

    Create custom condition

---
+++ src/main/java/com/demo/alkolicznik/ConditionalOnDataScript.java
@@ -0,0 +1,21 @@
+package com.demo.alkolicznik;
+
+import org.springframework.boot.autoconfigure.condition.AnyNestedCondition;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Profile;
+
+public class ConditionalOnDataScript extends AnyNestedCondition {
+
+    ConditionalOnDataScript() {
+        super(ConfigurationPhase.REGISTER_BEAN);
+    }
+
+    @ConditionalOnBean(com.demo.alkolicznik.DeleteScript.class)
+    static class DeleteScript {}
+
+    @ConditionalOnBean(com.demo.alkolicznik.ReloadScript.class)
+    static class ReloadScript {}
+
+    @Profile("embedded-db")
+    static class embeddedDbProfile {}
+}
