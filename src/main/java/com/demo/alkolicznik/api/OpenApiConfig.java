633d365f1af1d89ceef7c8d05e72daed49132559
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-29 14:34:47 +0200

    Move from annotation-based OpenAPI definition and customize server value on 'alwaysdata' profile

--- src/main/java/com/demo/alkolicznik/api/OpenApiConfig.java
+++ src/main/java/com/demo/alkolicznik/api/OpenApiConfig.java
@@ -1,39 +1,66 @@
 package com.demo.alkolicznik.api;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.tags.Tag;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
-@OpenAPIDefinition(
-        info = @Info(
-                contact = @Contact(
-                        name = "Marcin Startek",
-                        email = "icemajorr2@gmail.com"
-                ),
-                description = "API documentation for Alkolicznik",
-                title = "Alkolicznik API",
-                version = "1.0"
-        ),
-        tags = {
-                @Tag(name = "Account"),
-                @Tag(name = "Beer"),
-                @Tag(name = "Store"),
-                @Tag(name = "Beer Price"),
-                @Tag(name = "Image"),
-                @Tag(name = "City")
-        }
-)
-@SecurityScheme(
-        name = "JWT Authentication",
-        description = "Paste token below or use <b>/api/auth/authenticate</b> endpoint (which also sets a cookie)",
-        scheme = "bearer",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        in = SecuritySchemeIn.HEADER
-)
+import java.util.List;
+
+@Configuration
 public class OpenApiConfig {
+
+    @Bean
+    @Profile("!alwaysdata")
+    public OpenAPI openAPI() {
+        return defaultOpenAPIDefinition();
+    }
+
+    @Bean
+    @Profile("alwaysdata")
+    public OpenAPI OpenAPI(@Value("${server.domain}") String domain) {
+        return defaultOpenAPIDefinition()
+                .servers(List.of(
+                                new Server().url("https://" + domain)
+                        )
+                );
+    }
+
+    private OpenAPI defaultOpenAPIDefinition() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .contact(
+                                        new Contact()
+                                                .name("Marcin Startek")
+                                                .email("icemajorr2@gmail.com")
+                                )
+                                .description("API documentation for Alkolicznik")
+                                .title("Alkolicznik API")
+                )
+                .tags(List.of(
+                                new Tag().name("Account"),
+                                new Tag().name("Beer"),
+                                new Tag().name("Store"),
+                                new Tag().name("Beer Price"),
+                                new Tag().name("Image"),
+                                new Tag().name("City")
+                        )
+                )
+                .schemaRequirement("JWT Authentication",
+                        new SecurityScheme()
+                                .name("JWT Authentication")
+                                .description("Paste token below or use <b>/api/auth/authenticate</b> endpoint (which also sets a cookie)")
+                                .scheme("bearer")
+                                .type(SecurityScheme.Type.HTTP)
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                );
+    }
 }
